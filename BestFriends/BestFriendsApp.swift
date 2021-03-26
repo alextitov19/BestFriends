74ca808905bde039834fced4b2a1f195d0b62848
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-03-26 13:20:40 -0700

    init proj

---
+++ BestFriends/BestFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFriendsApp.swift
+//  BestFriends
+//
+//  Created by Alex Titov on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BestFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
