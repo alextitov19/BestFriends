a31f7fecfcaa3454a674983f2c17f85b7e7f66a9
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-03-26 14:25:51 -0700

    Update ViewController.swift

--- BestFriends/ViewController.swift
+++ BestFriends/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Amplify
+import AmplifyPlugins
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fetchCurrentAuthSession()
     }
 
+    func fetchCurrentAuthSession() {
+        _ = Amplify.Auth.fetchAuthSession { result in
+            switch result {
+            case .success(let session):
+                print("Is user signed in - \(session.isSignedIn)")
+            case .failure(let error):
+                print("Fetch session failed with error \(error)")
+            }
+        }
+    }
 
 }
 
