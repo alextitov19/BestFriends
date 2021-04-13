945d685a431ea770083e1fb60f03f4b600eb0afd
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-04-12 17:43:10 -0700

    amplify works

--- BestFriends/AppDelegate.swift
+++ BestFriends/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Amplify
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        do {
+            try Amplify.configure()
+            print("Initialized Amplify");
+        } catch {
+            print("Could not initialize Amplify: \(error)")
+        }
+        
         return true
     }
 
