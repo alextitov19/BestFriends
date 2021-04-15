7a2da6a0b05d3cda7a4820bacbd7af7ba8b75afc
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-04-14 20:24:18 -0700

    registration ui complete

--- BestFriends/ContentView.swift
+++ BestFriends/ContentView.swift
@@ -69,7 +69,6 @@ struct Page2: View {
         ZStack {
             Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
             VStack {
-                
                 TextField("Enter your last name", text: $lastName)
                     .multilineTextAlignment(.center)
                     .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
@@ -81,27 +80,211 @@ struct Page2: View {
                 Spacer()
                     .frame(height: 50)
                 
-                Button {
-                    print("Going to page 2")
-                } label: {
-                    Text("Next")
-                        .frame(width: 200, height: 50)
-                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-                        .cornerRadius(25)
-                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                }
+                NavigationLink(destination: Page3()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
             }
         }
     }
 }
 
+struct Page3: View {
+    
+    @State private var username: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                TextField("Enter your username", text: $username)
+                    .multilineTextAlignment(.center)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .frame(width: 300, height: 40, alignment: .center)
+                    .font(.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(20)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                NavigationLink(destination: Page4()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
+            }
+        }
+    }
+}
 
+struct Page4: View {
+    
+    @State private var password: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                TextField("Enter your password", text: $password)
+                    .multilineTextAlignment(.center)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .frame(width: 300, height: 40, alignment: .center)
+                    .font(.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(20)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                NavigationLink(destination: Page5()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
+            }
+        }
+    }
+}
+
+struct Page5: View {
+    
+    @State private var email: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .multilineTextAlignment(.center)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .frame(width: 300, height: 40, alignment: .center)
+                    .font(.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(20)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                NavigationLink(destination: Page6()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
+            }
+        }
+    }
+}
+
+struct Page6: View {
+    
+    @State private var pronouns: String = ""
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                TextField("Enter your pronouns", text: $pronouns)
+                    .multilineTextAlignment(.center)
+                    .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                    .frame(width: 300, height: 40, alignment: .center)
+                    .font(.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .cornerRadius(20)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                NavigationLink(destination: Page7()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
+            }
+        }
+    }
+}
+
+struct Page7: View {
+    
+    
+    @State private var birthDate: Date = Date()
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                Text("Please enter your birthdate")
+                    .font(.title)
+                
+                DatePicker("", selection: $birthDate, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                NavigationLink(destination: QuestionPage()) {
+                                    Text("Next")
+                                        .font(.title)
+                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                        .frame(width: 200, height: 50)
+                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                        .cornerRadius(25)
+                                }
+                
+            }
+        }
+    }
+}
+
+struct QuestionPage: View {
+    
+    
+    @State private var birthDate: Date = Date()
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)).ignoresSafeArea()
+            VStack {
+                Text("Please answer these questions for personalized ads")
+                    .font(.title)
+                    .bold()
+                    .frame(width: 300, height: 200, alignment: .center)
+                    .multilineTextAlignment(.center)
+                
+        
+                
+            }
+        }
+    }
+}
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-       ContentView()
-        Page1()
+       Page7()
     }
 }
 #endif
