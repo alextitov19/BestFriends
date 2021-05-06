01ff780d358fd119b145d015ebfddb42c26170bc
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-05 20:13:16 -0700

    works

--- BestFriends/BestFriendsApp.swift
+++ BestFriends/BestFriendsApp.swift
@@ -26,7 +26,7 @@ struct BestFriendsApp: App {
                 LoginView()
                     .environmentObject(sessionManager)
             case .signUp:
-                InfoViewPage1()
+                SignUpPage1()
                     .environmentObject(sessionManager)
             case .confirmationCode(let username):
                 ConfirmationView(username: username)
--- BestFriends/Settings/SettingsView.swift
+++ BestFriends/Settings/SettingsView.swift
@@ -29,7 +29,7 @@ struct SettingsView: View {
                 VStack {
                     
                     
-                    Link("Privacy Policy / Terms of Service", destination: URL(string: "https://https://socialtechlabs.com/privacy/")!)
+                    Link("Privacy Policy / Terms of Service", destination: URL(string: "https://socialtechlabs.com/privacy/")!)
                     .frame(width: 300, height: 50, alignment: .center)
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
