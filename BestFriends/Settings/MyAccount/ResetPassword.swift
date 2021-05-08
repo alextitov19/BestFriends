5bff72254dc298b15c59ca560407572a52d252d3
robroe1 <rroe@MadlyRad.com>
2021-05-08 19:36:40 -0400

    resetpasswd

--- BestFriends/Settings/MyAccount/ResetPassword.swift
+++ BestFriends/Settings/MyAccount/ResetPassword.swift
@@ -73,18 +73,21 @@ struct ResetPassword: View {
                         Spacer()
                             .frame(height: 50)
                         
+                        Text("Submit")
+                            .font(.title)
+                            .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                            .frame(width: 200, height: 50)
+                            .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                            .cornerRadius(25)
               
-                }
+                
             }
         }
     }
 }
 
-    struct ResetPassword_Previews : PreviewProvider {
-    static var previews: some View {
-        ResetPassword()
+   
     }
 }
 
-    }
-
+  
