56cd5253587c7004275cf5906a8289f8724eccbd
robroe1 <rroe@MadlyRad.com>
2021-05-09 21:46:01 -0400

    dark mode

--- DarkMode/DarkModeView.swift
+++ DarkMode/DarkModeView.swift
@@ -26,15 +26,21 @@ struct DarkModeView: View {
                     .foregroundColor(.white)
                     .font(.title)
             
+                Spacer().frame(height: 5)
+                Text("Let's fix this mess!")
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                    .foregroundColor(.white)
+                    .font(.title)
+            
                 Spacer().frame(height: 25)
-               
                 VStack {
                 
                     Spacer()
                         .frame(height: 30)
                     
                     
-                    Link("Getting Started -10 Questions You Can Ask.", destination: URL(string: "https://socialtechlabs.com/darkMode/")!)
+                    Link("Getting Started -10 Questions to Ask", destination: URL(string: "https://socialtechlabs.com/darkMode/")!)
                     .frame(width: 325, height: 50, alignment: .center)
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
@@ -47,7 +53,7 @@ struct DarkModeView: View {
                     NavigationLink(
                         destination: InfoViewPage1(),
                         label: {
-                            Text("ShakingCoolDarkMode")
+                            Text("ShakingCool-DarkMode")
                         })
                     .frame(width: 325, height: 50, alignment: .center)
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
