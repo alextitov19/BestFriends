b0e2c92d3bc08d5646897ba39193141a4aa67c3e
robroe1 <rroe@MadlyRad.com>
2021-05-09 20:22:00 -0400

    Dark Mode main page

---
+++ DarkMode/DarkModeView.swift
@@ -0,0 +1,85 @@
+//
+//  DarkModeView.swift
+//  BestFriends
+//
+//  Created by Robert Roe on 5/9/21.
+//
+
+import SwiftUI
+
+struct DarkModeView: View {
+    
+
+    var body: some View {
+        NavigationView {
+        ZStack {
+
+            Image("purpleBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            VStack {
+                Text("'DarkMode'")
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                    .foregroundColor(.white)
+                    .font(.title)
+            
+                Spacer().frame(height: 25)
+               
+                VStack {
+                
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    
+                    Link("Getting Started -10 Questions You Can Ask.", destination: URL(string: "https://socialtechlabs.com/darkMode/")!)
+                    .frame(width: 325, height: 50, alignment: .center)
+                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
+                    .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                    .cornerRadius(25)
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    
+                    NavigationLink(
+                        destination: InfoViewPage1(),
+                        label: {
+                            Text("ShakingCoolDarkMode")
+                        })
+                    .frame(width: 325, height: 50, alignment: .center)
+                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
+                    .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                    .cornerRadius(25)
+                    
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    
+                    NavigationLink(
+                        destination: InfoViewPage1(),
+                        label: {
+                            Text("DarkMode - Stipulations")
+                        })
+                    .frame(width: 325, height: 50, alignment: .center)
+                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
+                    .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                    .cornerRadius(25)
+                    
+                    
+                    
+}
+        }
+    }
+}
+    }
+}
+
+struct DarkModeView_Previews : PreviewProvider {
+    static var previews: some View {
+        DarkModeView()
+    }
+}
