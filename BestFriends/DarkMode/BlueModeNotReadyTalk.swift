622e74177978e880779fbd57ce9f347973dbe64f
robroe1 <rroe@MadlyRad.com>
2021-05-12 18:57:52 -0400

    blue

--- BestFriends/DarkMode/BlueModeNotReadyTalk.swift
+++ BestFriends/DarkMode/BlueModeNotReadyTalk.swift
@@ -69,7 +69,7 @@ struct BlueModeNotReadyTalk: View {
                         label: {
                             Text("Unlock special stickers to say the first words for you.")
                         })
-                    .frame(width: 300, height: 100, alignment: .center)
+                    .frame(width: 300, height: 75, alignment: .center)
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
                     .cornerRadius(25)
@@ -85,7 +85,7 @@ struct BlueModeNotReadyTalk: View {
                         label: {
                             Text("'BlueMode-Journal' coming V2.0")
                         })
-                    .frame(width: 300, height: 100, alignment: .center)
+                    .frame(width: 300, height: 50, alignment: .center)
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
                     .cornerRadius(25)
