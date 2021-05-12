b62726001180ffdc3d52d42486b0dcd37b634b23
robroe1 <rroe@MadlyRad.com>
2021-05-12 18:07:06 -0400

    bluemd

--- BestFriends/DarkMode/DarkModeView.swift
+++ BestFriends/DarkMode/DarkModeView.swift
@@ -66,34 +66,8 @@ struct DarkModeView: View {
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
                     .cornerRadius(25)
                     
-                    Spacer()
-                        .frame(height: 30)
-                    
-                    
-                    NavigationLink(
-                        destination: InfoViewPage1(),
-                        label: {
-                            Text("ShakingCool-Do NOT send hurtful images; you will be blocked.")
-                        })
-                    .frame(width: 325, height: 50, alignment: .center)
-                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
-                    .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
-                    .cornerRadius(25)
-                    
-                    
-                    Spacer()
-                        .frame(height: 30)
-                    
-                    
-                    NavigationLink(
-                        destination: InfoViewPage1(),
-                        label: {
-                            Text("Special 'BlueMode' Stickers")
-                        })
-                    .frame(width: 325, height: 50, alignment: .center)
-                    .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
-                    .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
-                    .cornerRadius(25)
+                
+                  
                     
                     Spacer()
                         .frame(height: 30)
@@ -123,12 +97,32 @@ struct DarkModeView: View {
                     .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
                     .background(Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
                     .cornerRadius(25)
+                    
+                    Spacer()
+                        .frame(height: 50)
+
+                    NavigationLink(destination: BlueModeNotReadyTalk()) {
+                                        Text("Next")
+                                            .font(.title)
+                                            .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                                            .frame(width: 200, height: 50)
+                                            .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                                            .cornerRadius(25)
+                    }
+                Spacer()
+                    .frame(height: 75)
+            
+            }
+            }
+                    
+                    
+                    
 }
         }
     }
 }
-    }
-}
+    
+
 
 struct DarkModeView_Previews : PreviewProvider {
     static var previews: some View {
