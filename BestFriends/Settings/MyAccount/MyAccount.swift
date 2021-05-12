715cc7f0737e284ae91805897a4774a13731fc1e
robroe1 <rroe@MadlyRad.com>
2021-05-11 22:25:07 -0400

    my acct

--- BestFriends/Settings/MyAccount/MyAccount.swift
+++ BestFriends/Settings/MyAccount/MyAccount.swift
@@ -50,7 +50,7 @@ struct MyAccount: View {
                       NavigationLink(
                           destination: ResetPassword(),
                           label: {
-                              Text("Change my Email")
+                              Text("Update my Email")
                           })
                       .frame(width: 300, height: 50, alignment: .center)
                       .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
