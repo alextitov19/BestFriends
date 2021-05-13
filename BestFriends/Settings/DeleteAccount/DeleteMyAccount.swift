68ac477a5f7f80dedcb51b8e879a7bfd95199abf
robroe1 <rroe@MadlyRad.com>
2021-05-13 14:57:42 -0400

    survey

--- BestFriends/Settings/DeleteAccount/DeleteMyAccount.swift
+++ BestFriends/Settings/DeleteAccount/DeleteMyAccount.swift
@@ -77,27 +77,24 @@ struct DeleteMyAccount: View {
                     
                 
                 Spacer()
-                    .frame(height: 50)
-                NavigationLink(destination: InfoViewPage2()) {
-                    
-                    // Take user to Website to take Survey
-                    
-                                    Text("Survey")
-                                        .font(.title)
-                                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                                        .frame(width: 200, height: 50)
-                                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-                                        .cornerRadius(25)
+                    .frame(height: 10)
+                
+                Link("Survey", destination: URL(string: "https://socialtechlabs.com/survey/")!)
+                .frame(width: 230, height: 50, alignment: .center)
+                .foregroundColor(Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)))
+                .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                .cornerRadius(25)
+                
                     
                     
                     Spacer()
-                        .frame(height: 25)
+                        .frame(height: 0)
                     
                     NavigationLink(destination: InfoViewPage2()) {
                                         Text("Delete Account")
                                             .font(.title)
                                             .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                                            .frame(width: 200, height: 50)
+                                            .frame(width: 230, height: 50)
                                             .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
                                             .cornerRadius(25)
                         Spacer()
@@ -116,4 +113,4 @@ struct DeleteMyAccount: View {
     }
 }
 }
-}
+
