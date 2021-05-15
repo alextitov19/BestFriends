e3adfd6e9ab7f7f6039a2a9209210f67245a708c
robroe1 <rroe@MadlyRad.com>
2021-05-15 15:36:13 -0400

    inf pgs

--- BestFriends/BestFriendsApp.swift
+++ BestFriends/BestFriendsApp.swift
@@ -28,7 +28,7 @@ struct BestFriendsApp: App {
                 LoginView()
                     .environmentObject(sessionManager)
             case .signUp:
-                SwipeGesture()
+                SettingsView()
                     .environmentObject(sessionManager)
             case .confirmationCode(let username):
                 ConfirmationView(username: username)
