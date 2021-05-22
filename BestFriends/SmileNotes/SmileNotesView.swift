087b04c31011a44dd663b65bb23211e88b501715
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-22 14:25:32 -0700

    card for smilenotes

--- BestFriends/SmileNotes/SmileNotesView.swift
+++ BestFriends/SmileNotes/SmileNotesView.swift
@@ -1,13 +1,39 @@
 
 import SwiftUI
+import Amplify
 
 struct SmileNotesView: View {
     
+    @State var messages: [Message] = []
+    
     var body: some View {
         ZStack {
-            Color(.green)
+            Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))
                 .ignoresSafeArea()
+            
+            VStack {
+                // MARK: Header
+                HStack {
+                    Text("Smile Notes")
+                        .font(.title).bold()
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+                
+                
+                
+                
+            }
+            
         }
+//        .onAppear { loadMessages() }
+    }
+    
+    private func loadMessages() {
+        guard let id = Amplify.Auth.getCurrentUser()?.username else { return }
+        let user = UserDataSource().getUser(id: id)
+        messages = user.smileNotes ?? []
     }
     
 }
