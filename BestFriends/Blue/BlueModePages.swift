46facba965b01948c71b8d716673597c4dfbfe3d
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-05-22 02:11:59 -0400

    blue mode to one page

--- BestFriends/Blue/BlueModePages.swift
+++ BestFriends/Blue/BlueModePages.swift
@@ -109,7 +109,7 @@ struct BlueModePage1: View {
                     .frame(height: 250)
                    
                    
-                NavigationLink(destination: DarkModePage2()) {
+                NavigationLink(destination: BlueModePage2()) {
                                     Text("Next")
                                         .font(.title)
                                         .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
@@ -143,7 +143,7 @@ struct BlueModePaqe1_Previews : PreviewProvider {
 
 
 // ********************************************
-struct DarkModePage2: View {
+struct BlueModePage2: View {
 
     var body: some View {
      //   NavigationView{
@@ -194,7 +194,7 @@ struct DarkModePage2: View {
                
                 
                 
-                NavigationLink(destination: BlueModeLP()) {
+                NavigationLink(destination: BlueModePage3()) {
                                     Text("Good because this, SUCKS!")
                                         .font(.system(size: 30))
                                         .fontWeight(.ultraLight)
@@ -214,10 +214,78 @@ struct DarkModePage2: View {
 
 
 
-struct DarkModePage2_Previews : PreviewProvider {
+struct BlueModePage2_Previews : PreviewProvider {
     static var previews: some View {
    
-        DarkModePage2()
+        BlueModePage2()
 
     }
 }
+
+
+// ***********************************
+struct BlueModePage3: View {
+
+    var body: some View {
+  
+        ZStack {
+            Image("Shayla3 copy")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            VStack {
+                ZStack {
+                    Image("ChatBubbleTrans")
+                        .resizable()
+                        .frame(width: 400, height: 250)
+                        .scaledToFill()
+                    
+                    Text("Before you start 'unfriending' each other on IG & Snap, or repost private messages that will be hurtful - let's try 'BlueMode'.")
+                        .italic()
+                        .font(.system(size: 21))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 250, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                .padding()
+                
+            VStack {
+                Spacer()
+                
+                Text("We can help.")
+                    .font(.system(size: 25))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                NavigationLink(destination: DarkModeView()) {
+                        Text("Ok, activate 'BlueMode'")
+                            .font(.system(size: 30))
+                            .fontWeight(.ultraLight)
+                            .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                            .frame(width: 350, height: 50)
+                            .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .cornerRadius(25)
+           
+                    }
+                
+                Spacer()
+                    .frame(height: 160)
+                }
+            }
+        }
+    }
+}
+
+
+struct BlueModePage3_Previews : PreviewProvider {
+    static var previews: some View {
+        BlueModePage3()
+    }
+}
+
+// **************************************
+
