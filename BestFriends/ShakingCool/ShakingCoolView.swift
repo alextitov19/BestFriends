b32d2c86d3bba2fc9fa2b558cf5c2b17d0f6afc8
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-24 15:42:10 -0700

    ui

--- BestFriends/ShakingCool/ShakingCoolView.swift
+++ BestFriends/ShakingCool/ShakingCoolView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct ShakingCoolView: View {
     
     var body: some View {
-        Text("Shaking cool")
+        ZStack {
+            Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1))
+                .ignoresSafeArea()
+            
+            VStack {
+                // MARK: Header
+                HStack {
+                    Text("Shaking Cool")
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                        .font(.system(size: 40, weight: .heavy))
+                }
+                
+                Spacer()
+                
+                
+            }
+            
+        }
     }
 }
 
