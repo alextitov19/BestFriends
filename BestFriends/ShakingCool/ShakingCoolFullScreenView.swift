4600a6f188ed4aeee32ea1e41b3a20a42439c393
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-05-25 06:51:11 -0700

    working on shaking cool, got links to load

--- BestFriends/ShakingCool/ShakingCoolFullScreenView.swift
+++ BestFriends/ShakingCool/ShakingCoolFullScreenView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import Amplify
 
 struct ShakingCoolFullScreenView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State var image: Image = Image("Loading")
+    
+    init() {
+        loadAndCycle()
+    }
 
     var body: some View {
-        Button("Dismiss Modal") {
-            presentationMode.wrappedValue.dismiss()
+        ZStack {
+            image
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFit()
+            
+            Button("Dismiss Modal") {
+                presentationMode.wrappedValue.dismiss()
+            }
+        }
+        
+        
+    }
+    
+    private func loadAndCycle() {
+        guard let id = Amplify.Auth.getCurrentUser()?.username else { return }
+        let user = UserDataSource().getUser(id: id)
+        print("Got user: ", user)
+        guard let links = user.shakingCoolLinks else { return }
+        for link in links {
+            print(link)
         }
     }
 }
