38e79f455ff86a158e4eb24a33d9269ad9d13733
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-05-30 21:58:05 -0400

    changingBackground pages start

---
+++ BestFriends/Settings/MyAccount/ChangingBackground/MotionBackground.swift
@@ -0,0 +1,8 @@
+//
+//  MotionBackground.swift
+//  BestFriends
+//
+//  Created by Social Tech on 5/30/21.
+//
+
+
---
+++ BestFriends/Settings/MyAccount/ChangingBackground/StaticBackground.swift
@@ -0,0 +1,8 @@
+//
+//  StaticBackground.swift
+//  BestFriends
+//
+//  Created by Social Tech on 5/30/21.
+//
+
+import Foundation
