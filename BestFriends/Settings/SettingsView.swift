68342ff7a1bb48b59beb2b4a00cec37b367d2cf7
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-06-12 00:16:47 -0400

    making settings look better - colors

--- BestFriends/Settings/SettingsView.swift
+++ BestFriends/Settings/SettingsView.swift
@@ -39,7 +39,7 @@ struct SettingsView: View {
                         .fontWeight(.ultraLight)
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                         .multilineTextAlignment(.center)
-                        .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                        
                     NavigationLink(
                             destination: InfoViewPage1(),
@@ -58,22 +58,8 @@ struct SettingsView: View {
                             Spacer().frame(height: 20)
                            
                            
-                                NavigationLink(
-                                    destination: AddFriends(),
-                                    label: {
-                                        Text("Add my Friends")
-                                    })
-                                    .frame(width: 325, height: 50)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 25)
-                                            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 5)
-                                            )
-                       
                        
                         VStack {
-                        Spacer().frame(height: 20)
-                       
                        
                             NavigationLink(
                                 destination: ShakingCoolView(),
@@ -109,30 +95,54 @@ struct SettingsView: View {
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 25)
-                                        .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 5)
+                                        .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 5)
                                         )
-                            
+                          
                             Spacer().frame(height: 45)
                            
+                            Text("My Friends")
+                                .font(.system(size: 32))
+                                .fontWeight(.ultraLight)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                       
+                           
+                            
+                            NavigationLink(
+                                destination: AddFriends(),
+                                label: {
+                                    Text("Add Friends")
+                                })
+                                .frame(width: 325, height: 50)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 5)
+                                        )
+                            Spacer().frame(height: 45)
+                         
+                            VStack {
                             
                             Text("Purchase Stickers & Gifs with Tokens")
                                 .font(.system(size: 32))
                                 .fontWeight(.ultraLight)
-                                .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                                .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
                                 .multilineTextAlignment(.center)
-                                .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .frame(width: 375, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                            
                                 NavigationLink(
                                     destination: Tokens(),
                                     label: {
                                         Text("How do Tokens work?")
                                     })
-                                    .frame(width: 325, height: 50)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 25)
-                                            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 5)
-                                            )
+                                    
+                                         .frame(width: 325, height: 50)
+                                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                         .overlay(
+                                             RoundedRectangle(cornerRadius: 25)
+                                                 .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
+                                                 )
                         
                         VStack {
                       
@@ -144,7 +154,7 @@ struct SettingsView: View {
                            
                             .font(.system(size: 32))
                             .fontWeight(.ultraLight)
-                            .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             .multilineTextAlignment(.center)
                             .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             
@@ -157,7 +167,7 @@ struct SettingsView: View {
                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             .overlay(
                                 RoundedRectangle(cornerRadius: 25)
-                                    .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
+                                    .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                     )
                         
                         Spacer()
@@ -169,7 +179,7 @@ struct SettingsView: View {
                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             .overlay(
                                 RoundedRectangle(cornerRadius: 25)
-                                    .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
+                                    .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                     )
                         
                        
@@ -186,7 +196,7 @@ struct SettingsView: View {
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 25)
-                                        .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
+                                        .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                         )
                                 
                             Spacer()
@@ -198,7 +208,7 @@ struct SettingsView: View {
                                 .font(.system(size: 32))
                                 .fontWeight(.ultraLight)
                                 
-                                .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                                .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
                                 .multilineTextAlignment(.center)
                                 .frame(width: 385, height: 50, alignment: .center
                                 )
@@ -214,7 +224,7 @@ struct SettingsView: View {
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 25)
-                                        .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                        .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                         )
                             
                             Spacer()
@@ -229,7 +239,7 @@ struct SettingsView: View {
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 25)
-                                        .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                        .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                         )
                         
                             
@@ -249,7 +259,7 @@ struct SettingsView: View {
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 25)
-                                            .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                            .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                             )
                                 
                                 Spacer()
@@ -264,7 +274,7 @@ struct SettingsView: View {
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                 .overlay(
                                     RoundedRectangle(cornerRadius: 25)
-                                        .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                        .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                         )
                                 
                                 
@@ -277,7 +287,7 @@ struct SettingsView: View {
                                     .font(.system(size: 32))
                                     .fontWeight(.ultraLight)
                                     
-                                    .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                     .multilineTextAlignment(.center)
                                     .frame(width: 385, height: 50, alignment: .center
                                     )
@@ -292,7 +302,7 @@ struct SettingsView: View {
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 25)
-                                            .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                             )
                                 
                                 Spacer()
@@ -308,7 +318,7 @@ struct SettingsView: View {
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 25)
-                                            .stroke(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)), lineWidth: 1)
+                                            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                             )
                                 VStack {
                                  
@@ -327,7 +337,7 @@ struct SettingsView: View {
                                             .font(.system(size: 32))
                                             .fontWeight(.ultraLight)
                                             
-                                            .foregroundColor(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+                                            .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
                                             .multilineTextAlignment(.center)
                                             .frame(width: 385, height: 50, alignment: .center
                                        )
@@ -338,7 +348,7 @@ struct SettingsView: View {
                                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                         .overlay(
                                             RoundedRectangle(cornerRadius: 25)
-                                                .stroke(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), lineWidth: 1)
+                                                .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                                 )
                                     
                                     
@@ -351,7 +361,7 @@ struct SettingsView: View {
                                        
                                         .font(.system(size: 32))
                                         .fontWeight(.ultraLight)
-                                        .foregroundColor(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)))
+                                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                         .multilineTextAlignment(.center)
                                         .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                         
@@ -367,7 +377,7 @@ struct SettingsView: View {
                                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                             .overlay(
                                                 RoundedRectangle(cornerRadius: 25)
-                                                    .stroke(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), lineWidth: 1)
+                                                    .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
                                                     )
                                  
                                    
@@ -446,13 +456,14 @@ struct SettingsView: View {
         }
     }
 }
+//
+//struct SettingsView_Previews : PreviewProvider {
+//    static var previews: some View {
+//        SettingsView()
+//    }
+//}
 
-struct SettingsView_Previews : PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
+
+
 }
-
-
-
 }
