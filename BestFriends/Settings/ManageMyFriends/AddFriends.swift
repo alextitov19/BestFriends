6804893e92be8d473a6bb7d6d39c0201483d9ef6
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-06-12 10:04:22 -0400

    Add Friends page

--- BestFriends/Settings/ManageMyFriends/AddFriends.swift
+++ BestFriends/Settings/ManageMyFriends/AddFriends.swift
@@ -30,15 +30,21 @@ struct AddFriends: View {
                         .multilineTextAlignment(.center)
                         .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
-                  
-                    
-                    Text("You're not on IG or Snap busy amassing connections. BestFriends is about building your team of friends that you trust, confide in and that actually care about you and your happiness.")
+                    Text("You're not on IG or Snap busy amassing connections.")
                          .italic() .italic()
+                         .font(.system(size: 25))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    Text("BestFriends is about building your team of friends that you trust, confide in and that actually care about you and your happiness.")
+                        
                          .font(.system(size: 30))
                          .fontWeight(.regular)
                          .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
                          .multilineTextAlignment(.center)
-                         .frame(width: 385, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                         .frame(width: 385, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
                     Spacer()
                         .frame(height: 35)
@@ -54,20 +60,28 @@ struct AddFriends: View {
                                  RoundedRectangle(cornerRadius: 25)
                                      .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
                                      )
-                    
+                    Spacer()
+                        .frame(height: 35)
                
+                    HStack {
                     
-                    Text("Tap 'Add Friends' icon at bottom of Homepage. Text your friend your QR code on your phone.")
+                    Image("inviteWhite")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                        .scaledToFill()
+                    
+                    
+                    Text("Tap icon on Homepage. Text your friend your QR code.")
                        
                         .font(.system(size: 20))
                         .fontWeight(.regular)
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
-                        .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: 200, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                       
-                  
+                    }
                     
-                    Text("Notes: 1) When a friend adds you send them a pic you want them to put on their ShakingCool. 2) You can be in multiple frind groups.")
+                    Text("Notes: You can be in multiple friend groups.")
                        
                         .font(.system(size: 20))
                         .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
