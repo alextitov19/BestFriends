f4962223b7c4f246e085d90957e354f59a8474bf
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-06-19 14:46:40 -0700

    smile popover

---
+++ BestFriends/Chat/SmileNotePopover.swift
@@ -0,0 +1,37 @@
+//
+//  RandomSmileNotePage.swift
+//  BestFriends
+//
+//  Created by Alex Titov on 6/19/21.
+//
+
+import SwiftUI
+import Amplify
+
+struct SmieNotePopover: View {
+    
+    var messages: [Message]
+    
+    init() {
+        let username = Amplify.Auth.getCurrentUser()?.username ?? "Could not find user"
+        let user = UserDataSource().getUser(id: username)
+        self.messages = user.smileNotes ?? []
+    }
+    
+    var body: some View {
+        ZStack {
+            Image("SmileNote4")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            if messages.count > 0 {
+                SmileNotesCard(message: messages.randomElement()!)
+            } else {
+             Text("No SmileNotes yet, you should add some!")
+                .foregroundColor(.white)
+            }
+            
+        }
+    }
+}
+
