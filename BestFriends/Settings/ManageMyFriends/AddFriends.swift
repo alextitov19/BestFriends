dbb552e10a69d3a5add816e3e2ee9e124da67c62
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-06-28 15:15:08 -0400

    Add Friends page

--- BestFriends/Settings/ManageMyFriends/AddFriends.swift
+++ BestFriends/Settings/ManageMyFriends/AddFriends.swift
@@ -29,40 +29,16 @@ struct AddFriends: View {
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                  
+                    Text("(you can be in multiple friend groups)")
+                        .italic()
+                        .font(.system(size: 20))
+                        .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 385, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
-                    Text("You're not on IG or Snap busy amassing connections.")
-                         .italic() .italic()
-                         .font(.system(size: 25))
-                         .fontWeight(.regular)
-                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                         .multilineTextAlignment(.center)
-                         .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    
-                    Text("BestFriends is about building your team of friends that you trust, confide in and that really care about you - and your happiness.")
-                        
-                         .font(.system(size: 30))
-                         .fontWeight(.regular)
-                         .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
-                         .multilineTextAlignment(.center)
-                         .frame(width: 385, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    
-                    Spacer()
-                        .frame(height: 35)
-                   
-                    NavigationLink(
-                        destination: SettingsView(),
-                        label: {
-                            Text("Let's get started ***(not linked yet)")
-                        })
-                             .frame(width: 325, height: 50)
-                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                             .overlay(
-                                 RoundedRectangle(cornerRadius: 25)
-                                     .stroke(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), lineWidth: 1)
-                                     )
-                    Spacer()
-                        .frame(height: 35)
-               
                     HStack {
                     
                     Image("inviteWhite")
@@ -71,28 +47,81 @@ struct AddFriends: View {
                         .scaledToFill()
                     
                     
-                    Text("Tap icon on Homepage. Text your friend your QR code.")
-                       
-                        .font(.system(size: 20))
+                    Text("Tap icon on Homepage.")
+                        .italic()
+                        .font(.system(size: 27))
                         .fontWeight(.regular)
-                        .foregroundColor(.white)
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
                         .multilineTextAlignment(.center)
                         .frame(width: 200, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                       
                     }
+                    Text("Step 1.  Select [my QR code]                  .")
+                        
+                         .font(.system(size: 22))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
-                    Text("Notes: You can be in multiple friend groups.")
-                       
-                        .font(.system(size: 20))
-                        .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
-                        .fontWeight(.regular)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 385, height: 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                       
-                            
-                                     Spacer()
-                                         .frame(height: 30)
+                    Text("Step 2. Screen-shot your QR code and text it to your friend.")
+                         
+                         .font(.system(size: 22))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 375, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Text("Step 3. Ask friend to save image of your QR code to their gallery.")
+                        
+                         .font(.system(size: 22))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 375, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                  
+                    Text("Step 4. Ask friend to select [my gallery] and tap the image of your QR code.")
+                        
+                         .font(.system(size: 22))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 375, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                   
+                    Spacer()
+                        .frame(height: 35)
+                    
+                    NavigationLink(
+                        destination: SettingsView(),
+                        label: {
+                            Text("I'm Done")
+                        })
+                        .font(.system(size: 35))
+                        .frame(width: 325, height: 50)
+                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                             .overlay(
+                                 RoundedRectangle(cornerRadius: 25)
+                                     .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
+                                     )
+                    
+                    VStack {
+                    
+                    Text("Add friends that you trust, confide in and that really care about you and your happiness.")
+                        .italic()
+                         .font(.system(size: 25))
+                         .fontWeight(.regular)
+                         .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
+                         .multilineTextAlignment(.center)
+                         .frame(width: 385, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                 
+                   
+               
+                    Spacer()
+                        .frame(height: 55)
+               
+                 
                          
                         }
                     }
@@ -107,3 +136,4 @@ struct AddFriends_Previews : PreviewProvider {
 }
 
         
+}
