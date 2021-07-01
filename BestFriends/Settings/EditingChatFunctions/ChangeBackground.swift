724747d46d6334339875f6f1b067beee8b780e6a
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-07-01 16:49:12 -0600

    change chat background

--- BestFriends/Settings/EditingChatFunctions/ChangeBackground.swift
+++ BestFriends/Settings/EditingChatFunctions/ChangeBackground.swift
@@ -8,6 +8,7 @@
 
 import Amplify
 import SwiftUI
+import AVKit
 
 struct ChangeBackground: View {
         
@@ -25,7 +26,13 @@ struct ChangeBackground: View {
                     .padding()
                 
                 ScrollView {
+                    
+                    //Start of IMAGE BACKGROUNDS
                     VStack {
+                        Text("Static images")
+                            .foregroundColor(.white)
+                            .font(.system(size: 18, weight: .ultraLight))
+                        
                         HStack {
                             VStack {
                                 Image("purpleBackground")
@@ -49,7 +56,7 @@ struct ChangeBackground: View {
                                     //                                .cornerRadius(25)
                                     .border(Color.white, width: 2)
                                 
-                                Button("Select", action: { saveData(index: 1) })
+                                Button("Select", action: { saveData(index: 2) })
                                     .frame(width: 110, height: 40)
                                     .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
                                     .foregroundColor(.white)
@@ -66,7 +73,7 @@ struct ChangeBackground: View {
     //                                .cornerRadius(25)
                                     .border(Color.white, width: 2)
                                 
-                                Button("Select", action: { saveData(index: 1) })
+                                Button("Select", action: { saveData(index: 3) })
                                     .frame(width: 110, height: 40)
                                     .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
                                     .foregroundColor(.white)
@@ -81,7 +88,72 @@ struct ChangeBackground: View {
     //                                .cornerRadius(25)
                                     .border(Color.white, width: 2)
                                 
-                                Button("Select", action: { saveData(index: 1) })
+                                Button("Select", action: { saveData(index: 4) })
+                                    .frame(width: 110, height: 40)
+                                    .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            }
+                            .padding()
+                        }
+                        
+                        //Start of VIDEO BACKGROUNDS
+                        Text("Looping videos")
+                            .foregroundColor(.white)
+                            .font(.system(size: 18, weight: .ultraLight))
+                        
+                        HStack {
+                            VStack {
+                                VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "CityMopeds", withExtension: "mp4")!))
+                                    .frame(width: 150, height: 250)
+    //                                .cornerRadius(25)
+                                    .border(Color.white, width: 2)
+                                
+                                Button("Select", action: { saveData(index: 101) })
+                                    .frame(width: 110, height: 40)
+                                    .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            }
+                            .padding()
+                                                    
+                            VStack {
+                                VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "CityMopeds", withExtension: "mp4")!))
+                                    .frame(width: 150, height: 250)
+    //                                .cornerRadius(25)
+                                    .border(Color.white, width: 2)
+                                
+                                Button("Select", action: { saveData(index: 102) })
+                                    .frame(width: 110, height: 40)
+                                    .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            }
+                            .padding()
+                        }
+                        
+                        HStack {
+                            VStack {
+                                VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "CityMopeds", withExtension: "mp4")!))
+                                    .frame(width: 150, height: 250)
+    //                                .cornerRadius(25)
+                                    .border(Color.white, width: 2)
+                                
+                                Button("Select", action: { saveData(index: 103) })
+                                    .frame(width: 110, height: 40)
+                                    .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            }
+                            .padding()
+                                                    
+                            VStack {
+                                VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "CityMopeds", withExtension: "mp4")!))
+                                    .frame(width: 150, height: 250)
+    //                                .cornerRadius(25)
+                                    .border(Color.white, width: 2)
+                                
+                                Button("Select", action: { saveData(index: 104) })
                                     .frame(width: 110, height: 40)
                                     .background(Color(#colorLiteral(red: 0.5280093551, green: 0.4946141839, blue: 1, alpha: 1)))
                                     .foregroundColor(.white)
@@ -91,6 +163,7 @@ struct ChangeBackground: View {
                         }
                     }
                 }
+                
                 
                 
             }
