0119f22b1723ae1c505f7cf0cf613f17867a5516
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-07-01 17:43:37 -0600

    changepin done

--- BestFriends/Settings/MyAccount/ChangePin.swift
+++ BestFriends/Settings/MyAccount/ChangePin.swift
@@ -10,29 +10,38 @@ import SwiftUI
 
 struct ChangePin: View {
     
-    @State private var PIN: String = ""
-    @State private var readyToProceed = false
+    @State private var oldPin: String = ""
+    @State private var newPin: String = ""
+    @State private var statusString = ""
+    @State private var statusColor = Color(.clear)
+
+    
+    var userDataSource = UserDataSource()
     
     var body: some View {
         NavigationView {
             ZStack {
                 
-                Image("purpleBackground")
+                Image("changeBackground")
                     .resizable()
                     .ignoresSafeArea()
                     .scaledToFill()
                 
                 VStack {
                     
-                    Text("Change Restore Chat PIN")
+                    Text("Change Pin")
                         .font(.system(size: 30))
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
+                        .padding()
                     
-                    Spacer()
-                        .frame(height: 30)
+                    Text(statusString)
+                        .font(.system(size: 30))
+                        .foregroundColor(statusColor)
+                        .multilineTextAlignment(.center)
+                        .padding()
                     
-                    TextField("Enter NEW PIN", text: $PIN)
+                    TextField("Old pin", text: $oldPin)
                         .multilineTextAlignment(.center)
                         .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
                         .frame(width: 300, height: 40, alignment: .center)
@@ -46,7 +55,7 @@ struct ChangePin: View {
                         .frame(height: 50)
                     
                     
-                    SecureField("Enter NEW PIN", text: $PIN)
+                    TextField("New pin", text: $newPin)
                         .multilineTextAlignment(.center)
                         .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
                         .frame(width: 300, height: 40, alignment: .center)
@@ -59,28 +68,38 @@ struct ChangePin: View {
                     Spacer()
                         .frame(height: 150)
                     
+                    Button("Submit", action: changePin)
+                        .font(.title)
+                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                        .frame(width: 200, height: 50)
+                        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                        .cornerRadius(25)
+                        .onTapGesture {
+                            
+                        }
                     
                 }
-                
-                
-                Text("Submit")
-                    .font(.title)
-                    .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                    .frame(width: 200, height: 50)
-                    .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-                    .cornerRadius(25)
-                
             }
-            
-            
-            
-            Spacer()
-                .frame(height: 50)
         }
-        
-        
-        
+    }
+    
+    private func changePin() {
+        var user = userDataSource.getCurrentUser()
+        if user.secretPin == oldPin {
+            user.secretPin = newPin
+            userDataSource.updateUser(user: user)
+            statusString = "Success"
+            statusColor = Color.green
+        } else {
+            statusString = "Wrong pin"
+            statusColor = Color.red
+        }
     }
 }
 
 
+struct ChangePin_Previews : PreviewProvider {
+    static var previews: some View {
+        ChangePin()
+    }
+}
