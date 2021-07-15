a17ef86654b83ad9948bad59af7a7be02a76d63f
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-07-15 09:14:07 -0400

    settings

--- BestFriends/BestFriendsApp.swift
+++ BestFriends/BestFriendsApp.swift
@@ -54,7 +54,7 @@ struct BestFriendsApp: App {
                 LoginView()
                     .environmentObject(sessionManager)
             case .signUp:
-                SettingsView()
+                SignUp()
                     .environmentObject(sessionManager)
             case .confirmationCode(let username, let password):
                 ConfirmationView(username: username, password: password)
