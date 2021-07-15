f3352e6dfe17f7ad5438bf0497598db62b67cc06
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-07-15 09:08:55 -0700

    add friends back button fixed

--- BestFriends/Settings/ManageMyFriends/AddFriends.swift
+++ BestFriends/Settings/ManageMyFriends/AddFriends.swift
@@ -15,56 +15,79 @@ struct AddFriends: View {
     @EnvironmentObject var sessionManager: SessionManager
     
     var body: some View {
-        NavigationView{
-            ZStack {
+        ZStack {
+            
+            Image("SignUpPinBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            VStack {
                 
-                Image("SignUpPinBackground")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .scaledToFill()
+                
+                
+                
                 
                 VStack {
-              
                     
-                   
                     
+                    Text("Step 1")
+                        
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
+                        
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Text("Select [Get my QR code]")
+                        
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Spacer()
+                        .frame(height: 15)
+                    
+                    
+                    Text("Step 2")
+                        
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
+                        
+                        .multilineTextAlignment(.center)
+                        .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    Text("Screen-shot your QR code and text image to your friend.")
+                        
+                        .font(.system(size: 22))
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        
+                        .multilineTextAlignment(.center)
+                        .frame(width: 275, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Spacer()
+                        .frame(height: 15)
                     
                     VStack {
-                       
                         
-                        Text("Step 1")
-                            
-                            .font(.system(size: 22))
-                            .fontWeight(.regular)
-                            
-                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
-                            .multilineTextAlignment(.center)
-                            .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        
-                        
-                        Text("Select [Get my QR code]")
-                            
-                            .font(.system(size: 22))
-                            .fontWeight(.regular)
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                            .multilineTextAlignment(.center)
-                            .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        
-                        
-                        Spacer()
-                            .frame(height: 15)
-                        
-                        
-                        Text("Step 2")
+                        Text("Step 3")
                             
                             .font(.system(size: 22))
                             .fontWeight(.regular)
                             .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
-                           
+                            
                             .multilineTextAlignment(.center)
                             .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
-                        Text("Screen-shot your QR code and text image to your friend.")
+                        Text("Ask friend to save QR image to their gallery.")
                             
                             .font(.system(size: 22))
                             .fontWeight(.regular)
@@ -74,59 +97,35 @@ struct AddFriends: View {
                             .frame(width: 275, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
                         
+                        
+                        
                         Spacer()
                             .frame(height: 15)
                         
+                        
+                        Text("Step 4")
+                            
+                            .font(.system(size: 22))
+                            .fontWeight(.regular)
+                            .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
+                            
+                            .multilineTextAlignment(.center)
+                            .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        
+                        Text("Ask friend to select [My Gallery] and tap the image of your QR code.")
+                            
+                            .font(.system(size: 22))
+                            .fontWeight(.regular)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .multilineTextAlignment(.center)
+                            .frame(width: 375, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        
+                        Spacer()
+                            .frame(height: 55)
+                        
                         VStack {
                             
-                            Text("Step 3")
-                                
-                                .font(.system(size: 22))
-                                .fontWeight(.regular)
-                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
-                               
-                                .multilineTextAlignment(.center)
-                                .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             
-                            Text("Ask friend to save QR image to their gallery.")
-                                
-                                .font(.system(size: 22))
-                                .fontWeight(.regular)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                               
-                                .multilineTextAlignment(.center)
-                                .frame(width: 275, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                            
-                            
-                            
-                            Spacer()
-                                .frame(height: 15)
-                            
-                            
-                            Text("Step 4")
-                                
-                                .font(.system(size: 22))
-                                .fontWeight(.regular)
-                                .foregroundColor(Color(#colorLiteral(red: 0, green: 0.9791358113, blue: 0, alpha: 1)))
-                               
-                                .multilineTextAlignment(.center)
-                                .frame(width: 375, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                            Text("Ask friend to select [My Gallery] and tap the image of your QR code.")
-                                
-                                .font(.system(size: 22))
-                                .fontWeight(.regular)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .multilineTextAlignment(.center)
-                                .frame(width: 375, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                            Spacer()
-                                .frame(height: 55)
-                            
-                            VStack {
-                            
-                        
                             
                             Spacer()
                                 .frame(height: 55)
@@ -135,7 +134,7 @@ struct AddFriends: View {
                         
                         VStack {
                             
-                     
+                            
                             
                             
                             
@@ -143,31 +142,30 @@ struct AddFriends: View {
                     }
                 }
             }
+            
         }
-        
     }
-}
-//struct AddFriends_Previews : PreviewProvider {
-//    static var previews: some View {
-//        AddFriends()
-//    }
-//}
-        
-        
-        
-        
-        
-        
-        //Text("Ok, got it.")
-        //    .onTapGesture {
-        //        sessionManager.getCurrentAuthUser()
-        //    }
-        //    .font(.system(size: 35))
-        //    .frame(width: 325, height: 50)
-        //    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-        //    .overlay(
-        //        RoundedRectangle(cornerRadius: 25)
-        //            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
-        //    )
-
+    //struct AddFriends_Previews : PreviewProvider {
+    //    static var previews: some View {
+    //        AddFriends()
+    //    }
+    //}
+    
+    
+    
+    
+    
+    
+    //Text("Ok, got it.")
+    //    .onTapGesture {
+    //        sessionManager.getCurrentAuthUser()
+    //    }
+    //    .font(.system(size: 35))
+    //    .frame(width: 325, height: 50)
+    //    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+    //    .overlay(
+    //        RoundedRectangle(cornerRadius: 25)
+    //            .stroke(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), lineWidth: 1)
+    //    )
+    
 }
--- BestFriends/Settings/ManageMyFriends/PreAddFriends.swift
+++ BestFriends/Settings/ManageMyFriends/PreAddFriends.swift
@@ -16,97 +16,95 @@ struct PreAddFriends: View {
     @EnvironmentObject var sessionManager: SessionManager
     
     var body: some View {
-        NavigationView{
-            ZStack {
+        ZStack {
+            
+            Image("SignUpPinBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            VStack {
                 
-                Image("SignUpPinBackground")
-                    .resizable()
-                    .ignoresSafeArea()
-                    .scaledToFill()
                 
-                VStack {
-                  
-                   
+                
+                Text("Adding Your Friends.")
                     
-                        Text("Adding Your Friends.")
-                            
-                            .font(.system(size: 40))
-                            .fontWeight(.regular)
-                            .foregroundColor(.white)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                        .frame(height: 20)
+                    .font(.system(size: 40))
+                    .fontWeight(.regular)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Spacer()
+                    .frame(height: 20)
+                
+                
+                HStack {
                     
                     
-                    HStack {
-                          
-                      
-                            Text("Tap this icon on the next page >")
-                                .italic()
-                                .font(.system(size: 27))
-                                .fontWeight(.regular)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .multilineTextAlignment(.center)
-                                .frame(width: 200, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            
-                       
-                        
-                        Image("inviteWhite")
-                            .resizable()
-                            .frame(width: 45, height: 45)
-                            .scaledToFill()
-                        
-                    }
-                        
-                        Text("(Follow steps on Pop up)")
-                            
-                            .font(.system(size: 25))
-                            .fontWeight(.regular)
-                            .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
-                            .multilineTextAlignment(.center)
-                            .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        
-                        
-                        
-                      
-                    
-                    Spacer()
-                        .frame(height: 300)
-                    
-                  
-                   
-                    Text("(You're done)")
+                    Text("Tap this icon on the next page >")
                         .italic()
-                        .font(.system(size: 25))
+                        .font(.system(size: 27))
                         .fontWeight(.regular)
-                        .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                         .multilineTextAlignment(.center)
-                        .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .frame(width: 200, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
-                    NavigationLink(destination: AddFriends()) {
-                        Text("Got it.")
-                            .font(.title)
-                            .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                            .frame(width: 250, height: 50)
-                            .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
-                            .cornerRadius(25)
-                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-
                     
-                    }
+                    
+                    Image("inviteWhite")
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                        .scaledToFill()
+                    
+                }
+                
+                Text("(Follow steps on Pop up)")
+                    
+                    .font(.system(size: 25))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                
+                
+                
+                
+                Spacer()
+                    .frame(height: 300)
+                
+                
+                
+                Text("(You're done)")
+                    .italic()
+                    .font(.system(size: 25))
+                    .fontWeight(.regular)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 385, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                NavigationLink(destination: AddFriends()) {
+                    Text("Got it.")
+                        .font(.title)
+                        .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                        .frame(width: 250, height: 50)
+                        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                        .cornerRadius(25)
+                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    
+                    
+                }
             }
         }
     }
 }
 
-  struct PreAddFriends_Previews : PreviewProvider {
-        static var previews: some View {
-            PreAddFriends()
-    
-
-
-            
-        }
-  }
+struct PreAddFriends_Previews : PreviewProvider {
+    static var previews: some View {
+        PreAddFriends()
+        
+        
+        
+        
+    }
 }
