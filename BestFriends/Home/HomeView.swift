89136620c48e188c81fe9255501fe1137e9c6230
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-07-17 21:08:29 -0700

    spinning

--- BestFriends/Home/HomeView.swift
+++ BestFriends/Home/HomeView.swift
@@ -96,11 +96,14 @@ struct HomeView: View {
                                 membersOfNewRoom.remove(at: membersOfNewRoom.firstIndex(of: stars[index].id)!)
                                 stars[index].image = Image(uiImage: UIImage(named: "starBlue")!)
                                 print("Color change 1")
+//                                stars[index].isSpinning = false
                             } else {
                                 membersOfNewRoom.append(stars[index].id)
                                 stars[index].image = Image(uiImage: UIImage(named: "starGreen")!)
                                 print("Color change 2")
+//                                stars[index].isSpinning = true
                             }
+                            
                         }
                     }) {
                         stars[index]
--- BestFriends/Home/Star.swift
+++ BestFriends/Home/Star.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct Star: View {
     var id: String
     var name: String
-//    @State var isSelected = false
-//    @State var showingActionSheet = false
     var image = Image(uiImage: UIImage(named: "starPurple")!)
     var hidingName = true
-
+//    var isSpinning = true
+    @State var angle: Double = 0.0
+        @State var isAnimating = false
+        
+        var foreverAnimation: Animation {
+            Animation.linear(duration: 2.0)
+                .repeatForever(autoreverses: false)
+        }
+    
     var body: some View {
         VStack {
             Text(name)
@@ -22,41 +28,29 @@ struct Star: View {
                 .foregroundColor(.white)
                 .offset(y: 10)
                 .isHidden(hidingName)
-                           
             
-            image
-                .resizable()
-                .frame(width: 80, height: 80, alignment: .center)
-                .scaledToFill()
-                .blendMode(.screen)
+//            if isSpinning {
+                image
+                    .resizable()
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .scaledToFill()
+                    .blendMode(.screen)
+                    .rotationEffect(Angle(degrees: self.isAnimating ? 360.0 : 0.0))
+                                    .animation(self.foreverAnimation)
+                                    .onAppear {
+                                        self.isAnimating = true
+                                }
+//            } else {
+//                image
+//                    .resizable()
+//                    .frame(width: 80, height: 80, alignment: .center)
+//                    .scaledToFill()
+//                    .blendMode(.screen)
+//            }
+            
             
             
         }
-//        .gesture(LongPressGesture(minimumDuration: 1)
-//                    .onEnded { _ in
-//                        showingActionSheet = true
-//                    })
-//        .actionSheet(isPresented: $showingActionSheet) {
-//            ActionSheet(title: Text("Manage \(name)"), message: Text("Before you block or delete your friend, try BlueMode!"), buttons: [
-//                .default(Text("Switch to BlueMode")) {
-//                    // Code for switching to BlueMode below
-//                    let friend = UserDataSource().getUser(id: id)
-//                    let myself = UserDataSource().getCurrentUser()
-//                    if friend.BMFriend == nil && myself.BMFriend == nil {
-//                        BlueModeDataSource().addFriendToBlueMode(friendID: id)
-//                    }
-//                },
-//                .default(Text("Block \(name)")) {
-//                    // Code for blocking your friend below
-//
-//                },
-//                .default(Text("Delete \(name)")) {
-//                    // Code for deleting your friend below
-//
-//                },
-//                .cancel()
-//            ])
-//        }
         
     }
     
