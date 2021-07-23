0c42b6b66209b946f177f7028aa2d86cabc55d23
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-07-22 19:49:30 -0700

    NEW API

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "2e617f60f3e042c183ce26c1bb8a285b"
+  public let version: String = "41fad4cb6b23f0e9331e7c78675e7d04"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: Room.self)
