8ce1d0fc9ff3ffe0e4ebaee383f45bf856574af5
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-08-16 14:37:12 -0700

    circles

--- BestFriends/Registration2/WelcomeView.swift
+++ BestFriends/Registration2/WelcomeView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct WelcomeView: View {
     
     @EnvironmentObject var sessionManager: SessionManager
-
+    
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1))
                 .ignoresSafeArea()
+            VStack {
+                Circle()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                    .offset(x: 200, y: -100)
+                
+                Circle()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                    .offset(x: -200, y: 100)
+            }
+            
         }
     }
 }
