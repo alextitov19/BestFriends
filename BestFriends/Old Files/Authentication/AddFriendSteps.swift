9976b77c1d775bd5075ff617c23d5bda5f194bef
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-08-27 09:07:05 -0700

    add friend

--- BestFriends/Old Files/Authentication/AddFriendSteps.swift
+++ BestFriends/Old Files/Authentication/AddFriendSteps.swift
@@ -16,10 +16,24 @@ struct AddFriendSteps: View {
         NavigationView{
             ZStack {
                 
-                Image("SignUpPinBackground")
-                    .resizable()
+                Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 0.9))
                     .ignoresSafeArea()
-                    .scaledToFill()
+                ZStack {
+                    Circle()
+                        .frame(width: 400, height: 400)
+                        .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                        .offset(x: 200, y: -400)
+                    
+                    Circle()
+                        .frame(width: 300, height: 300)
+                        .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                        .offset(x: -200, y: -100)
+                    
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                        .offset(x: 150, y: 100)
+                }
                 
                 
                 VStack {
