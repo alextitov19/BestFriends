46bb393ba7e7765ac0ae40b020a9043e70c93fb3
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-08-28 02:38:36 -0400

    Photo Pop background upgrade

--- BestFriends/Necessary Files/ShakingCool/ShakingCoolView.swift
+++ BestFriends/Necessary Files/ShakingCool/ShakingCoolView.swift
@@ -29,16 +29,38 @@ struct ShakingCoolView: View {
     private var shakingCoolDataSource = ShakingCoolDataSource()
     
     var body: some View {
+//        ZStack {
+//            Image("SignUpPinBackground")
+//                .resizable()
+//                .scaledToFill()
+//                .ignoresSafeArea()
+//                .onAppear {
+//                    reloadData()
+//                }
+//
+           
         ZStack {
-            Image("SignUpPinBackground")
-                .resizable()
-                .scaledToFill()
+            Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 0.9))
                 .ignoresSafeArea()
-                .onAppear {
-                    reloadData()
-                }
-            
-            VStack {
+            ZStack {
+                Circle()
+                    .frame(width: 400, height: 400)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                    .offset(x: 200, y: -400)
+                
+                Circle()
+                    .frame(width: 300, height: 300)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                    .offset(x: -200, y: -100)
+                
+                Circle()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
+                    .offset(x: 150, y: 100)
+            }
+        
+        
+        VStack {
                 // MARK: Header
                 //                Text("ShakingCool")
                 //                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
