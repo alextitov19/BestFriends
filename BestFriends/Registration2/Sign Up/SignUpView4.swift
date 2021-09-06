5f4278277ebf30ffd8770917942f3fbe32c50638
robroe1 <76884097+robroe1@users.noreply.github.com>
2021-09-06 00:09:56 -0400

    cookies

--- BestFriends/Registration2/Sign Up/SignUpView4.swift
+++ BestFriends/Registration2/Sign Up/SignUpView4.swift
@@ -54,7 +54,7 @@ struct SignUpView4: View {
                     .padding(.top, 50)
                     .padding(.bottom, 10)
                 
-                Text("Unlike major social platforms ... we don't install cookies stalking you outside of BestFriends. We never have and never will sell your data.")
+                Text("Our Privacy Policy gives us permission to use 'cookies, however we decided to NOT install cookies stalking you outside of BestFriends. And, nope we don't sell your data.")
                     .font(.system(size: 26, weight: .light))
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
