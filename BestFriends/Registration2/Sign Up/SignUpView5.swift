2ce3158f8e027885d8d7df072c628a55ca9e59df
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-09-12 19:02:17 -0700

    Background

--- BestFriends/Registration2/Sign Up/SignUpView5.swift
+++ BestFriends/Registration2/Sign Up/SignUpView5.swift
@@ -39,25 +39,11 @@ struct SignUpView5: View {
 //                    .offset(x: -200, y: 400)
 //            }
        
-            Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 0.9))
-                .ignoresSafeArea()
-            ZStack {
-                Circle()
-                    .frame(width: 400, height: 400)
-                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
-                    .offset(x: 200, y: -400)
-                
-                Circle()
-                    .frame(width: 300, height: 300)
-                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
-                    .offset(x: -200, y: -100)
-                
-                Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(Color(#colorLiteral(red: 0.4874756932, green: 0.2377186716, blue: 0.9663465619, alpha: 1)))
-                    .offset(x: 150, y: 100)
-                
-            }
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+            
             VStack {
                 HStack {
                     Text("Get Started")
