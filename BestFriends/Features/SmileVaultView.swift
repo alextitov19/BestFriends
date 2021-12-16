fd27828fbd0ef0a432487ea338f5ed64cf6ad498
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2021-12-15 17:22:58 -0800

    done w sv

--- BestFriends/Features/SmileVaultView.swift
+++ BestFriends/Features/SmileVaultView.swift
@@ -15,12 +15,18 @@ struct SmileVaultView: View {
     private let userDS = UserDataSource()
     private let imageDS = ImageDataSource()
     
-    @State private var images: [UIImage] = []  //The actual images, with .frame and .offset and .ontap
-    @State private var imageLinks: [String] = [] //The S3 links to images gotten from User.smileVaultLinks
+    @State private var images: [UIImage] = []  // The actual images, with .frame and .offset and .ontap
+    @State private var imageLinks: [String] = [] // The S3 links to images gotten from User.smileVaultLinks
+    
+    @State private var user: User?
+    @State private var friends: [User] = [] // User's friends
     
     @State private var isImagePickerActive = false // Image Picker for choosing image from gallery to upload to own vault
     @State private var inputImage : UIImage? // Image selected by the image picker is saved here
     @State private var currentImageIndex = 0
+    
+    @State private var menuTitle = "Choose Vault" // The title of the clicable menu which switches the vault
+    
     var body: some View {
         
         ZStack {
@@ -34,50 +40,93 @@ struct SmileVaultView: View {
             // Images
             
             if images.count > 0 && currentImageIndex >= 0 && currentImageIndex < images.count {
-                    Image(uiImage: images[currentImageIndex])
-                        .resizable()
-                        .frame(maxWidth: .infinity)
-                        .scaledToFit()
-                        .gesture(DragGesture(minimumDistance: 3, coordinateSpace: .local)
-                                            .onEnded({ value in
-                                                if value.translation.width < 0 {
-                                                    // Swipe left
-                                                    if currentImageIndex + 1 < images.count {
-                                                        currentImageIndex += 1
-                                                    }
-                                                }
-
-                                                if value.translation.width > 0 {
-                                                    // right
-                                                    if currentImageIndex > 0 {
-                                                        currentImageIndex -= 1
-                                                    }
-                                                }
-                                            }))
+                Image(uiImage: images[currentImageIndex])
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .scaledToFit()
+                    .gesture(DragGesture(minimumDistance: 3, coordinateSpace: .local)
+                                .onEnded({ value in
+                        if value.translation.width < 0 {
+                            // Swipe left
+                            if currentImageIndex + 1 < images.count {
+                                currentImageIndex += 1
+                            }
+                        }
+                        
+                        if value.translation.width > 0 {
+                            // right
+                            if currentImageIndex > 0 {
+                                currentImageIndex -= 1
+                            }
+                        }
+                    }))
             }
-
+            
             
             
             // The border buttons and menus
             VStack {
+                // Top menu...
+                HStack {
+                    Menu {
+                        Button {
+                            if user != nil {
+                                menuTitle = "My Vault"
+                                imageLinks = user!.smileVaultLinks
+                                loadImages()
+                            }
+                        } label: {
+                            Text("My Vault")
+                        }
+                        ForEach(friends, id: \.id) { friend in
+                            Button {
+                                menuTitle = "\(friend.firstName)'s Vault"
+                                imageLinks = friend.smileVaultLinks
+                                loadImages()
+                            } label: {
+                                Text("\(friend.firstName)'s Vault")
+                            }
+                        }
+                        
+                    } label: {
+                        Text(menuTitle)
+                            .frame(width: 150, height: 50)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .background(ColorManager.purple5)
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal)
+                    
+                    // Add an image button
+                    Button(action: {
+                        openImagePicker()
+                    }) {
+                        Text("Add Image")
+                            .frame(width: 150, height: 50)
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .background(ColorManager.purple5)
+                            .cornerRadius(10)
+                            .sheet(isPresented: $isImagePickerActive, onDismiss: addImage) {
+                                // Image Picker for uploading image into my own vault...
+                                ImagePicker(image: self.$inputImage, sourceType: .photoLibrary)
+                            }
+                    }
+                    .padding(.horizontal)
+                    
+                }
                 
                 Spacer()
                 
-                // Add an image button
-                Button(action: {
-                    openImagePicker()
-                }) {
-                    Text("Add Image")
-                        .frame(width: 200, height: 50)
-                        .foregroundColor(.white)
-                        .font(.system(size: 25))
-                        .background(ColorManager.purple7)
-                        .cornerRadius(20)
-                        .sheet(isPresented: $isImagePickerActive, onDismiss: addImage) {
-                            // Image Picker for uploading image into my own vault...
-                            ImagePicker(image: self.$inputImage, sourceType: .photoLibrary)
-                        }
-                }
+                // Bottom navigation menu
+                Image("hideouts")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .scaledToFill()
+                    .onTapGesture {
+                        sessionManager.showHideouts()
+                    }
             }
             
         }
@@ -87,15 +136,22 @@ struct SmileVaultView: View {
     // Called when first opening the view
     private func initialLoad() {
         DispatchQueue.global(qos: .userInitiated).async() {
-            imageLinks = userDS.getCurrentUser().smileVaultLinks
-            print("Image links: ", imageLinks)
-            loadImages()
+            user = userDS.getCurrentUser()
+            if user != nil {
+                for id in user!.friends {
+                    friends.append(userDS.getUser(id: id))
+                }
+                imageLinks = user!.smileVaultLinks
+                print("Image links: ", imageLinks)
+                loadImages()
+            }
         }
     }
     
     // Load images from links
     private func loadImages() {
         DispatchQueue.global(qos: .userInitiated).async() {
+            images = []
             for link in imageLinks {
                 let image = imageDS.downloadImage(key: link, rotating: false, tall: false)
                 images.append(image)
