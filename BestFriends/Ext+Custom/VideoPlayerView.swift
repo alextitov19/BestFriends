f9574c99f05ae67db3ac6cf0452d648c8f589658
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2022-03-31 19:02:57 -0700

    stars

---
+++ BestFriends/Ext+Custom/VideoPlayerView.swift
@@ -0,0 +1,68 @@
+//
+//  AdPlayerView.swift
+//  BestFriends
+//
+//  Created by Alex Titov on 5/26/21.
+//
+
+import SwiftUI
+import AVKit
+import AVFoundation
+
+struct VideoPlayerView: UIViewRepresentable {
+    
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<VideoPlayerView>) {
+    }
+    
+    func makeUIView(context: Context) -> UIView {
+        return AdLoopingPlayerUIView(frame: .zero, name: name)
+    }
+}
+
+
+class AdLoopingPlayerUIView: UIView {
+    private let playerLayer = AVPlayerLayer()
+    private var playerLooper: AVPlayerLooper?
+    
+    var name: String
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(frame: CGRect, name: String) {
+        self.name = name
+        
+        super.init(frame: frame)
+        
+        
+        // Load the resource
+        let fileUrl = Bundle.main.url(forResource: name, withExtension: "mp4")!
+        let asset = AVAsset(url: fileUrl)
+        let item = AVPlayerItem(asset: asset)
+        
+        // Setup the player
+        let player = AVQueuePlayer()
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer)
+        
+        // Create a new player looper with the queue player and template item
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        
+        // Start the movie
+        player.play()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+}
+
--- BestFriends/HomeView.swift
+++ BestFriends/HomeView.swift
@@ -10,45 +10,29 @@ import SwiftUI
 struct HomeView: View {
     
     @EnvironmentObject var sessionManager: SessionManager
-
+    
+    @State private var homeData: HomeData?
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                ColorManager.purple1
-                    .ignoresSafeArea()
-                
-                Circle()
-                    .frame(width: 400, height: 400)
-                    .foregroundColor(ColorManager.purple3)
-                    .offset(x: 200, y: -400)
-                
-                Circle()
-                    .frame(width: 300, height: 300)
-                    .foregroundColor(ColorManager.purple3)
-                    .offset(x: -200, y: 400)
-                
-                VStack {
-                    Text("Welcome Home")
-                        .font(.custom("MainFont", size: 40).bold())
-                        .foregroundColor(ColorManager.grey3)
-                        .padding(30)
-                    
-                    Button(action: getHomeData, label: {
-                        Text("Get home data")
-                            .font(.custom("MainFont", size: 30).bold())
-                            .foregroundColor(ColorManager.red)
-                    })
-                    .padding(30)
-                    
-                    
-                }
-            }
+        ZStack {
+            // Background Image...
+            Image("blueBackground")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            // Stars animation...
+            AdPlayerView(name: "backgroundAnimation")
+                .ignoresSafeArea()
+                .blendMode(.screen)
+            
         }
     }
     
     private func getHomeData() {
         RestApi.instance.getHomeData().then{ data in
             print("Got HomeData: ", data)
+            homeData = data
         }.catch { err in
             print("Got error")
             print(err)
