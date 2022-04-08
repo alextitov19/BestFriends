a6d97b61d2166c626efd821d39ad1575410e44a8
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-08 10:10:10 -0400

    TermsOfServicePrivacy-changed the webpage destination & added a text box with instructions

---
+++ BestFriends/HomeView/Settings/TermsOfServicePrivacy.swift
@@ -0,0 +1,99 @@
+//
+//  TermsOfServicePrivacy.swift
+//  BestFriends
+//
+//  Created by Social Tech on 4/8/22.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct TermsConditionsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image("arrowRight")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .scaledToFit()
+                            .colorInvert()
+                            .rotationEffect(Angle(degrees: 180))
+                    }
+                    
+                    Spacer()
+                    
+                    Text("Privacy Policy & Terms of Service")
+                        .font(.system(size: 30))
+                        .offset(x: -25)
+           
+                    Spacer()
+                    
+                    Text("(in footer of website page)")
+                        .font(.system(size: 30))
+                        .offset(x: -25)
+                    
+          
+                    
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                Link(destination: URL(string: "https://socialtechlabs.com")!) {
+                    
+                    HStack {
+                        Text("Privacy Policy")
+                            .font(.system(size: 20))
+                            .foregroundColor(.black)
+
+                        Spacer().frame(maxHeight: 0)
+                        
+                        Image("arrowRight")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .scaledToFit()
+                            .colorInvert()
+                    }
+                }
+                
+                Divider()
+                
+                Link(destination: URL(string: "https://socialtechlabs.com")!) {
+                    HStack {
+                        Text("Terms of Serivce")
+                            .font(.system(size: 20))
+                            .foregroundColor(.black)
+                        
+                        Spacer()
+                        
+                        Image("arrowRight")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .scaledToFit()
+                            .colorInvert()
+                    }
+                }
+                
+                
+                
+                
+                Spacer()
+            }
+            .padding(30)
+            .navigationBarHidden(true)
+        }
+    }
+    
+}
