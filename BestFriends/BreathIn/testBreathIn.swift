86eb5ad2e88d02a47ca9206a29d414945b58c547
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-11 17:19:28 -0400

    Update testBreathIn.swift

--- BestFriends/BreathIn/testBreathIn.swift
+++ BestFriends/BreathIn/testBreathIn.swift
@@ -16,7 +16,6 @@ struct testBreathIn: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     
-    
     let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
     @State var count: Int = 10
@@ -35,7 +34,6 @@ struct testBreathIn: View {
             .scaledToFill()
 
         Image("settingsBackground")
-//            .fixedSize()
             .resizable()
             .ignoresSafeArea()
             .scaledToFill()
@@ -74,17 +72,31 @@ struct testBreathIn: View {
             .lineLimit(1)
                        }
 
-        .onReceive(timer, perform: { _ in
-    if count  <= 1 {
-        finishedText = "E  X  H  A  L  E"
+//        .onReceive(timer, perform: { _ in
+//    if count  == 1 {
+//        timer.upstream.connect().cancel()
+//        finishedText = "E  X  H  A  L  E"
+//
+//    } else {
+//        count -= 1
+//
+//
+        
+            .onReceive(timer) { _ in
+        if count  == 1 {
+            timer.upstream.connect().cancel()
+            finishedText = "E  X  H  A  L  E"
+                
+        } else {
+            count -= 1
             
-    } else {
-        count -= 1
+        
+        
         
         
                 }
             }
-        )}
+        }
         
 
 }
