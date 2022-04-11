b66a2360e3182240538f88fb47270e4a72fecfbf
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-11 15:10:10 -0400

    testBreathIn more updates

--- BestFriends/BreathIn/testBreathIn.swift
+++ BestFriends/BreathIn/testBreathIn.swift
@@ -19,9 +19,9 @@ struct testBreathIn: View {
     
     var body: some View {
     ZStack {
-//        Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
-//            .ignoresSafeArea()
-//
+        Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
+           .ignoresSafeArea()
+
         Image("Star Page")
             .resizable()
             .ignoresSafeArea()
@@ -81,25 +81,9 @@ struct testBreathIn: View {
             }
         )}
         
-        Spacer()
-            .frame(height: 20)
+
 }
 
-  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     struct testBreathIn_Previews : PreviewProvider {
