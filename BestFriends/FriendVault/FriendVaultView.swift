600d41ce12006e0351636714fca9896f94e52483
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-16 03:22:07 -0400

    friendvault page 1

--- BestFriends/FriendVault/FriendVaultView.swift
+++ BestFriends/FriendVault/FriendVaultView.swift
@@ -10,47 +10,100 @@ import SwiftUI
 
 struct FriendVaultView: View {
     
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
         ScrollView{
             
             ZStack {
                 Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
                     .ignoresSafeArea()
-                Image("Star Page")
+                
+                Image("Background")
                     .resizable()
                     .ignoresSafeArea()
                     .scaledToFill()
+                    .padding(-50)
                 
-                VStack {
+              
+               
+             
+                VStack {    
+                        HStack {
+                            Button(action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Image("arrowRight")
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                    .scaledToFit()
+                                    .colorInvert()
+                                    .rotationEffect(Angle(degrees: 180))
+                            }
+                       
+                        Spacer()
+                            .frame(height: 40)
+                    }
+                    
+                    HStack {
+                        Image("planetFV")
+
+                        Spacer()
+
+                        Image("Chat-Select")
+
+                       Spacer()
+
+                       Image("Friend Vault")
+
+                            .padding()
+                     }
+                 
+                    
                     
                     Spacer()
-                        .frame(height: 50)
-                    
-                    Text("FriendVault")
-                        .font(.system(size: 30, weight: .bold))
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 0)
-                    
+                        .frame(height: 40)
+                    Image("My feeling1")
                     
                     Spacer()
                         .frame(height: 15)
                     
-                   
-                        
-                }
+                    Image("My feeling")
+                    
+                    Spacer()
+                        .frame(height: 15)
+                    
+                    Image("Goal")
+                    
+                 Spacer()
+                      
+                               .frame(height: 50)
+                
+                              
+                
+                                .frame(height: 15)
+                
+                
+                                   Spacer()
+                              }
             }
         }
     }
-}
-                    
-                    
-                    
-                    
-                    struct FriendVaultView_Previews : PreviewProvider {
-                        static var previews: some View {
-                            FriendVaultView()
-                        }
-                    }
+    
+    
+    
+//        .padding(40)
+//        .navigationBarHidden(true)
+    
+    struct FriendVaultView_Previews : PreviewProvider {
+        static var previews: some View {
+            FriendVaultView()
+        }
+    }
+    
+    
 
+
+  
+}
 
