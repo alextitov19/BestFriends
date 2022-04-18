05709aa0a84580322519a99fc5def4e5c810c6a4
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-18 19:09:12 -0400

    added chat icon

--- BestFriends/FriendVault/FriendVault.swift
+++ BestFriends/FriendVault/FriendVault.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct FriendVault: View {
     
     var body: some View {
-    
+        NavigationView {
             
             ZStack {
                 Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
@@ -85,38 +85,38 @@ struct FriendVault: View {
                             .padding(.horizontal, 0)
                         
                         Spacer()
-                           .frame(height: 180)
+                           .frame(height: 50)
+                        
+                        
+                        
+//                        Image("ChatBlue")
 //
 //
-//                        NavigationLink(
-//                            destination: HomeView(),
-//                            label: {
-//                                Text("take me to CHAT")
-//                                    .fontWeight(.semibold)
-//                                    .foregroundColor(Color.white)
-//                                    .frame(width: 350.0, height: 50)
-//                                   .font(.system(size: 20))
-//
-//.background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
-//
-//                                                    .cornerRadius(15)
-//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                      NavigationLink(
+                         destination: HomeView(),
+                           label: {
+                                Text("take me to CHAT")
+                                   .fontWeight(.semibold)
+                                   .foregroundColor(Color.white)
+                                   .frame(width: 350.0, height: 50)
+                                  .font(.system(size: 20))
+
+.background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
+
+                                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
 
 
       
                                     
-                                }
-                            
-                        
+                        }
+                      )}
+                    
                 }
             }
-            
         }
-    
-    
-
-
-
+    }
+}
 struct FriendVault_Previews : PreviewProvider {
     static var previews: some View {
         FriendVault()
@@ -124,4 +124,4 @@ struct FriendVault_Previews : PreviewProvider {
 }
 
 
-}
+
