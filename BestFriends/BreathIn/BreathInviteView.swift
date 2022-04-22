cd8872f4c276736908bd19f6c08e3bc2f9f8abc2
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-22 03:55:43 -0400

    calming breaths [share] button edits

--- BestFriends/BreathIn/BreathInviteView.swift
+++ BestFriends/BreathIn/BreathInviteView.swift
@@ -247,7 +247,7 @@ struct BreathInviteView: View {
                                             
                                                 .background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
                                             
-                                                .cornerRadius(15)
+                                                .cornerRadius(0)
                                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                                             
                                             
