24707a2ff257cf52e5f69225a9de222f671a68cf
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2022-04-23 20:48:50 -0700

    Update RestAPI.swift

--- BestFriends/API/RestAPI.swift
+++ BestFriends/API/RestAPI.swift
@@ -11,8 +11,8 @@ import Promises
 class RestApi {
     var helper: Helper
     var userId: String?
-    let API_URL = "http://54.173.213.37:8080/api/v1/services"
-    let WS_URL = "ws://54.173.213.37:8080/api/v1/services/messages/"
+    let API_URL = "http://54.226.29.220:8080/api/v1/services"
+    let WS_URL = "ws://54.226.29.220:8080/api/v1/services/messages/"
 
 //    let API_URL = "http://localhost:8080/api/v1/services"
 //    let WS_URL = "ws://localhost:8080/api/v1/services/messages/"
