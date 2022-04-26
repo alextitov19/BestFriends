d524971ef838c8d595de5d8e6ed6f2cc338cc9c2
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-25 23:48:18 -0400

    streaks

--- BestFriends/Streaks/MyStreaks.swift
+++ BestFriends/Streaks/MyStreaks.swift
@@ -39,6 +39,12 @@ struct MyStreaks: View {
                     
                     Spacer()
                         .frame(height: 10)
+                    Text("Friend Streaks")
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 0)
                     
                     
                     
@@ -122,14 +128,67 @@ struct MyStreaks: View {
                                 .background(Color(hue: 0.128, saturation: 0.086, brightness: 1.0))
                                 .cornerRadius(15)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                               
-                           
+                        
                         })
+                    VStack {
                     
                     Spacer()
-                        .frame(height: 400)
+                        .frame(height: 10)
+                    Text("Goal Streaks")
+                        .font(.system(size: 30))
+                        .foregroundColor(.green)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 0)
+                  
+                        NavigationLink(
+                            destination: BreathInviteView(),
+                            label: {
+                                Text("Finished (xx) Goals")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.black)
+                                    .multilineTextAlignment(.leading)
+                                    .frame(width: 325, height: 30, alignment: .leading)
+                                    .font(.system(size: 20))
+                                    .padding(10)
+                                    .background(Color(hue: 0.128, saturation: 0.086, brightness: 1.0))
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                   
+                               
+                            })
+                         
+                        NavigationLink(
+                            destination: BreathInviteView(),
+                            label: {
+                                Text("Finished (xx) Rockets ")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.black)
+                                    .multilineTextAlignment(.leading)
+                                    .frame(width: 325, height: 30, alignment: .leading)
+                                    .font(.system(size: 20))
+                                    .padding(10)
+                                    .background(Color(hue: 0.128, saturation: 0.086, brightness: 1.0))
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                   
+                               
+                            })
+                     
+                        
+                        
+                        
+                        
+                        Spacer()
+                        .frame(height: 300)
            
-
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
                 }
             }
         }
