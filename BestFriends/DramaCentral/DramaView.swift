5d3721803a55691e88fcb5472da0a73536b7dc01
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-27 17:16:29 -0400

    DramaView

---
+++ BestFriends/DramaCentral/DramaView.swift
@@ -0,0 +1,112 @@
+//
+//  DramaView.swift
+//  BestFriends
+//
+//  Created by Social Tech on 4/27/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct DramaView: View {
+    
+    var body: some View {
+        NavigationView {
+            
+            ZStack {
+                Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
+                    .ignoresSafeArea()
+                Image("purpleBackground")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+              
+                Image("ballons")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                
+                
+                AdPlayerView(name: "backgroundAnimation")
+                    .ignoresSafeArea()
+                    .blendMode(.screen)
+                    .offset(y: -250)
+                
+                VStack {
+                    
+             
+                    Text("go ahead")
+                        .font(.system(size: 100))
+                        .foregroundColor(.white)
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 0)
+                    
+                    Spacer()
+                        .frame(height: 100)
+                    
+                    Text("say something nice")
+                        .font(.system(size: 30))
+                        .italic()
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 0)
+                    Spacer()
+                        .frame(height: 5)
+                    
+                    Text("to your freind in Chat")
+                        .font(.system(size: 30, weight: .bold))
+                        .italic()
+                        .foregroundColor(.white)
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.leading)
+                        .padding(.horizontal, 0)
+                    
+                  
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    Text("get a smile back")
+                        .font(.system(size: 45, weight: .bold))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+//                        .italic()
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 0)
+                    
+                    
+                    Spacer()
+                        .frame(height: 30)
+                    
+                    NavigationLink(
+                        destination: HomeView(),
+                        label: {
+                            Text("Ok, take me to Chat")
+                                .fontWeight(.light)
+                                .foregroundColor(Color.black)
+                                .multilineTextAlignment(.center)
+                                .frame(width: 190, height: 30, alignment: .leading)
+                                .font(.system(size: 20))
+                                .padding(10)
+                                .background(Color.gray)
+                                .cornerRadius(15)
+                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                        })
+                               
+                 }
+                      }
+                    
+               }
+            }
+        }
+    
+
+struct DramaView_Previews : PreviewProvider {
+    static var previews: some View {
+        DramaView()
+    }
+}
+
+
---
+++ BestFriends/DramaCentral/HugOut.swift
@@ -0,0 +1,8 @@
+//
+//  HugOut.swift
+//  BestFriends
+//
+//  Created by Social Tech on 4/27/22.
+//
+
+import Foundation
