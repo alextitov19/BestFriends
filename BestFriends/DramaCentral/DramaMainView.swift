19abf395db21abfd861a231287f6a77781e8b1df
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-28 19:04:28 -0400

    drama central

--- BestFriends/DramaCentral/DramaMainView.swift
+++ BestFriends/DramaCentral/DramaMainView.swift
@@ -84,7 +84,7 @@ struct DramaMainView: View {
                             .foregroundColor(ColorManager.pmbc_blue)
                             .shadow(color: .black, radius: 2, x: 0, y: 2)
                         
-                        Text("NOT \nready to talk \n YET ...")
+                        Text("NOT \nready to talk \n YET!")
                             .foregroundColor(.white)
                             .font(.system(size: 17, weight: .bold))
                             .shadow(color: .black, radius: 1, x: 0, y: 1)
