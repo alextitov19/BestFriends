a9707fbabef4a941f8cb7360437541525c11326b
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-28 02:24:00 -0400

    DramaMainView page

---
+++ BestFriends/DramaCentral/DramaMainView.swift
@@ -0,0 +1,201 @@
+//
+//  DramaMainView.swift
+//  BestFriends
+//
+//  Created by Social Tech on 4/28/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct DramaMainView: View {
+    
+//    let user: User
+//
+//    var planet: Int
+//    var mood: Int
+//
+//    var body: some View {
+//        PlanetView(planet: planet, mood: mood)
+//            .scaledToFit()
+//            .frame(width: 120, height: 120)
+//            .glow(color: glowColor())
+//    }
+//
+//    private func glowColor() -> Color {
+//        switch mood {
+//        case 0:
+//            return ColorManager.pmbc_blue
+//        case 1:
+//            return ColorManager.pmbc_green
+//        case 2:
+//            return ColorManager.pmbc_pink
+//        default:
+//            return ColorManager.pmbc_blue
+//        }
+//    }
+//}
+
+//struct PlanetView: View {
+//    let planet: Int
+//    let mood: Int
+//    
+//var body: some View {
+//       Image("planet_\(planet)")
+//           .resizable()
+// }
+//}
+
+//struct PlanetActionsView: View {
+    
+    @EnvironmentObject var sessionManager: SessionManager
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Button(action: {
+                    
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_green)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Build\nProtective\nAtmosphere")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                }
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                Button(action: {
+                    
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_blue)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Customize\nmy \nPlanet")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                }
+                
+            }
+            
+            VStack {
+                Button(action: {
+                    sessionManager.showInvite()
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_blue)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Add\nTrusted\nFriends")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                    
+                }
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                Button(action: {
+                    sessionManager.showDramaView()
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_pink)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Drama \nwith  \n Friend")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                }
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                Button(action: {
+                    sessionManager.showMasterFriendVault()
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_green)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Keeping\nFriends\nSafe")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                  
+                    
+                    }
+                }
+                
+            }
+            
+            VStack {
+                Button(action: {
+                    
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_green)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Daily\nHorizon")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                }
+                
+                Spacer()
+                    .frame(height: 40)
+                
+                Button(action: {
+                    
+//                    trying to link to FriendVault page
+                    sessionManager.showBestFriendMessages()
+                }) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(ColorManager.pmbc_blue)
+                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                        
+                        Text("Friend's \nThourhtful \nMessages")
+                            .foregroundColor(.white)
+                            .font(.system(size: 15, weight: .bold))
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    }
+                }
+                
+            }
+        }
+    }
+    
+}
+
+struct DramaMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        DramaMainView()
+    }
+}
