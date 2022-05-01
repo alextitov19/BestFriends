839c7724220543acb6ef6dc96f5e142c69a323c1
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-04-30 21:33:48 -0400

    send a hug

--- BestFriends/DramaCentral/TimeToTalk.swift
+++ BestFriends/DramaCentral/TimeToTalk.swift
@@ -72,7 +72,7 @@ struct TimeToTalk: View {
                                 VStack {
                                     Link(destination: URL(string: "https://socialtechlabs.com/apoloigize/")!) {
                                
-                                           Text("??? not done yet")
+                                           Text("? send a hug or herart ??")
                                                 .fontWeight(.thin)
                                                 .frame(width: 325, height: 50)
                                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
