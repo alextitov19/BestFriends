3429cf03a70f1961ed0fdc5bcf9244fdc1b9a8f4
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-03 07:48:54 -0400

    setting up push notification for fight page

--- BestFriends/DramaCentral/Push Notifiction Fight Invite/FightPushNoteResponse.swift
+++ BestFriends/DramaCentral/Push Notifiction Fight Invite/FightPushNoteResponse.swift
@@ -27,7 +27,7 @@ struct FightPushNoteResponse: View {
             
             VStack {
            
-                Text("Reaching out First")
+                Text("Thany you for responding")
                 .font(.system(size: 40, weight: .ultraLight))
                 .foregroundColor(Color.purple)
             
@@ -46,7 +46,7 @@ struct FightPushNoteResponse: View {
                 VStack {
                     Link(destination: URL(string: "https://socialtechlabs.com/applink/")!) {
                         
-                        Text("Not Sure What to Say?")
+                        Text("I can Chat NOW")
                             .fontWeight(.thin)
                             .frame(width: 310, height: 50)
                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
@@ -62,11 +62,11 @@ struct FightPushNoteResponse: View {
                     VStack {
                         Link(destination: URL(string: "https://socialtechlabs.com/apoloigize/")!) {
                             
-                            Text("Should I Appologize?")
+                            Text("Let's Set a Time to Meet")
                                 .fontWeight(.thin)
                                 .frame(width: 310, height: 50)
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 30))
+                                .font(.system(size: 27))
                                 .background(ColorManager.purple3)
                                 .cornerRadius(15)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
@@ -80,7 +80,7 @@ struct FightPushNoteResponse: View {
                             NavigationLink(
                                 destination: DramaMainView(),
                                 label: {
-                                    Text("Send 1st Text \nwith Push Notification")
+                                    Text("I Need More Time")
                                         .fontWeight(.thin)
                                         .foregroundColor(Color.white)
                                         .frame(width: 310, height: 70)
