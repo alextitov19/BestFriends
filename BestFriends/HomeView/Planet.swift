c11f4b0e77f9ea1b101f0b26771f6d059e4a22a8
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-06 03:26:55 -0400

    Planet page edits in the circles

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -72,7 +72,7 @@ struct PlanetActionsView: View {
                             .foregroundColor(ColorManager.pmbc_green)
                             .shadow(color: .black, radius: 2, x: 0, y: 2)
                         
-                        Text("Build\nProtective\nAtmosphere")
+                        Text("Protective \nAtmosphere")
                             .foregroundColor(.white)
                             .font(.system(size: 15, weight: .bold))
                             .shadow(color: .black, radius: 1, x: 0, y: 1)
--- BestFriends/ThoughtfulMessages/BestFriendMessages.swift
+++ BestFriends/ThoughtfulMessages/BestFriendMessages.swift
@@ -74,7 +74,7 @@ struct BestFriendMessages: View {
                     Image(systemName: "heart.fill")
                         .resizable()
                         .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
-                        .frame(width: 190, height: 190)
+                        .frame(width: 200, height: 200)
 //                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 3, x: 1, y: 3)
                         .shadow(color: .orange, radius: 50, x: 40, y: 50)
                     
@@ -85,6 +85,7 @@ struct BestFriendMessages: View {
                             .foregroundColor(.orange)
                             .fontWeight(.medium)
                             .multilineTextAlignment(.center)
+                            .shadow(color: .black, radius: 1, x: 0, y: 1)
                    
                         Text("you 'long-tapped' \nin Chat")
                             .font(.system(size: 20))
