5e59e54e3e89902bf267971583a53e4b625b238d
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-10 05:53:47 -0400

    Teen Reality Page

--- BestFriends/DramaCentral/Friend Group Fighting/Step1FG_AcceptedSolutions.swift
+++ BestFriends/DramaCentral/Friend Group Fighting/Step1FG_AcceptedSolutions.swift
@@ -30,40 +30,50 @@ struct Step1FG_SuggestedSolutions: View {
             
             VStack {
           
-                Text("We Think Differently")
-                    .font(.system(size: 30))
-                    .foregroundColor(.blue)
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
+           
                 
                 Spacer()
                     .frame(height: 10)
                 
-                Text("Differently")
+                Text("Teen Reality")
                     .font(.system(size: 70))
                     .foregroundColor(.white)
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     
                 Spacer()
-                    .frame(height: 45)
+                    .frame(height: 25)
                 
-                Text("Teen Reality")
-                    .font(.system(size: 35))
+               
+                Text("Teens say they dislike drama")
+                    .font(.system(size: 30))
                     .fontWeight(.light)
                     .foregroundColor(Color.blue)
                 
+                Text("but some in Friend Groups are:")
+                    .font(.system(size: 26))
+                    .fontWeight(.light)
+                    .foregroundColor(Color.blue)
+               
                 Spacer()
                     .frame(height: 20)
                 
-                Text("- teens talk all the time to group members \n- know exactly what is going on with everyone \n- they always take sides \n- they talk to each other about other's fights \n- they express their opinions \n- exactly opposite what professionals say to do ")
-                    .font(.system(size: 18))
+            VStack {
+                Text("- drawn to it \n- get involved in others fights \n- commerizerate playing both sides \n- express their opinions \n- play two against one \n- talk behind others backs \n- win friends at the cost of others")
+                    .font(.system(size: 24))
                     .fontWeight(.light)
                     .foregroundColor(Color.white)
      
-                VStack {
-                    Spacer()
-                        .frame(height: 45)
+                Text("")
+                    .font(.system(size: 17))
+                    .fontWeight(.light)
+                    .foregroundColor(Color.white)
+                
+                
+                
+            VStack {
+                Spacer()
+                    .frame(height: 45)
                     
                     NavigationLink(
                         destination: Step2FG_NewSolutions(),
@@ -77,31 +87,23 @@ struct Step1FG_SuggestedSolutions: View {
                                 .cornerRadius(15)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                       
-                        
-                        
                         })
-                
-//
-//                Button(action: {
-//                    sessionManager.showDramaMainView()
-//                }, label: {
-//                    Text("NEXT")
-//                        .fontWeight(.thin)
-//                        .frame(width: 100, height: 40)
-//                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                        .font(.system(size: 30))
-//                        .background(ColorManager.purple3)
-//                        .cornerRadius(15)
-//                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//                })
-//                .padding(.vertical, 40)
-                
+
             }
+                Spacer()
+                    .frame(height: 25)
+                
+                Text("We hope for something \nbetter")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                
         }
     }
 }
 }
-
+}
 
 
 
