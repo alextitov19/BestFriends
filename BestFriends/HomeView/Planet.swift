55e80ba0366fb826c6ab656ade2e8ae426474244
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-11 05:49:44 -0400

    planet page

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -148,7 +148,7 @@ struct PlanetActionsView: View {
                        
                         VStack {
                             
-                            Text("Settings, \nDemo, \nMy Planet")
+                            Text("My Planet, \nSettings, \n & Demo")
                                 .foregroundColor(.white)
                                 .font(.system(size: 15, weight: .bold))
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
