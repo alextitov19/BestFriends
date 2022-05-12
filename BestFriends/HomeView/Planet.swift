734db115d9b95f75af87efaa446414c1a18c48de
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-12 19:10:29 -0400

    planet edit

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -71,7 +71,7 @@ struct PlanetActionsView: View {
                             .foregroundColor(ColorManager.pmbc_blue)
                             .shadow(color: .black, radius: 2, x: 0, y: 2)
                         VStack {
-                            Text("Protective \nAtmosphere")
+                            Text("Safe \nAtmosphere")
                                 .foregroundColor(.white)
                                 .font(.system(size: 15, weight: .bold))
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
