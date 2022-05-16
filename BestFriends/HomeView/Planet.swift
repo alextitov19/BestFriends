e4fef44d1afe6ccfa872b8f269f8da9341394ae0
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-16 06:59:03 -0400

    planet page edits

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -78,7 +78,7 @@ struct PlanetActionsView: View {
                       
                             Text("SUPPORTING")
                                 .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
+                                .font(.system(size: 14, weight: .bold))
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
                             Text("me")
                                 .foregroundColor(.white)
@@ -113,7 +113,7 @@ struct PlanetActionsView: View {
                       
                             Text("SUPPORTING")
                                 .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
+                                .font(.system(size: 14, weight: .bold))
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
                             Text("friends")
                                 .foregroundColor(.white)
