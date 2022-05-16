436a991e89c34c60d71635d85dfe05ec20bc9aba
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-16 07:01:22 -0400

    planet

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -184,7 +184,7 @@ struct PlanetActionsView: View {
                         VStack {
                             Text("Huge \nFight")
                                 .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
+                                .font(.system(size: 16, weight: .bold))
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
                         }
                     }
