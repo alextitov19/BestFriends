6e0cc736925d059b809852a01c00a3fefe2c608d
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-18 17:13:20 -0400

    clean up code

--- BestFriends/FriendVault/FriendValutTrackMoods.swift
+++ BestFriends/FriendVault/FriendValutTrackMoods.swift
@@ -14,55 +14,54 @@ struct FriendVaultTrackMoods: View {
     
     var body: some View {
         ZStack {
-          
+            
             ColorManager.purple2
                 .ignoresSafeArea()
             
             VStack {
-      
-               
+                
+                
                 
                 Text("Tracking My Moods")
                     .font(.system(size: 40))
                     .foregroundColor(.white)
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
-                 
+                
                 Spacer()
                     .frame(height: 30)
-                 
+                
                 Rectangle()
                     .frame(width:325, height: 150)
                     .cornerRadius(15)
-                   .foregroundColor(ColorManager.purple3)
+                    .foregroundColor(ColorManager.purple3)
                 
-
+                
                 
                 Image("My feeling-1")
-         
-                    Spacer()
-                        .frame(height: 60)
-                    
-                    NavigationLink(
-                        destination: DramaCentralView(),
-                        label: {
-                            Text("NEXT")
-                                .fontWeight(.thin)
-                                .frame(width: 100, height: 40)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 30))
-                                .background(ColorManager.purple3)
-                                .cornerRadius(15)
-                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        })
-             
                 
-              
-             }
+                Spacer()
+                    .frame(height: 60)
+                
+                NavigationLink(
+                    destination: DramaCentralView(),
+                    label: {
+                        Text("NEXT")
+                            .fontWeight(.thin)
+                            .frame(width: 100, height: 40)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .font(.system(size: 30))
+                            .background(ColorManager.purple3)
+                            .cornerRadius(15)
+                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    })
+                
+                
             }
-            
         }
+        
     }
+}
 
 
 
