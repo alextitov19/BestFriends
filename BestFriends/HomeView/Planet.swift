f42a973113c4e776c253312d5bc22fdfee0fc521
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-05-20 08:28:35 -0400

    Planet - took out the 'i' icon. Don;t need now with changes to landing page

--- BestFriends/HomeView/Planet.swift
+++ BestFriends/HomeView/Planet.swift
@@ -63,218 +63,218 @@ struct PlanetActionsView: View {
     var body: some View {
         
         VStack {
-//             MARK: need Setting icon. Takes to OurMission page
-            NavigationLink(destination: OurMission(),
-                           label: {
-            Image("Exclamation Mark")
-                .resizable()
-                .frame(width: 50, height: 50)
-                .scaledToFit()
-                .colorInvert()
-                .rotationEffect(Angle(degrees: 180))
-//        }
-        
-        
-        HStack {
-            VStack {
-                NavigationLink(destination: BestFriendMessages(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres),
-                               label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.purple3)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        VStack {
-                            Text("my")
-                                .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                      
-                            Text("SMILES")
-                                .foregroundColor(.white)
-                                .font(.system(size: 14, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-//                            Text("me")
-//                                .foregroundColor(.white)
-//                                .font(.system(size: 15, weight: .bold))
-//                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-//
-                        
-                        
+            ////             MARK: need Setting icon. Takes to OurMission page
+            //            NavigationLink(destination: OurMission(),
+            //                           label: {
+            //            Image("Exclamation Mark")
+            //                .resizable()
+            //                .frame(width: 50, height: 50)
+            //                .scaledToFit()
+            //                .colorInvert()
+            //                .rotationEffect(Angle(degrees: 180))
+            ////        }
+            //
+            //
+            HStack {
+                VStack {
+                    NavigationLink(destination: BestFriendMessages(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres),
+                                   label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.purple3)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            VStack {
+                                Text("my")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 15, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                                Text("SMILES")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 14, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                //                            Text("me")
+                                //                                .foregroundColor(.white)
+                                //                                .font(.system(size: 15, weight: .bold))
+                                //                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                //
+                                
+                                
+                            }
                         }
-                    }
-                })
-                
-                Spacer()
-                    .frame(height: 40)
-                
-                NavigationLink(destination: Friend1VaultPractice(user: friends[0], friend: user, friendAtmosphere: atmosphere),
-                                label: {
-             
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.purple3)
-                            .foregroundColor(.pink)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
+                    })
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    NavigationLink(destination: Friend1VaultPractice(user: friends[0], friend: user, friendAtmosphere: atmosphere),
+                                   label: {
                         
-                        VStack {
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.purple3)
+                                .foregroundColor(.pink)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
                             
-                 
-//                            Text("Support")
-//                                .foregroundColor(.white)
-//                                .font(.system(size: 15, weight: .bold))
-//                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-//
-                            Text("my")
-                                .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-
-                            Text("Friends")
-                                .foregroundColor(.white)
-                                .font(.system(size: 15, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                        
+                            VStack {
+                                
+                                
+                                //                            Text("Support")
+                                //                                .foregroundColor(.white)
+                                //                                .font(.system(size: 15, weight: .bold))
+                                //                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                //
+                                Text("my")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 15, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                                Text("Friends")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 15, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                            }
                         }
                     }
+                    )}
+                
+                VStack {
+                    Button(action: {
+                        sessionManager.showInvite()
+                    }) {
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.pmbc_green)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            
+                            VStack {
+                                Text("+")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 30, weight: .regular))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                Text("Friends")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 17, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            }
+                        }
+                    }
+                    
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    NavigationLink(destination: OurMission(),
+                                   label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 120, height: 120)
+                                .foregroundColor(ColorManager.grey1)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            
+                            VStack {
+                                Text("Settings")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 13, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            }
+                        }
+                    })
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    NavigationLink(destination: WhoFighting(),
+                                   label: {
+                        
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.pmbc_blue)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            
+                            VStack {
+                                Text("Huge Fight")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                Text("friend group \nparents \nBFF")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            }
+                        }
+                    })
+                    
                 }
-                )}
-            
-            VStack {
-                Button(action: {
-                    sessionManager.showInvite()
-                }) {
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.pmbc_green)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        
-                        VStack {
-                            Text("+")
-                                .foregroundColor(.white)
-                                .font(.system(size: 30, weight: .regular))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            Text("Friends")
-                                .foregroundColor(.white)
-                                .font(.system(size: 17, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                
+                
+                
+                
+                VStack {
+                    NavigationLink(destination: AtmosphereMain2(user: user, atmosphere: atmosphere, friends: friends),
+                                   label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.purple2)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            VStack {
+                                Text("Atmosphere")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                                Text("my mood")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            }
                         }
-                    }
+                    })
+                    
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    
+                    NavigationLink(destination: BestFriendMessages(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres),
+                                   label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(ColorManager.purple2)
+                                .shadow(color: .black, radius: 2, x: 0, y: 2)
+                            VStack {
+                                
+                                
+                                Text("Horizons")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 16, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                Text("dreams with friends")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 12, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                            }
+                        }
+                    })
+                    
                 }
                 
-           
-                Spacer()
-                    .frame(height: 40)
-                
-                NavigationLink(destination: OurMission(),
-                               label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 120, height: 120)
-                            .foregroundColor(ColorManager.grey1)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        
-                        VStack {
-                            Text("Settings")
-                                .foregroundColor(.white)
-                                .font(.system(size: 13, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                        }
-                    }
-                })
-                
-                Spacer()
-                    .frame(height: 40)
-
-                NavigationLink(destination: WhoFighting(),
-                              label: {
-
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.pmbc_blue)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        
-                        VStack {
-                            Text("Huge Fight")
-                                .foregroundColor(.white)
-                                .font(.system(size: 16, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            Text("friend group \nparents \nBFF")
-                                .foregroundColor(.white)
-                                .font(.system(size: 12, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                        }
-                    }
-                })
-          
             }
             
-            
-         
-            
-           VStack {
-               NavigationLink(destination: AtmosphereMain2(user: user, atmosphere: atmosphere, friends: friends),
-                               label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.purple2)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        VStack {
-                            Text("Atmosphere")
-                                .foregroundColor(.white)
-                                .font(.system(size: 16, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            
-                            Text("my mood")
-                                .foregroundColor(.white)
-                                .font(.system(size: 12, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                        }
-                    }
-                })
-            
-            
-            Spacer()
-                    .frame(height: 40)
-                
-     
-                NavigationLink(destination: BestFriendMessages(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres),
-                              label: {
-                    ZStack {
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(ColorManager.purple2)
-                            .shadow(color: .black, radius: 2, x: 0, y: 2)
-                        VStack {
-                          
-                            
-                            Text("Horizons")
-                                .foregroundColor(.white)
-                                .font(.system(size: 16, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            Text("dreams with friends")
-                                .foregroundColor(.white)
-                                .font(.system(size: 12, weight: .bold))
-                                .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            
-                        }
-                    }
-                })
-              
-            }
- 
         }
-     
+        
     }
-      
-)}
-
+    
 }
-}
+//}
 
 //
 //
