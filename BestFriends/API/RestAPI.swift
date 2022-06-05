cf988d784f0a013d287956ef05b94994cd2ca504
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2022-06-05 16:26:46 -0700

    build 5

--- BestFriends/API/RestAPI.swift
+++ BestFriends/API/RestAPI.swift
@@ -12,11 +12,11 @@ import FirebaseMessaging
 class RestApi {
     var helper: Helper
     var userId: String?
-//    let API_URL = "http://54.226.29.220:8080/api/v1/services"
-//    let WS_URL = "ws://54.226.29.220:8080/api/v1/services"
+    let API_URL = "http://54.226.29.220:8080/api/v1/services"
+    let WS_URL = "ws://54.226.29.220:8080/api/v1/services"
 
-    let API_URL = "http://localhost:8080/api/v1/services"
-    let WS_URL = "ws://localhost:8080/api/v1/services"
+//    let API_URL = "http://localhost:8080/api/v1/services"
+//    let WS_URL = "ws://localhost:8080/api/v1/services"
 
     public static var instance = RestApi()
     
