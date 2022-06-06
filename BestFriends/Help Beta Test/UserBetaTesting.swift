f11c8c1c285f66d2975ee012bf3dd44c2ff7fdc6
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-06-06 17:06:32 -0400

    beta test page

--- BestFriends/Help Beta Test/UserBetaTesting.swift
+++ BestFriends/Help Beta Test/UserBetaTesting.swift
@@ -44,14 +44,14 @@ struct UserBetaTesting: View {
                            .rotationEffect(Angle(degrees: 180))
                     }
                     
-                    Spacer()
+//                    Spacer()
                     
-                    Text("Asking For Your Help")
+                    Text("Asking for Help")
                        
               
-                        .font(.system(size: 40))
+                        .font(.system(size: 50))
                         .fontWeight(.thin)
-                        .foregroundColor(.purple)
+                        .foregroundColor(.gray)
                         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                 }
                     Spacer()
@@ -70,52 +70,56 @@ struct UserBetaTesting: View {
                             .background(ColorManager.purple3)
                             .cornerRadius(15)
                             .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-
-                        Image("arrowRight")
-                          
-                            .resizable() .foregroundColor(.green)
-                            .frame(width: 25, height: 25)
-                           
-                            .scaledToFit()
-                           
                     }
                 }
-                
-
-//
+   
                 Spacer()
                       .frame(height: 35)
                
-                Text("The BestFriends team sincerely hopes to make life a little better for teens worldwide. \n\nBut, we need your help. \n\nSignUp for an interview with one of our teen staff members. \n\nTake a 3 question survey.")
+                Text("The BestFriends team sincerely hopes to make life a little better for teens worldwide. \n\nBut, we need your help. \n\nSignUp for an interview with one of our teen staff members.")
                     .font(.system(size: 23))
                     .fontWeight(.thin)
                     .italic()
                     .offset(x: -3)
                     .foregroundColor(.purple)
-                    .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                    .padding(25)
+
                 Spacer()
                       .frame(height: 25)
                 
-                
-                Text("Thank you for helping us \nmake BestFriends SPECIAL")
-                    .font(.system(size: 28))
-                    .fontWeight(.light)
-                    .italic()
-                    .offset(x: -3)
-                    .foregroundColor(.gray)
-                    .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                Link(destination: URL(string: "https://socialtechlabs.com/beta-tester-_-bestfriends-app/")!) {
                     
-                
-                Spacer()
+                    HStack {
+                        Text("Report 'issues' in BestFriends")
+                            .fontWeight(.thin)
+                            .frame(width: 370, height: 32)
+                            .foregroundColor(.pink)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .font(.system(size: 30))
+                            .background(ColorManager.purple3)
+                            .cornerRadius(15)
+                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+
+//                Text("Thank you for helping us \nmake BestFriends SPECIAL")
+//                    .font(.system(size: 28))
+//                    .fontWeight(.light)
+//                    .italic()
+//                    .offset(x: -3)
+//                    .foregroundColor(.gray)
+//                    .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+//
+//
+//                Spacer()
             }
-            .padding(40)
+            .padding(20)
             .navigationBarHidden(true)
         }
     }
     
 }
         
-        
+    }
+}
     
 
 struct UserBetaTesting_Previews : PreviewProvider {
@@ -125,17 +129,3 @@ struct UserBetaTesting_Previews : PreviewProvider {
 }
 
 
-//
-//NavigationLink(
-//    destination: FG_VideoCall(),
-//    label: {
-//        Text("NEXT")
-//            .fontWeight(.thin)
-//            .frame(width: 100, height: 40)
-//            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//            .font(.system(size: 30))
-//            .background(ColorManager.purple3)
-//            .cornerRadius(15)
-//            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//
-//    })
