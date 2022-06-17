71608311fb2cae1956f1db327aa2ff9a2aca5e5d
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-06-17 19:22:33 -0400

    my planet changes

--- BestFriends/HomeView/PlanetMain/Planet.swift
+++ BestFriends/HomeView/PlanetMain/Planet.swift
@@ -174,14 +174,19 @@ struct PlanetActionsView: View {
                                .opacity(0.65)
                             
                             VStack {
-                                Text("My")
+                                Text("Friends")
                                     .foregroundColor(.white)
                                     .font(.system(size: 16, weight: .bold))
                                     .shadow(color: .black, radius: 1, x: 0, y: 1)
                                 
-                                Text("Planet")
+                                Text("Celebrating")
                                     .foregroundColor(.white)
-                                    .font(.system(size: 17, weight: .bold))
+                                    .font(.system(size: 16, weight: .bold))
+                                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+                                
+                                Text("Friendships")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 16, weight: .bold))
                                     .shadow(color: .black, radius: 1, x: 0, y: 1)
                             }
                         }
