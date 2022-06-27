a826636a64f227f4824c940ac70fd90b7868fa4a
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-06-27 13:50:25 -0400

    Updating MyRoomPreload page

--- BestFriends/MY ROOM/HIDEOUTS/MyRoomPreload.swift
+++ BestFriends/MY ROOM/HIDEOUTS/MyRoomPreload.swift
@@ -61,19 +61,32 @@ struct MyRoomPreload: View {
                 Spacer()
                     .frame(height: 50)
                 
-                NavigationLink(
-                    destination: EmptyView(),
-                    label: {
-                        Text("My Room")
-                            .fontWeight(.thin)
-                            .frame(width: 300, height: 40)
-                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                            .font(.system(size: 30))
-                            .background(ColorManager.purple3)
-                            .cornerRadius(15)
-                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        
-                    })
+                
+                
+                NavigationLink(destination: HideoutsView(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres:  friendAtmospheres), label: {
+                    Text("My Room")
+                        .fontWeight(.thin)
+                        .frame(width: 200, height: 40)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .font(.system(size: 30))
+                        .background(ColorManager.purple3)
+                        .cornerRadius(15)
+                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                })
+                
+//                NavigationLink(
+//                    destination: EmptyView(),
+//                    label: {
+//                        Text("My Room")
+//                            .fontWeight(.thin)
+//                            .frame(width: 300, height: 40)
+//                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                            .font(.system(size: 30))
+//                            .background(ColorManager.purple3)
+//                            .cornerRadius(15)
+//                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//
+//                    })
                 VStack {
                     Spacer()
                         .frame(height: 30)
@@ -128,18 +141,7 @@ struct MyRoomPreload: View {
                         
                         
                         
-                        
-                        
-                        NavigationLink(destination: HideoutsView(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres:  friendAtmospheres), label: {
-                            Text("NEXT")
-                                .fontWeight(.thin)
-                                .frame(width: 100, height: 40)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 30))
-                                .background(ColorManager.purple3)
-                                .cornerRadius(15)
-                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        })
+                    
                     }
                     
                     
