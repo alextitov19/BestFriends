099a5abebc9fd37d45a37ed89b222f1f14eea851
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-07-12 00:36:17 -0400

    home button

--- BestFriends/DramaCentral/Fight w Friend/DramaMainView.swift
+++ BestFriends/DramaCentral/Fight w Friend/DramaMainView.swift
@@ -197,7 +197,7 @@ struct DramaMainView: View {
                     label: {
                         Text("Home")
                             .fontWeight(.thin)
-                            .frame(width: 200, height: 40)
+                            .frame(width: 100, height: 40)
                             .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                             .font(.system(size: 30))
                             .background(ColorManager.purple3)
