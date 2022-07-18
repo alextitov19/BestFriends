971ae02acc37e5296e040a11068e666b07750c60
christy260 <43527129+christy260@users.noreply.github.com>
2022-07-17 20:35:15 -0400

    HorizonSetGoal.swift
    and modify HorizonGoalChoices.swift to fit smaller screen

--- BestFriends/Horizons/HorizonGoalChoices.swift
+++ BestFriends/Horizons/HorizonGoalChoices.swift
@@ -19,190 +19,198 @@ struct HorizonGoalChoices: View {
                 .ignoresSafeArea()
                 .scaledToFill()
             
-            VStack
+            HStack
             {
-                HStack
-                {
-                    Image(systemName: "wrench")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                      
-                    Text("Custom")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                        
-                        
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "bubble.left")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Check on my friends")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "person.2.fill")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Make plan with roommate")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "music.note.list")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Create a new Playlist for my friend's mood")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "cloud")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Set up my Dreams")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "tshirt")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Declutter my closet")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "hands.sparkles")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
-                    
-                    Text("Volunteer for a Day")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
             
-                HStack
+                VStack
                 {
-                    Image(systemName: "lightbulb")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
+                    HStack
+                    {
+                        Image(systemName: "wrench")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                          
+                        Text("Custom")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                            
+                            
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
                     
-                    Text("Try something new")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
+                    HStack
+                    {
+                        Image(systemName: "bubble.left")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Check on my friends")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                    
+                    HStack
+                    {
+                        Image(systemName: "person.2.fill")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Make plan with roommate")
+                            .font(.system(size: 15))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                    
+                    HStack
+                    {
+                        Image(systemName: "music.note.list")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 55, height: 57)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("New Playlist for my friend's mood")
+                            .font(.system(size: 14))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                    
+                    HStack
+                    {
+                        Image(systemName: "cloud")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Set up my Dreams")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                }
+                VStack
+                {
+                    HStack
+                    {
+                        Image(systemName: "tshirt")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Declutter my closet")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                    
+                    HStack
+                    {
+                        Image(systemName: "hands.sparkles")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Volunteer for a Day")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                
+                    HStack
+                    {
+                        Image(systemName: "lightbulb")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 50)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Try something new")
+                            .font(.system(size: 16))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
 
-                
-                HStack
-                {
-                    Image(systemName: "book")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
-                        
                     
-                    Text("Read my favorite book")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-                
-                HStack
-                {
-                    Image(systemName: "sparkles.tv")
-                        .resizable()
-                        .foregroundColor(.black)
-                        .background(.white)
-                        .frame(width: 50, height: 50)
-                        .padding(5)
-                        .border(Color.white,width:5)
+                    HStack
+                    {
+                        Image(systemName: "book")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 53)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
                         
+                        Text("Read my favorite book")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
                     
-                    Text("Watch a new movie")
-                        .font(.system(size: 20))
-                        .frame(width: 200,alignment: .leading)
-                }//HStack
-                .background(.blue)
-                .cornerRadius(15)
-            }//VStack
+                    HStack
+                    {
+                        Image(systemName: "sparkles.tv")
+                            .resizable()
+                            .foregroundColor(.black)
+                            .background(.white)
+                            .frame(width: 50, height: 53)
+                            .padding(5)
+                            .border(Color.white,width:5)
+                            
+                        
+                        Text("Watch a new movie")
+                            .font(.system(size: 17))
+                            .frame(width: 80,alignment: .leading)
+                    }//HStack
+                    .background(.blue)
+                    .cornerRadius(15)
+                }//VStack
+                
+            }//HStack
+           
         }//ZStack
 //        }//UIScrollView
     }
@@ -211,5 +219,6 @@ struct HorizonGoalChoices: View {
 struct HorizonGoalChoices_Previews: PreviewProvider {
     static var previews: some View {
         HorizonGoalChoices()
+            .previewDevice("iPod touch (7th generation)")
     }
 }
---
+++ BestFriends/Horizons/HorizonSetGoal.swift
@@ -0,0 +1,84 @@
+//
+//  HorizonSetGoal.swift
+//  BestFriends
+//
+//  Created by Christine Maldonado on 7/17/22.
+//
+
+import SwiftUI
+
+struct HorizonSetGoal: View {
+    
+    @State private var date = Date()
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+
+    var body: some View {
+        ZStack
+        {
+            Image("purpleBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+            
+            VStack
+            {
+                Text("Title")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                DatePicker(
+                    "Start Date",
+                    selection: $date,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(.graphical)
+                .background(.white)
+                .accentColor(ColorManager.purple3)
+                
+                HStack
+                {
+                    Text("Start Time")
+                        .fontWeight(.bold)
+                    
+                    DatePicker("", selection: $startDate, displayedComponents: .hourAndMinute).colorInvert()
+                        .colorMultiply(Color.white)
+                            .labelsHidden()
+                            
+                    Text(">")
+                }
+                .foregroundColor(.white)
+//                .padding()
+                
+                HStack
+                {
+                    Text("End Time")
+                        .fontWeight(.bold)
+                    
+                    DatePicker("", selection: $endDate, displayedComponents: .hourAndMinute).colorInvert()
+                        .colorMultiply(Color.white)
+                            .labelsHidden()
+                            
+                    Text(">")
+                }
+                .foregroundColor(.white)
+                
+                Text("  Save  ")
+                    .font(.title)
+                    .background(ColorManager.purple4)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                
+                Spacer()
+
+            }//VStack
+        }//ZStack
+    }//body
+}//struct
+
+struct HorizonSetGoal_Previews: PreviewProvider {
+    static var previews: some View {
+        HorizonSetGoal()
+            .previewDevice("iPod touch (7th generation)")
+    }
+}
