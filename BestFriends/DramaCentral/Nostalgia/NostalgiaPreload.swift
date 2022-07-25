20fae2c1f3180083b9081a127b99df316fff1aa8
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2022-07-25 10:59:26 -0700

    nost

--- BestFriends/DramaCentral/Nostalgia/NostalgiaPreload.swift
+++ BestFriends/DramaCentral/Nostalgia/NostalgiaPreload.swift
@@ -14,19 +14,9 @@ struct NostalgiaPreload: View {
     
     var body: some View {
         ZStack {
-            
-//            Image("purpleBackground")
-//                .resizable()
-//                .ignoresSafeArea()
-//                .scaledToFill()
-
-            
-                        ColorManager.grey4
-                            .ignoresSafeArea()
-                            .onAppear()
-
-             
-         
+            ColorManager.grey4
+                .ignoresSafeArea()
+                .onAppear()
             
             VStack {
                 
@@ -35,15 +25,9 @@ struct NostalgiaPreload: View {
                     .fontWeight(.ultraLight)
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color.purple)
-             
-//                Text("N O S T A L S I A")
-//                    .font(.system(size: 45))
-//                    .fontWeight(.ultraLight)
-//                    .multilineTextAlignment(.center)
-//                    .foregroundColor(Color.purple)
                 
                 Spacer()
-                .frame(height: 15)
+                    .frame(height: 15)
                 
                 Text("OK, you're not a teen anymore")
                     .font(.system(size: 25))
@@ -56,93 +40,73 @@ struct NostalgiaPreload: View {
                     .multilineTextAlignment(.center)
                     .foregroundColor(Color.white)
                 
-                
-                    
                 VStack {
-               
                     Spacer()
-                    .frame(height: 50)
+                        .frame(height: 50)
                     
                     Text("Head to Chat")
                         .font(.system(size: 40))
                         .fontWeight(.thin)
                         .foregroundColor(Color.purple)
                         .kerning(10)
-           
-                    Spacer()
-                    .frame(height: 20)
                     
-                Text("- Send a song you listened to together")
-                    .font(.system(size: 20))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white)
-                  
                     Spacer()
-                    .frame(height: 10)
+                        .frame(height: 20)
+                    
+                    Text("- Send a song you listened to together")
+                        .font(.system(size: 20))
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.white)
+                    
+                    Spacer()
+                        .frame(height: 10)
                 }
                 
-            VStack {
-                Text("- let youself be vounrable, \nsay something nice")
-                    .font(.system(size: 20))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white)
-              
-                Spacer()
-                .frame(height: 10)
-                
-                Text("-Scroll through your photo gallery \nand find a couple of special pics")
-                    .font(.system(size: 20))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white)
-                
-                Spacer()
-                .frame(height: 10)
-                
-                Text("")
-                    .font(.system(size: 25))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.white)
-                
-               
-         
-                Spacer()
-                    .frame(height: 20)
-                
-//                NavigationLink(
-//                    destination: EmptyView(),
-//                    label: {
-//                        Text("CHAT")
-//                            .fontWeight(.thin)
-//                            .frame(width: 100, height: 40)
-//                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                            .font(.system(size: 30))
-//                            .background(ColorManager.purple3)
-//                            .cornerRadius(15)
-//                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//
-//
-//                    })
-                
-                Spacer()
-                .frame(height: 15)
-                
-                Text("*Even if you friend isn't on BestFriends - \nDo it on your Phone!")
-                    .font(.system(size: 15))
-                    .italic()
-                    .fontWeight(.light)
-                    .foregroundColor(Color.blue)
-    
-                
-                
+                VStack {
+                    Text("- let youself be vounrable, \nsay something nice")
+                        .font(.system(size: 20))
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.white)
+                    
+                    Spacer()
+                        .frame(height: 10)
+                    
+                    Text("-Scroll through your photo gallery \nand find a couple of special pics")
+                        .font(.system(size: 20))
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.white)
+                    
+                    Spacer()
+                        .frame(height: 10)
+                    
+                    Text("")
+                        .font(.system(size: 25))
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.white)
+                    
+                    
+                    
+                    Spacer()
+                        .frame(height: 20)
+                    
+                    Spacer()
+                        .frame(height: 15)
+                    
+                    Text("*Even if you friend isn't on BestFriends - \nDo it on your Phone!")
+                        .font(.system(size: 15))
+                        .italic()
+                        .fontWeight(.light)
+                        .foregroundColor(Color.blue)
+                    
+                }
             }
+            
         }
-        
     }
-}
 }
 
 
