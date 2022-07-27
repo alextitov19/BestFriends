a7f5a7cbdc93a3660c1eca7162ef87908e0eb221
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-07-27 07:24:53 -0400

    Update FightWithParents.swift

--- BestFriends/DramaCentral/FightWithParents/FightWithParents.swift
+++ BestFriends/DramaCentral/FightWithParents/FightWithParents.swift
@@ -16,10 +16,13 @@ struct FightWithParents: View {
     var body: some View {
         ZStack {
             
-            
-                        ColorManager.purple4
-                            .ignoresSafeArea()
-                            .onAppear()
+            Image("blueBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+//                        ColorManager.purple4
+//                            .ignoresSafeArea()
+//                            .onAppear()
                         
              
             
@@ -40,7 +43,7 @@ struct FightWithParents: View {
                 Text("Fighting with")
                     .font(.system(size: 40))
                     .fontWeight(.ultraLight)
-                    .foregroundColor(Color.purple)
+                    .foregroundColor(Color.blue)
                 
                 Spacer()
                     .frame(height: 10)
@@ -48,41 +51,47 @@ struct FightWithParents: View {
                 Text("P A R E N T S")
                     .font(.system(size: 60))
                     .fontWeight(.ultraLight)
-                    .foregroundColor(Color.purple)
-                
-                Spacer()
-                    .frame(height: 20)
-                
-                Text("This is a tough one to fix")
-                    .font(.system(size: 25))
-                    .fontWeight(.light)
-                    .foregroundColor(Color.white)
-               
-            
-      
+                    .foregroundColor(Color.blue)
+   
                 VStack {
                     Spacer()
-                        .frame(height: 70)
+                        .frame(height: 15)
                     
-                    Text("Please be patient,")
-                        .font(.system(size: 40))
+                    Text("Family drama may be normal, but it still sucks. We have some suggestions to help handle these conflicts.")
+                        .font(.system(size: 15))
                         .fontWeight(.ultraLight)
                         .foregroundColor(Color.white)
+                        .padding()
                     
-                    Text("we're working on it")
-                        .font(.system(size: 40))
+                    Text("Parents Checking my Phone: If the phone was a gift to you by your parents before you were an adult, since you are now an adult you have a right to privacy in all your affairs, including your mobile phone.")
+                        .font(.system(size: 15))
                         .fontWeight(.ultraLight)
                         .foregroundColor(Color.white)
+                        .padding()
+                    
+                    Text("Why do I hate my parents? Toxic behaviors, abuse, neglect, or conflict are just a few factors that can lead to feelings of animosity.")
+                        .font(.system(size: 15))
+                        .fontWeight(.ultraLight)
+                        .foregroundColor(Color.white)
+                        .padding()
+                    Text("Can my parents take my phone if I paid for it? As long as you're under age 18 and living with your parents, you're under their authority; they can take away your cell phone if they want, and the law gives you no recourse to get it back.")
+                        .font(.system(size: 15))
+                        .fontWeight(.ultraLight)
+                        .foregroundColor(Color.white)
+                        .padding()
+                   
+                  
+                   
                     
                     Spacer()
-                        .frame(height: 25)
+                        .frame(height: 15)
                     
                    
                     
                     NavigationLink(
                         destination: EmptyView(),
                         label: {
-                            Text("Comming Fall 2022")
+                            Text("NEXT")
                                 .fontWeight(.thin)
                                 .frame(width: 310, height: 40)
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
