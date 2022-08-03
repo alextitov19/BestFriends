ecb01860bbfba1e61279e181103490f8ad79b44e
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-08-02 21:38:42 -0400

    get premium

--- BestFriends/HomeView/IntuitiveView.swift
+++ BestFriends/HomeView/IntuitiveView.swift
@@ -143,24 +143,20 @@ struct IntuitiveView: View {
                   
                     })
                 
-//
-//                Spacer()
-//                .frame(height: 15)
-//
-//                NavigationLink(
-//                    destination: BuiltByTeens2(),
-//                    label: {
-//                        Text("Team / Demo / Prototypes")
-//                            .fontWeight(.thin)
-//                            .frame(width: 300, height: 40)
-//                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                            .font(.system(size: 25))
-//                            .background(ColorManager.purple3)
-//                            .cornerRadius(15)
-//                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//
-//                    })
-              
+                NavigationLink(
+                    destination: Subscriptions(),
+                    label: {
+                        Text("Get PREMIUM")
+                            .fontWeight(.thin)
+                            .frame(width: 150, height: 40)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .font(.system(size: 25))
+                            .background(ColorManager.purple3)
+                            .cornerRadius(15)
+                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                  
+                    })
+                
 
                 
                 Spacer()
