38da55bcbceb62de1c78e1871bde41f6f3a14975
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2022-08-05 12:23:26 -0700

    Update HorizonInviteFriends.swift

--- BestFriends/Horizons/HorizonInviteFriends.swift
+++ BestFriends/Horizons/HorizonInviteFriends.swift
@@ -14,7 +14,8 @@ struct HorizonInviteFriends: View {
     
     @State private var mood: Int = -1
     @State private var summary = ""
-    @State private var sharedWith: [String] = []
+    @State private var invitedFriends: [String] = []
+    @State private var sharedFriends: [String] = []
     @State private var colorChangeTap: String = ""
     
     var body: some View {
@@ -39,84 +40,66 @@ struct HorizonInviteFriends: View {
                 }//VStack
                 
                 HStack {
-                    if friends.count > 1 {
-                        Button(action: {
-                            defaultMessageButtonTapped(defaultMessage: "SHARE")
-                            shareMood()
-                        },
-                               label: {
-                            Text("ALL")
-                                .fontWeight(.bold)
-                                .frame(width: 100, height: 30)
-                            
-                                .background(colorChangeTap == "SHARE" ? ColorManager.grey3 : ColorManager.purple3)
-                            
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 15))
-        //                                    .background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
-                                .cornerRadius(25)
-                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        })
-                    }
+                   
                     if friends.count > 0 {
-                        RectView(user: user, friend: friends[0], color: sharedWith.contains(friends[0].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[0], color: sharedFriends.contains(friends[0].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[0].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[0].id }
+                                if sharedFriends.contains(friends[0].id) {
+                                    sharedFriends = sharedFriends.filter { $0 != friends[0].id }
                                 } else {
-                                    sharedWith.append(friends[0].id)
+                                    sharedFriends.append(friends[0].id)
                                 }
-                                print(sharedWith)
+                                print(sharedFriends)
                             })
                     }
                     
                     if friends.count > 1 {
-                        RectView(user: user, friend: friends[1], color: sharedWith.contains(friends[1].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[1], color: sharedFriends.contains(friends[1].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[1].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[1].id }
+                                if sharedFriends.contains(friends[1].id) {
+                                    sharedFriends = sharedFriends.filter { $0 != friends[1].id }
                                 } else {
-                                    sharedWith.append(friends[1].id)
+                                    sharedFriends.append(friends[1].id)
                                 }
-                                print(sharedWith)
+                                print(sharedFriends)
                             })
                     }
                 }
                 
                 HStack {
                     if friends.count > 2 {
-                        RectView(user: user, friend: friends[2], color: sharedWith.contains(friends[2].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[2], color: sharedFriends.contains(friends[2].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[2].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[2].id }
+                                if sharedFriends.contains(friends[2].id) {
+                                    sharedFriends = sharedFriends.filter { $0 != friends[2].id }
                                 } else {
-                                    sharedWith.append(friends[2].id)
+                                    sharedFriends.append(friends[2].id)
                                 }
-                                print(sharedWith)
+                                print(sharedFriends)
                             })
                     }
                     
                     if friends.count > 3 {
-                        RectView(user: user, friend: friends[3], color: sharedWith.contains(friends[3].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[3], color: sharedFriends.contains(friends[3].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[3].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[3].id }
+                                if sharedFriends.contains(friends[3].id) {
+                                    sharedFriends = sharedFriends.filter { $0 != friends[3].id }
                                 } else {
-                                    sharedWith.append(friends[3].id)
+                                    sharedFriends.append(friends[3].id)
                                 }
-                                print(sharedWith)
+                                print(sharedFriends)
                             })
                     }
                     
                     if friends.count > 4 {
-                        RectView(user: user, friend: friends[4], color: sharedWith.contains(friends[4].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[4], color: sharedFriends.contains(friends[4].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[4].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[4].id }
+                                if sharedFriends.contains(friends[4].id) {
+                                    sharedFriends = sharedFriends.filter { $0 != friends[4].id }
                                 } else {
-                                    sharedWith.append(friends[4].id)
+                                    sharedFriends.append(friends[4].id)
                                 }
-                                print(sharedWith)
+                                print(sharedFriends)
                             })
                     }
                 }//HStack
@@ -135,84 +118,66 @@ struct HorizonInviteFriends: View {
                 }//VStack
                 
                 HStack {
-                    if friends.count > 1 {
-                        Button(action: {
-                            defaultMessageButtonTapped(defaultMessage: "SHARE")
-                            shareMood()
-                        },
-                               label: {
-                            Text("ALL")
-                                .fontWeight(.bold)
-                                .frame(width: 100, height: 30)
-                            
-                                .background(colorChangeTap == "SHARE" ? ColorManager.grey3 : ColorManager.purple3)
-                            
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 15))
-        //                                    .background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
-                                .cornerRadius(25)
-                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        })
-                    }
+                   
                     if friends.count > 0 {
-                        RectView(user: user, friend: friends[0], color: sharedWith.contains(friends[0].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[0], color: invitedFriends.contains(friends[0].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[0].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[0].id }
+                                if invitedFriends.contains(friends[0].id) {
+                                    invitedFriends = invitedFriends.filter { $0 != friends[0].id }
                                 } else {
-                                    sharedWith.append(friends[0].id)
+                                    invitedFriends.append(friends[0].id)
                                 }
-                                print(sharedWith)
+                                print(invitedFriends)
                             })
                     }
                     
                     if friends.count > 1 {
-                        RectView(user: user, friend: friends[1], color: sharedWith.contains(friends[1].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[1], color: invitedFriends.contains(friends[1].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[1].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[1].id }
+                                if invitedFriends.contains(friends[1].id) {
+                                    invitedFriends = invitedFriends.filter { $0 != friends[1].id }
                                 } else {
-                                    sharedWith.append(friends[1].id)
+                                    invitedFriends.append(friends[1].id)
                                 }
-                                print(sharedWith)
+                                print(invitedFriends)
                             })
                     }
                 }
                 
                 HStack {
                     if friends.count > 2 {
-                        RectView(user: user, friend: friends[2], color: sharedWith.contains(friends[2].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[2], color: invitedFriends.contains(friends[2].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[2].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[2].id }
+                                if invitedFriends.contains(friends[2].id) {
+                                    invitedFriends = invitedFriends.filter { $0 != friends[2].id }
                                 } else {
-                                    sharedWith.append(friends[2].id)
+                                    invitedFriends.append(friends[2].id)
                                 }
-                                print(sharedWith)
+                                print(invitedFriends)
                             })
                     }
                     
                     if friends.count > 3 {
-                        RectView(user: user, friend: friends[3], color: sharedWith.contains(friends[3].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[3], color: invitedFriends.contains(friends[3].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[3].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[3].id }
+                                if invitedFriends.contains(friends[3].id) {
+                                    invitedFriends = invitedFriends.filter { $0 != friends[3].id }
                                 } else {
-                                    sharedWith.append(friends[3].id)
+                                    invitedFriends.append(friends[3].id)
                                 }
-                                print(sharedWith)
+                                print(invitedFriends)
                             })
                     }
                     
                     if friends.count > 4 {
-                        RectView(user: user, friend: friends[4], color: sharedWith.contains(friends[4].id) ? ColorManager.purple3 : ColorManager.purple5)
+                        RectView(user: user, friend: friends[4], color: invitedFriends.contains(friends[4].id) ? ColorManager.purple3 : ColorManager.purple5)
                             .onTapGesture(perform: {
-                                if sharedWith.contains(friends[4].id) {
-                                    sharedWith = sharedWith.filter { $0 != friends[4].id }
+                                if invitedFriends.contains(friends[4].id) {
+                                    invitedFriends = invitedFriends.filter { $0 != friends[4].id }
                                 } else {
-                                    sharedWith.append(friends[4].id)
+                                    invitedFriends.append(friends[4].id)
                                 }
-                                print(sharedWith)
+                                print(invitedFriends)
                             })
                     }
                 }//HStack
@@ -223,8 +188,8 @@ struct HorizonInviteFriends: View {
                 HStack {
                 
                     Button(action: {
-                        defaultMessageButtonTapped(defaultMessage: "SHARE")
-                        shareMood()
+                        share()
+                        colorChangeTap = "SHARE"
                     },
                        label: {
                     Text("Share")
@@ -246,30 +211,13 @@ struct HorizonInviteFriends: View {
 
 }
 
-private func shareMood() {
-    RestApi.instance.createMoodLog(mood: mood, summary: summary, friends: sharedWith).then({ moodLog in
-        print("Got mood log: ", moodLog)
-        var m = atmosphere.moodLogs ?? []
-        m.append(moodLog.id)
-        let atm = Atmosphere(id: atmosphere.id, planet: atmosphere.planet, mood: mood, moodLogs: m)
-        RestApi.instance.updateAtmosphere(atmosphere: atm).then({ response in
-            if response == 200 {
-                print("Successfully updated atmosphere")
-                for i in sharedWith {
-                    for f in friends {
-                        if i == f.id {
-                            RestApi.instance.sendPushNotification(title: "BestFriends - Horizons", body: "\(user.firstName) You are invited to Horizons", APNToken: f.APNToken )
-                        }
-                    }
-                    mood = -1
-                    summary = ""
-                    sharedWith = []
-                }
-            } else {
-                print("Failed to update atmosphere")
-            }
-        })
-    })
+private func share() {
+    for i in sharedFriends {
+        RestApi.instance.sendPushNotification(title: "\(user.firstName) has set a new goal!", body: "Support their journey on BestFriends", APNToken: i)
+    }
+    for i in invitedFriends {
+        RestApi.instance.sendPushNotification(title: "\(user.firstName) has made a new goal!", body: "You have been invited to participate in \(user.firstName)'s new goal.", APNToken: i)
+    }
 }
 
 private struct RectView: View {
