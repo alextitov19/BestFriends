a71826ae9c04457d0f4acdd0b80412f55b2c7377
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-08-09 18:19:11 -0400

    test flight

--- BestFriends/Purple Subscription/PurpleSubscriptionView.swift
+++ BestFriends/Purple Subscription/PurpleSubscriptionView.swift
@@ -10,10 +10,31 @@ import StoreKit
 
 struct PurpleSubscriptionView: View {
     
+//    @EnvironmentObject var sessionManager: SessionManager
+    
     @StateObject var storeManager: StoreManager
     
     var body: some View {
+
+//        Spacer ()
+//            .frame(height: 50)
+//        
+//        Button(action: {
+//            sessionManager.showLogin()
+//        },
+//            label: {
+//                Text("Home / Chat")
+//                    .fontWeight(.thin)
+//                    .frame(width: 200, height: 40)
+//                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                    .font(.system(size: 30))
+//                    .background(ColorManager.purple3)
+//                    .cornerRadius(15)
+//                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//            })
         NavigationView {
+           
+            
             List(storeManager.myProducts, id: \.self) { product in
                 HStack {
                     VStack(alignment: .leading) {
@@ -48,3 +69,5 @@ struct PurpleSubscriptionView: View {
         }
     }
 }
+
+
