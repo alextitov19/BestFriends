214999893acd100bac5b415c2234967b77ca770a
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-08-10 10:46:41 -0400

    fixed the 'home' button in Chat

--- BestFriends/HomeView/Chat/ChatView.swift
+++ BestFriends/HomeView/Chat/ChatView.swift
@@ -46,18 +46,18 @@ struct ChatView: View {
                 // MARK: Header
                 HStack {
                     
-                    Text("<")
-//                        .resizable()
-                        .frame(width: 35, height: 35)
-                        .font(.system(size: 30))
-//                        .fontWeight(.light)
-                       .foregroundColor(.blue)
-                      
-                        .scaledToFill()
-                        .onTapGesture(perform: {
-                            sessionManager.showHome()
-                        })
-                    
+//                    Text("<")
+////                        .resizable()
+//                        .frame(width: 35, height: 35)
+//                        .font(.system(size: 30))
+////                        .fontWeight(.light)
+//                       .foregroundColor(.blue)
+//                      
+//                        .scaledToFill()
+//                        .onTapGesture(perform: {
+//                            sessionManager.showHome()
+//                        })
+//                    
                     
                     Image(systemName: "info.circle")
                         .resizable()
@@ -68,14 +68,14 @@ struct ChatView: View {
                             sessionManager.infoView(user: user, group: group)
                         })
                     
-//
-//                    Image("home-alt2")
-//                        .resizable()
-//                        .frame(width: 30, height: 30)
-//                        .scaledToFill()
-//                        .onTapGesture(perform: {
-//                            sessionManager.showHome()
-//                        })
+
+                    Image("home-alt2")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .scaledToFill()
+                        .onTapGesture(perform: {
+                            sessionManager.showHome()
+                        })
                     
                     Text(group.name)
                         .task {
