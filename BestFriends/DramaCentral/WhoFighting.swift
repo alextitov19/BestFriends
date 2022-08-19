a4b37f3ab5ddf75fa56cc1989ff6048b008c65dc
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-08-19 14:05:21 -0400

    family drama

--- BestFriends/DramaCentral/WhoFighting.swift
+++ BestFriends/DramaCentral/WhoFighting.swift
@@ -74,7 +74,7 @@ struct WhoFighting: View {
 //
             
             NavigationLink(destination: FD_Step1(), label: {
-                WhoFightingCircle (color: .green, friendName: "BestFamily")
+                WhoFightingCircle (color: .green, friendName: "Family \nDrama")
             })
             .offset(x: showItems ? -20 : 0, y: showItems ? 200: 0)
             .opacity(0.7)
