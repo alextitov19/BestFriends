d94d84d9f66b16f5e527ac95a06ce9616f4596fc
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-08-30 12:32:39 -0400

    smile notes

--- BestFriends/MY ROOM/HIDEOUTS/MyRoomPreload.swift
+++ BestFriends/MY ROOM/HIDEOUTS/MyRoomPreload.swift
@@ -35,17 +35,17 @@ struct MyRoomPreload: View {
             
             
             VStack {
-                
-                NavigationLink(destination: MyRoomDVid(user: user, friends: friends, atmosphere: atmosphere, friendAtmospheres:  friendAtmospheres), label: {
-                    Text("Demo Video")
-                        .fontWeight(.thin)
-                        .frame(width: 200, height: 40)
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                        .font(.system(size: 30))
-                        .background(ColorManager.pmbc_blue)
-                        .cornerRadius(15)
-                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                })
+//                
+//                NavigationLink(destination: MyRoomDVid(user: user, friends: friends, atmosphere: atmosphere, friendAtmospheres:  friendAtmospheres), label: {
+//                    Text("Demo Video")
+//                        .fontWeight(.thin)
+//                        .frame(width: 200, height: 40)
+//                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                        .font(.system(size: 30))
+//                        .background(ColorManager.pmbc_blue)
+//                        .cornerRadius(15)
+//                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                })
                 
                 Text("Yep, its one of those")
                     .font(.system(size: 37))
