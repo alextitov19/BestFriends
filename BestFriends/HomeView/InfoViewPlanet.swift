d1f9679f9cdf072028d8bee56ff675ba305507f9
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-09-08 17:27:18 -0400

    setting and home buttons

--- BestFriends/HomeView/InfoViewPlanet.swift
+++ BestFriends/HomeView/InfoViewPlanet.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 
 struct InfoViewPlanet: View {
+    
     @EnvironmentObject var sessionManager: SessionManager
     
     let user: User
@@ -215,7 +216,7 @@ struct InfoViewPlanet: View {
              
               
                 Spacer()
-                .frame(height: 40)
+                .frame(height: 15)
                 
                 VStack {
                         NavigationLink(
@@ -233,7 +234,22 @@ struct InfoViewPlanet: View {
                             
                             })
             
-
+                    Spacer()
+                    .frame(height: 20)
+                    
+                    Button(action: {
+                        sessionManager.showLogin()
+                    },
+                        label: {
+                            Text("Home / Chat")
+                                .fontWeight(.thin)
+                                .frame(width: 200, height: 40)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .font(.system(size: 30))
+                                .background(ColorManager.purple3)
+                                .cornerRadius(15)
+                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                        })
                 
                 Spacer()
                 .frame(height: 100)
