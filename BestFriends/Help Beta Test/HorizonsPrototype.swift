f87f9b697ae9e49e57ffaa62518da2d6c7e0bcce
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-09-09 01:20:41 -0400

    beta testers and deleted pages unused

--- BestFriends/Help Beta Test/HorizonsPrototype.swift
+++ BestFriends/Help Beta Test/HorizonsPrototype.swift
@@ -25,6 +25,9 @@ struct HorizonsPrototype: View {
 
             VStack {
                
+                Spacer()
+                    .frame(height: 65)
+                
                 Link(destination: URL(string: "https://socialtechlabs.com/beta-tester-_-bestfriends-app/")!) {
                     
                     ZStack {
@@ -38,6 +41,26 @@ struct HorizonsPrototype: View {
                             .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                     }
                 }
+                
+                Spacer()
+                    .frame(height: 15)
+                
+                
+                Button(action: {
+                    sessionManager.showLogin()
+                },
+                    label: {
+                        Text("Home / Chat")
+                            .fontWeight(.thin)
+                            .frame(width: 200, height: 40)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .font(.system(size: 30))
+                            .background(ColorManager.purple3)
+                            .cornerRadius(15)
+                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    })
+                
+
 
                 }
             }
