872f74eea2245f073fceade1d84a4c1a5682255d
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-09-27 13:52:03 -0400

    new feature in World

---
+++ BestFriends/DREAMS/BeSinger.swift
@@ -0,0 +1,123 @@
+//
+//  BeSinger.swift
+//  BestFriends
+//
+//  Created by Social Tech on 9/27/22.
+//
+
+
+import Foundation
+import SwiftUI
+import AVKit
+
+
+struct BeSinger: View {
+    @EnvironmentObject var sessionManager: SessionManager
+    
+    let user: User
+    
+    
+    var body: some View {
+        ZStack {
+            
+            Image("purpleBackground")
+                .resizable()
+                .ignoresSafeArea()
+                .scaledToFill()
+
+//            AdPlayerView(name: "FieldFlowers")
+//                .ignoresSafeArea()
+            
+//            ColorManager.purple7
+//                .ignoresSafeArea()
+//                .onAppear()
+            
+            AdPlayerView(name: "sky2")
+                .ignoresSafeArea()
+                .blendMode(.screen)
+            
+            VStack {
+                
+                Text("YEP!")
+                    .font(.system(size: 30))
+                    .italic()
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager.purple3)
+                
+                Text("we own an indie")
+                    .font(.system(size: 20))
+                    .italic()
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager.purple3)
+             
+                Spacer()
+                .frame(height: 20)
+                
+               
+                Text("Record Label")
+                    .font(.system(size: 40))
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager.purple3)
+            
+                Spacer()
+                .frame(height: 15)
+                
+          
+                
+
+//
+                VStack {
+                
+                Spacer()
+                .frame(height: 35)
+            
+                
+            
+                
+                    
+                    
+                    
+                    
+                    Link(destination: URL(string: "https://socialtechlabs.com/i-wanna-be-a-singer/")!) {
+                        
+                        Text("See Contest Details")
+                            .fontWeight(.thin)
+                            .frame(width: 280, height: 40)
+                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            .font(.system(size: 20))
+                            .background(ColorManager.pmbc_green)
+                            .cornerRadius(15)
+                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    }
+                      
+                    Text("We're recording ONE orginal song \nfor 3 new artists in 2023 - FREE")
+                        .font(.system(size: 17))
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.center)
+    //                    .foregroundColor(.white)
+                       .foregroundColor(ColorManager.grey2)
+                    
+                
+             
+      
+      
+   
+                Spacer()
+                .frame(height: 75)
+                
+
+           
+               
+            }
+        }
+        
+    }
+}
+}
+
+
+
+
--- BestFriends/DREAMS/DreamVaultView.swift
+++ BestFriends/DREAMS/DreamVaultView.swift
@@ -40,9 +40,9 @@ struct DreamVaultView: View {
         
         ZStack {
             
-//            NavigationLink(destination: OurStrategy(), label: { WhoFightingCircle (color: .orange, friendName: "how we \nhelp you")
-//            })
-//                .offset(x: showItems ? 150 : 0, y: showItems ? -140: 0)
+            NavigationLink(destination: BeSinger(user: user), label: { WhoFightingCircle (color: .orange, friendName: "'I' wanna \nbe a \nsinger")
+            })
+                .offset(x: showItems ? 150 : 0, y: showItems ? -140: 0)
 
             NavigationLink(destination: DreamSteps(), label: { WhoFightingCircle (color: .orange, friendName: "dream \nsteps")
             })
@@ -88,6 +88,9 @@ struct DreamVaultView: View {
                .offset(x: showItems ? 150 : 0, y: showItems ? -270: 0)
          
             
+
+        ZStack {
+                    
             Image(systemName: "rectangle.fill")
                                 .resizable()
                                 .foregroundColor(ColorManager .grey4)
@@ -159,216 +162,8 @@ struct DreamCircle: View {
         
     }
 }
+}
 
 
 
 
-
-
-
-
-
-
-//
-//
-//import Foundation
-//import SwiftUI
-//import AVKit
-//
-//struct DreamVaultView: View {
-//
-//    @State private var showItems: Bool = false
-//    @State private var offset: CGFloat = 200.0
-////    @EnvironmentObject var sessionManager: SessionManager
-//
-//    let user: User
-//    let atmosphere: Atmosphere
-//
-//    let friends: [User]
-//    let friendAtmospheres: [Atmosphere]
-//
-//
-//    var body: some View {
-//
-//        ZStack {
-//
-//
-//            Image("purpleBackground")
-//                .resizable()
-//                .ignoresSafeArea()
-//                .scaledToFill()
-//
-//            AdPlayerView(name: "PlanetSound")
-//                .ignoresSafeArea()
-//                .blendMode(.screen)
-//
-//            ZStack {
-//
-//            NavigationLink(destination: Heart(user: user), label: {
-//                WhoFightingCircle (color: .gray, friendName: "twinkie \ndreams")
-//            })
-//               .offset(x: showItems ? 150 : 0, y: showItems ? -100: 0)
-//
-//            NavigationLink(destination: MiroWhiteboard(user: user, friends: friends), label: {
-//                WhoFightingCircle (color: .gray, friendName: "sticky \nnotes")
-//            })
-//                .offset(x: showItems ? 75 : 0, y: showItems ? -300: 0)
-//
-//
-//            NavigationLink(destination: DreamMap(user: user, friends: friends), label: { WhoFightingCircle (color: .gray, friendName: "after \nthe \nstart")
-//            })
-//                .offset(x: showItems ? -25 : 0, y: showItems ? -325: 0)
-//
-//
-//            NavigationLink(destination: EverybodyOk(user: user), label: {
-//                WhoFightingCircle (color: .gray, friendName: "passion")
-//            })
-//               .offset(x: showItems ? -50 : 0, y: showItems ? -150: 0)
-//
-//
-//
-//
-//
-//            NavigationLink(destination: Heart(user: user), label: {
-//                WhoFightingCircle (color: .gray, friendName: "heart \nto \ncontinue")
-//            })
-//               .offset(x: showItems ? -110 : 0, y: showItems ? -260: 0)
-//
-//            NavigationLink(destination: HorizonsPreload1(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres:  friendAtmospheres), label: {
-//                WhoFightingCircle (color: .gray, friendName: "dreams \nwe \ndare")
-//            })
-////            .frame(width: 185, height: 85)
-//            .offset(x: showItems ? 50 : 0, y: showItems ? -160: 0)
-//
-//            VStack {
-//                Spacer()
-//                    .frame(height: 30)
-////
-              
-//
-//
-//
-//
-//
-//
-//                Spacer()
-//                    .frame(height: 300)
-//
-//            }
-//
-//            VStack {
-//
-////                ZStack {
-//        Image(systemName: "rectangle.fill")
-//                    .resizable()
-//                    .foregroundColor(ColorManager .grey4)
-//                    .frame(width: 90, height: 220)
-////                    .blur(radius: 2)
-//                    .opacity(0.90)
-//
-//                    .shadow(color: .white, radius: 65, x: 30, y: 50)
-//
-//
-//                    VStack {
-////               Spacer ()
-////                      .frame(height: 35)
-////
-//
-//              Text("Explore \nbefore \nSleep")
-//                            .font(.system(size: 20))
-//                            .foregroundColor(.white)
-//                            .fontWeight(.medium)
-//                            .multilineTextAlignment(.center)
-//                            .shadow(color: .black, radius: 1, x: 0, y: 1)
-//
-//
-//
-//                    }
-//                }
-//
-//
-//               .onTapGesture {
-//                   withAnimation {
-//                   self.showItems.toggle()
-//                   }
-//                   print("tap function is working")
-//               }
-//
-//               .animation(Animation.easeInOut(duration: 1.0), value: showItems)
-//
-//                }
-//            }
-//        }
-//
-//
-//    struct WhoFightingCircle: View {
-//        var color: Color
-//        var friendName: String
-//
-//        var body: some View {
-//
-//            ZStack {
-//
-//            Rectangle()
-//            .frame(width: 80, height: 80)
-//            .clipShape(Circle())
-//            .foregroundColor(color)
-//            .opacity(0.7)
-//            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 5, x: 0, y: 5)
-//
-//            Text(friendName)
-//                    .fontWeight(.light)
-//                    .italic()
-//                    .foregroundColor(.black)
-//
-//            }
-//        }
-//    }
-//
-//}
-//
-//
-////
-////                }
-////            }
-////
-////                .onTapGesture {
-////                    withAnimation {
-////                    self.showItems.toggle()
-////                    }
-////                    print("tap function is working")
-////                }
-////
-////                .animation(Animation.easeInOut(duration: 3.0), value: showItems)
-////
-////            }
-////            }
-////        }
-////
-////
-////
-////struct HideoutsViewCircle: View {
-////    var color: Color
-////    var friendName: String
-////
-////    var body: some View {
-////
-////        ZStack {
-////
-////        Rectangle()
-////        .frame(width: 85, height: 85)
-////        .clipShape(Circle())
-////        .foregroundColor(color)
-////        .opacity(0.3)
-////        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 5, x: 0, y: 5)
-////
-////        Text(friendName)
-////                .fontWeight(.light)
-////                .italic()
-////                .foregroundColor(ColorManager.grey1)
-////
-////        }
-////    }
-////}
-////}
-////
