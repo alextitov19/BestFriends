1fe3b7ad404cd6391d8008c733142d5505e27909
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-10-10 21:26:15 -0400

    edited the Start Here page

--- BestFriends/HomeView/InfoViewPlanet1.swift
+++ BestFriends/HomeView/InfoViewPlanet1.swift
@@ -36,73 +36,108 @@ struct InfoViewPlanet1: View {
             
         VStack {
 
-         
             VStack {
+                Spacer()
+                .frame(height: 50)
 
-                Text("Welcome to")
-                    .italic()
-                    .font(.system(size: 25))
+                Text("First time through")
+                    .font(.system(size: 20))
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager.grey2)
                 
-                Text("BestFriends")
-                    .font(.system(size: 50))
+                Text("is CONFUSING!")
+                    .font(.system(size: 23))
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager.grey2)
+          
+
+              
+            }
+            
+     
+           
+        VStack {
+
+            NavigationLink(
                 
-                Text("on iOS")
-                    .italic()
-                    .font(.system(size: 25))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(ColorManager.grey2)
-                
-                Text("Android version summer 2023")
-                    .font(.system(size: 15))
-                    .italic()
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(ColorManager.red)
-               
-                
-                Link(destination: URL(string: "https://socialtechlabs.com/our-story/")!) {
-                    
-                    Text("Our Story")
+                destination: InfoViewPlanet(user: user, atmosphere: atmosphere),
+                label: {
+                    Text("-> Walk-Through <-")
                         .fontWeight(.thin)
-                        .frame(width: 250, height: 40)
-                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .foregroundColor(Color.white)
+                        .frame(width: 250, height: 60)
                         .font(.system(size: 25))
-                        .background(ColorManager.pmbc_green)
+                        .background(ColorManager.red)
                         .cornerRadius(15)
                         .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    
                 }
                 
+                
+            )}
+            
+            
+        Text("take a minute to review Setup")
+            .font(.system(size: 20))
+            .fontWeight(.light)
+            .multilineTextAlignment(.center)
+            .foregroundColor(ColorManager.red)
+            
+            VStack {
+
+//                Text("Welcome to")
+//                    .italic()
+//                    .font(.system(size: 25))
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.grey2)
+//
+//                Text("BestFriends")
+//                    .font(.system(size: 50))
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.grey2)
+//
+//                Text("on iOS")
+//                    .italic()
+//                    .font(.system(size: 25))
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.grey2)
+//
+//                Text("Android version summer 2023")
+//                    .font(.system(size: 15))
+//                    .italic()
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.red)
+//
+             
                 VStack {
                     
+             
+//
+//                Text("This is our frist semi-Public launch.")
+//                    .italic()
+//                    .font(.system(size: 15))
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.red)
+//
+//                Text("Yep! We're still work'n out the bugs.")
+//                    .italic()
+//                    .font(.system(size: 15))
+//                    .fontWeight(.light)
+//                    .multilineTextAlignment(.center)
+//                    .foregroundColor(ColorManager.red)
+//
+//
+//
+                
                 Spacer()
-                .frame(height: 70)
-                
-                Text("This is our frist semi-Public launch.")
-                    .italic()
-                    .font(.system(size: 15))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(ColorManager.red)
-                
-                Text("Yep! We're still work'n out the bugs.")
-                    .italic()
-                    .font(.system(size: 15))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(ColorManager.red)
-                
-           
-                
-                
-                Spacer()
-                .frame(height: 20)
+                .frame(height: 50)
                 
          
                 VStack {
@@ -128,56 +163,11 @@ struct InfoViewPlanet1: View {
                         .multilineTextAlignment(.center)
                         .foregroundColor(ColorManager.red)
                 
-                VStack {
-                    Spacer()
-                    .frame(height: 50)
-
-                    Text("First time through")
-                        .font(.system(size: 20))
-                        .fontWeight(.light)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(ColorManager.grey2)
-                    
-                    Text("is CONFUSING!")
-                        .font(.system(size: 23))
-                        .fontWeight(.light)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(ColorManager.grey2)
-                    
-                Text("take a minute to review Setup")
-                    .font(.system(size: 20))
-                    .fontWeight(.light)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(ColorManager.red)
-
-                  
-                }
-                
-         
-               
-            VStack {
- 
-                NavigationLink(
-                    
-                    destination: InfoViewPlanet(user: user, atmosphere: atmosphere),
-                    label: {
-                        Text("-> Walk-Through <-")
-                            .fontWeight(.thin)
-                            .foregroundColor(Color.white)
-                            .frame(width: 250, height: 60)
-                            .font(.system(size: 25))
-                            .background(ColorManager.red)
-                            .cornerRadius(15)
-                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        
-                    }
-                    
-                    
-                )}
+       
              
                     
                 Spacer()
-                .frame(height: 30)
+                .frame(height: 100)
                 
                 VStack {
                         NavigationLink(
@@ -185,10 +175,10 @@ struct InfoViewPlanet1: View {
                             label: {
                                 Text("S E T T I N G S")
                                     .fontWeight(.thin)
-                                    .frame(width: 310, height: 40)
+                                    .frame(width: 310, height: 80)
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 32))
-                                    .background(ColorManager.purple3)
+                                    .font(.system(size: 50))
+                                    .background(ColorManager.pmbc_green)
                                     .cornerRadius(15)
                                     .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                           
