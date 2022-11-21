74ab56bd4d015afce65e410fa5741024e2a9ea0c
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-11-21 02:41:03 -0500

    bugging you page

--- BestFriends/HomeView/WORLD/SomethingBuggingYou/BuggingPreload1.swift
+++ BestFriends/HomeView/WORLD/SomethingBuggingYou/BuggingPreload1.swift
@@ -13,7 +13,7 @@ import AVKit
 
 struct BuggingPreload1: View {
     
-//    @EnvironmentObject var sessionManager: SessionManager
+    //    @EnvironmentObject var sessionManager: SessionManager
     
     let user: User
     let friends: [User]
@@ -21,7 +21,7 @@ struct BuggingPreload1: View {
     
     @State private var showItems: Bool = false
     @State private var offset: CGFloat = 200.0
-
+    
     
     var body: some View {
         
@@ -30,163 +30,178 @@ struct BuggingPreload1: View {
             ColorManager.purple1
                 .ignoresSafeArea()
                 .onAppear()
-           
-//            Image("purpleBackground")
-//                .resizable()
-//                .ignoresSafeArea()
-//                .scaledToFill()
-//
-//
-//            AdPlayerView(name: "storm2")
-//                .ignoresSafeArea()
-//                .blendMode(.screen)
+            
+            //            Image("purpleBackground")
+            //                .resizable()
+            //                .ignoresSafeArea()
+            //                .scaledToFill()
+            //
+            //
+            //            AdPlayerView(name: "storm2")
+            //                .ignoresSafeArea()
+            //                .blendMode(.screen)
             
             
             
             
             
             ZStack{
-//                  Text("Say something")
-//                      .font(.system(size: 50))
-//                      .foregroundColor(ColorManager.purple2)
-//                      .fontWeight(.thin)
-//                      .multilineTextAlignment(.center)
-//
-//                  Text("'Nice' to a Friend")
-//                      .font(.system(size: 50))
-//                      .foregroundColor(ColorManager.purple2)
-//                      .fontWeight(.thin)
-//                      .multilineTextAlignment(.center)
-//                  Spacer()
-//                      .frame(height: 80)
-        
-        
-
-     
-
-
-              NavigationLink(destination: HurtFeelings2(user: user, friends: friends, groups: groups), label: {
-                  BuggingCircle (color: ColorManager.purple2, friendName: "My \nFeeling \ngot hurt")
-            })
-              .offset(x: showItems ? -80 : 0, y: showItems ? -250: 0)
-            
-            NavigationLink(destination: Apologies(user: user, friends: friends, groups: groups), label: {
-                BuggingCircle (color: ColorManager.purple4, friendName: "I hurt \nmy friend's \nFeelings")
-            })
-            .offset(x: showItems ? 100 : 0, y: showItems ? -175: 0)
-            
-
-                  
+                //                  Text("Say something")
+                //                      .font(.system(size: 50))
+                //                      .foregroundColor(ColorManager.purple2)
+                //                      .fontWeight(.thin)
+                //                      .multilineTextAlignment(.center)
+                //
+                //                  Text("'Nice' to a Friend")
+                //                      .font(.system(size: 50))
+                //                      .foregroundColor(ColorManager.purple2)
+                //                      .fontWeight(.thin)
+                //                      .multilineTextAlignment(.center)
+                //                  Spacer()
+                //                      .frame(height: 80)
                 
-            
-        Image(systemName: "triangle.fill")
+                
+                
+                
+                
+                
+                NavigationLink(destination: HurtFeelings2(user: user, friends: friends, groups: groups), label: {
+                    BuggingCircle (color: ColorManager.purple2, friendName: "My \nFeeling \ngot hurt")
+                })
+                .offset(x: showItems ? -80 : 0, y: showItems ? -250: 0)
+                
+                NavigationLink(destination: Apologies(user: user, friends: friends, groups: groups), label: {
+                    BuggingCircle (color: ColorManager.purple4, friendName: "I hurt \nmy friend's \nFeelings")
+                })
+                .offset(x: showItems ? 100 : 0, y: showItems ? -175: 0)
+                
+                
+                
+                
+                
+                Image(systemName: "triangle.fill")
                     .resizable()
                     .foregroundColor(.pink)
-                    .frame(width: 220, height: 180)
-//                    .blur(radius: 2)
-
+                    .frame(width: 300, height: 260)
+                //                    .blur(radius: 2)
+                
                     .shadow(color: .purple, radius: 65, x: 30, y: 50)
                     .opacity(0.99)
-            
-           VStack {
-               
-            Spacer ()
-                   .frame(height: 30)
-               
-               Text("Nervious")
-                             .font(.system(size: 15))
-                         
-                             .foregroundColor(.white)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
-
-               Text("to say anything?")
-                             .font(.system(size: 15))
-                         
-                             .foregroundColor(.white)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
- //                            .shadow(color: .black, radius: 1, x: 0, y: 1)
-         
-               Spacer ()
-                      .frame(height: 8)
-               
-               
-               Text("Join users around the ")
-                             .font(.system(size: 15))
-                         
-                             .foregroundColor(.white)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
-               
-               Text("world saying something")
-                             .font(.system(size: 15))
-                         
-                             .foregroundColor(.white)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
-               
-               Text("NOW!")
-                             .font(.system(size: 25))
-                         
-                             .foregroundColor(.white)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
-               
-               
-               Text("(strength in numbers)")
-                             .font(.system(size: 15))
-                         
-                             .foregroundColor(.blue)
-                             .fontWeight(.light)
-                             .multilineTextAlignment(.center)
+                
+                VStack {
+                    
+                    Spacer ()
+                        .frame(height: 30)
+                    
+                    Text("Nervious")
+                        .font(.system(size: 15))
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                    
+                    Text("to say anything?")
+                        .font(.system(size: 15))
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                    //                            .shadow(color: .black, radius: 1, x: 0, y: 1)
+                    
+                    Spacer ()
+                        .frame(height: 8)
+                    
+                    
+                    Text("Join users around the ")
+                        .font(.system(size: 17))
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                    
+                    Text("world saying something")
+                        .font(.system(size: 17))
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer ()
+                        .frame(height: 8)
+                    
+                    
+                    Text("NOW!")
+                        .font(.system(size: 22))
+                    
+                        .foregroundColor(.white)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
                
                
-               
-       
+                    
+                    VStack {
+                       
+                        Spacer ()
+                            .frame(height: 15)
+                        
+                        Text("you can do this")
+                            .font(.system(size: 17))
+                            .italic()
+                            .foregroundColor(.blue)
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                        
+                        Text("there's strength in numbers")
+                            .font(.system(size: 17))
+                            .italic()
+                            .foregroundColor(.blue)
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                       
+                        
+                    }
                 }
-            }
-            
-          
-           .onTapGesture {
-               withAnimation {
-               self.showItems.toggle()
-               }
-               print("tap function is working")
-           }
-            
-           .animation(Animation.easeInOut(duration: 1.5), value: showItems)
-
+                
+                
+                .onTapGesture {
+                    withAnimation {
+                        self.showItems.toggle()
+                    }
+                    print("tap function is working")
+                }
+                
+                .animation(Animation.easeInOut(duration: 1.5), value: showItems)
+                
             }
         }
     }
-   
-
-struct BuggingCircle: View {
-    var color: Color
-    var friendName: String
     
-    var body: some View {
-       
-        ZStack {
-          
-        Rectangle()
-        .frame(width: 110, height: 110)
-        .clipShape(Circle())
-        .foregroundColor(color)
-        .opacity(0.7)
-        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 5, x: 0, y: 5)
+    
+    struct BuggingCircle: View {
+        var color: Color
+        var friendName: String
         
-        Text(friendName)
-                .fontWeight(.light)
-                .italic()
-                .foregroundColor(.black)
-
+        var body: some View {
+            
+            ZStack {
+                
+                Rectangle()
+                    .frame(width: 110, height: 110)
+                    .clipShape(Circle())
+                    .foregroundColor(color)
+                    .opacity(0.7)
+                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 5, x: 0, y: 5)
+                
+                Text(friendName)
+                    .fontWeight(.light)
+                    .italic()
+                    .foregroundColor(.black)
+                
+            }
         }
     }
+    
 }
-
-
 
 
 
