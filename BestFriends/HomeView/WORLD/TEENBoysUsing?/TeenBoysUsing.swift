eaddcd211f3c398f0fcf18bb65ed90283e4f0b6d
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-12-02 03:01:40 -0500

    boys using app

--- BestFriends/HomeView/WORLD/TEENBoysUsing?/TeenBoysUsing.swift
+++ BestFriends/HomeView/WORLD/TEENBoysUsing?/TeenBoysUsing.swift
@@ -15,32 +15,32 @@ struct TeenBoysUsing: View {
     
     @EnvironmentObject var sessionManager: SessionManager
     
-//    let user: User
-//    let atmosphere: Atmosphere
-//    let friends: [User]
-//    let groups: [Group]
-//    let friendAtmospheres: [Atmosphere]
-//
+    //    let user: User
+    //    let atmosphere: Atmosphere
+    //    let friends: [User]
+    //    let groups: [Group]
+    //    let friendAtmospheres: [Atmosphere]
+    //
     var body: some View {
         ZStack {
-
             
-//            Image("ShaylaBest")
-//                .resizable()
-//                .ignoresSafeArea()
-//                .scaledToFill()
-
-
+            
+            //            Image("ShaylaBest")
+            //                .resizable()
+            //                .ignoresSafeArea()
+            //                .scaledToFill()
+            
+            
             
             Color .black
-              .ignoresSafeArea()
-              .onAppear()
+                .ignoresSafeArea()
+                .onAppear()
             
-//            Image("purpleBackground")
-//                .resizable()
-//                .ignoresSafeArea()
-//                .scaledToFill()
-
+            //            Image("purpleBackground")
+            //                .resizable()
+            //                .ignoresSafeArea()
+            //                .scaledToFill()
+            
             AdPlayerView(name: "sky2")
                 .ignoresSafeArea()
                 .blendMode(.screen)
@@ -49,89 +49,88 @@ struct TeenBoysUsing: View {
             
             VStack {
                 
+           
                 
-                       
-                        
-                        Text("Over 90% of the teen boys interviewed")
-                            .font(.system(size: 17, weight: .bold))
-                            .foregroundColor(ColorManager .grey1)
-                            .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            .opacity(0.50)
-                            .multilineTextAlignment(.center)
-
-            
-                        Text("during beta testing said,")
-                            .font(.system(size: 17, weight: .bold))
-                            .foregroundColor(ColorManager .grey1)
-                            .shadow(color: .black, radius: 1, x: 0, y: 1)
-                            .opacity(0.50)
-                            .multilineTextAlignment(.center)
-                
-                
-                Spacer()
-                    .frame(height: 20)
-                
-                Text("YES")
-//                    .font(.system(size: 25, weight: .bold))
-                    .font(.system(size: 35, weight: .bold))
-                    .foregroundColor(ColorManager .grey1)
-                    .shadow(color: .black, radius: 1, x: 0, y: 1)
-                    .opacity(0.50)
-//                    .foregroundColor(ColorManager.purple5)
-//                    .fontWeight(.ultraLight)
-                    .multilineTextAlignment(.center)
-                
-                Spacer()
-                    .frame(height: 50)
-              
-//
-//
-//                        Link(destination: URL(string: "https://docs.google.com/forms/d/e/1FAIpQLSdpTah0IIOpTjgGgC6x8hxxA5zs5MA6JZWohmd0bKwXDzK0bQ/viewform?usp=sf_link")!) {
-//
-//                            Text("Take, 90-second Survey")
-//                                .fontWeight(.thin)
-//                                .frame(width: 310, height: 40)
-//                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                                .font(.system(size: 25))
-//                                .background(ColorManager.pmbc_green)
-//                                .cornerRadius(15)
-//                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//                        }
-                     
-                Text("If invited by a girl")
-//                    .font(.system(size: 25, weight: .bold))
-                    .font(.system(size: 35, weight: .bold))
-                    .foregroundColor(ColorManager .grey1)
-                    .shadow(color: .black, radius: 1, x: 0, y: 1)
-                    .opacity(0.50)
-//                    .foregroundColor(ColorManager.purple5)
-//                    .fontWeight(.ultraLight)
-                    .multilineTextAlignment(.center)
-                
-//
-//                Text("connections over more, often toxic, ones")
-////                    .font(.system(size: 25, weight: .bold))
-//                    .font(.system(size: 35, weight: .bold))
-//                    .foregroundColor(ColorManager .grey1)
-//                    .shadow(color: .black, radius: 1, x: 0, y: 1)
-//                    .opacity(0.50)
-////                    .foregroundColor(ColorManager.purple5)
-////                    .fontWeight(.ultraLight)
-//                    .multilineTextAlignment(.center)
-//
-               
-                 
-                Spacer()
-                    .frame(height: 50)
-   
-     
-                    }
+                Link(destination: URL(string: "https://www.cnn.com/2022/11/29/health/men-friendships-wellness/index.html")!) {
+                    
+                    Text("Stanford University Study")
+                        .fontWeight(.thin)
+                        .frame(width: 310, height: 40)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .font(.system(size: 25))
+                        .background(ColorManager.pmbc_green)
+                        .cornerRadius(15)
+                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                 }
                 
+                VStack {
+                    
+                }
+                Text("We felt it necessary to address this issue. It is the issue of teen boys that we conduct interviews with when asked if they would use the app on a scale of 1 to 10 (ten being yes) we consistently get a ranking of 4.")
+                    .font(.system(size: 15, weight: .thin))
+                    .foregroundColor(ColorManager .grey1)
+                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+//                    .multilineTextAlignment(.center)
+                    .padding(15)
+                
+                
+                Text("However, when we ask them if they would use the app if invited by a girl they almost always say, yes.")
+                    .font(.system(size: 15, weight: .thin))
+                    .foregroundColor(ColorManager .grey1)
+                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+//                    .multilineTextAlignment(.center)
+                    .padding(15)
+                
+                Text("In a current study by Stanford University, it was found the only 1 in 5 teen boys have a male friend that they can talk to about more personal topics. The reasons for this are well documented from both a neuroscience and societal prospective. ")
+                    .font(.system(size: 15, weight: .thin))
+                    .foregroundColor(ColorManager .grey1)
+                    .shadow(color: .black, radius: 1, x: 0, y: 1)
+//                    .multilineTextAlignment(.center)
+                    .padding(15)
+                
+                
+            
+                    
+                    
+                    Text("The intent of this app, and all the dedicated high school and college interns that have worked tirelessly, is too simply provide a platform to promote more resilient friendships and reduce some of the drama that occurs in friend groups thereby promoting good mental health in teens worldwide. ")
+                        .font(.system(size: 15, weight: .thin))
+                        .foregroundColor(ColorManager .grey1)
+                        .shadow(color: .black, radius: 1, x: 0, y: 1)
+//                        .multilineTextAlignment(.center)
+                        .padding(15)
+                    
+                    
+                    Text("All we are trying to say is that if you do get the chance to be invited by a teen girl to join her friend group, we sincerely hope you take advantage of this opportunity. ")
+                        .font(.system(size: 15, weight: .thin))
+                        .foregroundColor(ColorManager .grey1)
+                        .shadow(color: .black, radius: 1, x: 0, y: 1)
+//                        .multilineTextAlignment(.center)
+                        .padding(15)
+                    
+                    
+                    Text("")
+                        .font(.system(size: 15, weight: .thin))
+                        .foregroundColor(ColorManager .grey1)
+                        .shadow(color: .black, radius: 1, x: 0, y: 1)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    
+                    
+                    
+                    
+                    //
+                    //                Spacer()
+                    //                    .frame(height: 50)
+                    //
+                    
+                }
             }
-}
+            
+        }
+    }
     
-    
+
     
 
 
