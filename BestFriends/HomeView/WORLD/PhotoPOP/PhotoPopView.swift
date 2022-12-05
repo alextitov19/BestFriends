2deef7e9e3dad232a62f35caad33aeca7d19cbc9
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-12-05 18:08:27 -0500

    photopop cleanup

--- BestFriends/HomeView/WORLD/PhotoPOP/PhotoPopView.swift
+++ BestFriends/HomeView/WORLD/PhotoPOP/PhotoPopView.swift
@@ -47,34 +47,34 @@ struct PhotoPopView: View {
 //                Text("PhotoPOP")
 //                    .font(.system(size: 30, weight: .light))
 //                    .foregroundColor(.white)
-                
-                Text("Select Friend, add image from gallery")
-                    .font(.system(size: 15, weight: .light))
-                    .italic()
-                    .foregroundColor(.white)
-                
-                Text("Tap uploaded images to replace")
-                    .font(.system(size: 15, weight: .light))
-                    .italic()
-                    .foregroundColor(.white)
+//
+//                Text("Select Friend - add image from gallery")
+//                    .font(.system(size: 15, weight: .light))
+//
+//                    .foregroundColor(.white)
+//
+//                Text("(tap uploaded images to replace)")
+//                    .font(.system(size: 13, weight: .light))
+//                    .italic()
+//                    .foregroundColor(.white)
                 
                 Text("SHAKE iPhone to see images")
-                    .font(.system(size: 20, weight: .light))
+                    .font(.system(size: 24, weight: .light))
                     .foregroundColor(.green)
-                    
-                NavigationLink(
-                   destination: PhotoPop3(user: user, friends: friends),
-                   label: {
-                       Text("Send Push Notification")
-                           .fontWeight(.thin)
-                           .frame(width: 250, height: 30)
-                           .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                           .font(.system(size: 25))
-                           .background(ColorManager.purple3)
-                           .cornerRadius(15)
-                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                   })
-             
+//                    
+//                NavigationLink(
+//                   destination: PhotoPop3(user: user, friends: friends),
+//                   label: {
+//                       Text("Send Push Notification")
+//                           .fontWeight(.thin)
+//                           .frame(width: 250, height: 30)
+//                           .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                           .font(.system(size: 25))
+//                           .background(ColorManager.purple3)
+//                           .cornerRadius(15)
+//                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                   })
+//             
                 
                 Button(action: {
                     showingRecipients = true
@@ -106,7 +106,7 @@ struct PhotoPopView: View {
                 
                 Spacer()
                 
-//                
+//
 //                Button(action: {
 //                    showingRecipients = true
 //                }, label: {
@@ -175,7 +175,34 @@ struct PhotoPopView: View {
                             .background(ColorManager.grey3)
                             .cornerRadius(15)
                     })
+                    
                     .padding()
+                    
+                    Text("Select Friend - add image from gallery")
+                        .font(.system(size: 15, weight: .light))
+                        
+                        .foregroundColor(.white)
+                    
+                    Text("(tap uploaded images to replace)")
+                        .font(.system(size: 13, weight: .light))
+                        .italic()
+                        .foregroundColor(.white)
+                    
+                    
+                NavigationLink(
+                   destination: PhotoPop3(user: user, friends: friends),
+                   label: {
+                       Text("Send Push Notification")
+                           .fontWeight(.thin)
+                           .frame(width: 250, height: 30)
+                           .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                           .font(.system(size: 25))
+                           .background(ColorManager.purple3)
+                           .cornerRadius(15)
+                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                   })
+                    
+//                    .padding()
                 }
             }
         }
