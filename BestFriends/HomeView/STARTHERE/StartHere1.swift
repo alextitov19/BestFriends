61eca4273cbf3c78cbde84bbbf536293fe71536b
robroe1 <76884097+robroe1@users.noreply.github.com>
2022-12-13 05:11:56 -0500

    settings pages backgrounds

--- BestFriends/HomeView/STARTHERE/StartHere1.swift
+++ BestFriends/HomeView/STARTHERE/StartHere1.swift
@@ -40,7 +40,7 @@ struct StartHere1: View {
                 .ignoresSafeArea()
             
             Image("Meet750")
-               .frame(width: 200, height: 120)
+               .frame(width: 100, height: 50)
                 .ignoresSafeArea()
             
             
