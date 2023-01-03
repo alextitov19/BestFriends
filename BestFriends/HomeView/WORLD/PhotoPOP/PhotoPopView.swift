6a6db020fe38aa5e68bdc2d702017242d9e97710
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-01-03 11:44:21 -0500

    photopop

--- BestFriends/HomeView/WORLD/PhotoPOP/PhotoPopView.swift
+++ BestFriends/HomeView/WORLD/PhotoPOP/PhotoPopView.swift
@@ -189,31 +189,33 @@ struct PhotoPopView: View {
                     
                     .padding()
                     
-                    Text("Select Friend - add image from gallery")
-                        .font(.system(size: 15, weight: .light))
-                        
-                        .foregroundColor(.white)
+                    Text("Up-loading to PhotoPOP:")
+                        .font(.system(size: 16, weight: .light))
+                        .foregroundColor(.green)
                     
-                    Text("(tap uploaded images to replace)")
+                    Text("-Select frined, tap image from photo gallery")
                         .font(.system(size: 13, weight: .light))
                         .italic()
                         .foregroundColor(.white)
                     
+                    Text("-Send Push Notification asking friend to 'SHAKE' iPhone")
+                        .font(.system(size: 13, weight: .light))
+                        .italic()
+                        .foregroundColor(.white)
                     
-//                NavigationLink(
-//                   destination: PhotoPop3(user: user, friends: friends),
-//                   label: {
-//                       Text("Send Push Notification")
-//                           .fontWeight(.thin)
-//                           .frame(width: 250, height: 30)
-//                           .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                           .font(.system(size: 25))
-//                           .background(ColorManager.purple3)
-//                           .cornerRadius(15)
-//                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//                   })
+                    Spacer()
+                        .frame(height: 15)
                     
-//                    .padding()
+                    Text("Replacing image:")
+                        .font(.system(size: 16, weight: .light))
+                        .foregroundColor(.green)
+                    
+                    Text("-Tap image, then exit & re-enter page")
+                        .font(.system(size: 13, weight: .light))
+                        .italic()
+                        .foregroundColor(.white)
+                    
+
                 }
             }
         }
