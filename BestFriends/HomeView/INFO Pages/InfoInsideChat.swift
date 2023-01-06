7aad6fe198dd5d1096d631ae025bda0f97d956d2
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-01-06 10:57:26 -0500

    rob thr night

--- BestFriends/HomeView/INFO Pages/InfoInsideChat.swift
+++ BestFriends/HomeView/INFO Pages/InfoInsideChat.swift
@@ -12,71 +12,71 @@ import SwiftUI
 import AVKit
 
 struct InfoInsideChat: View {
-   
+
     let user: User
-   
+
     var body: some View {
-        
+
         ZStack {
-            
+
             ColorManager .purple1
                 .ignoresSafeArea()
                 .onAppear()
-            
-        
-            
-            
+
+
+
+
             VStack {
-                
-                
-                
+
+
+
                 Text("Friend Need a HUG?")
                     .font(.system(size: 35))
                     .italic()
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager.grey2)
-             
-                
+
+
                 ZStack {
-                    
-                    
+
+
                     Image(systemName: "heart.fill")
                         .resizable()
                         .foregroundColor(ColorManager .grey2)
                         .frame(width: 330, height: 330)
                         .shadow(color: .purple, radius: 65, x: 30, y: 50)
                         .opacity(0.8)
-                    
-                    
-                    
+
+
+
                     VStack {
-                        
-                        
+
+
                         Spacer ()
                             .frame(height: 60)
-                        
+
                         Text("If your friend is having a bad day")
                             .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color .white)
-                        
+
                         Text("ask them to hold their iPnone")
                             .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color .white)
-                     
+
                         Text("to their heart")
                             .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color .white)
-                        
+
                         Spacer ()
                             .frame(height: 20)
 
@@ -86,18 +86,18 @@ struct InfoInsideChat: View {
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(Color .white)
-                        
 
-                  
-                        
+
+
+
                            Spacer ()
                                 .frame(height: 100)
-                   
+
                     }
                 }
             }
         }
     }
-    
+
 }
- 
+
