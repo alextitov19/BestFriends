5f8f79dc01a6d198b8267443a22fed2bf9885726
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-01-16 13:46:30 -0500

    rob

--- BestFriends/HomeView/PLANET PAGE/Planet.swift
+++ BestFriends/HomeView/PLANET PAGE/Planet.swift
@@ -235,7 +235,7 @@ struct PlanetActionsView: View {
                                    
 
                                     
-                                    Text("Private")
+                                    Text("World's")
                                         .foregroundColor(.white)
                                         .font(.system(size: 30, weight: .bold))
                                         .shadow(color: .black, radius: 1, x: 0, y: 1)
