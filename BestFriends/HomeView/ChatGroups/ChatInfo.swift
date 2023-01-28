93ff0dde01a02cccfa526bfa9ca8db8d59dc7b48
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-01-28 12:18:37 -0500

    create chat rooms instructions

--- BestFriends/HomeView/ChatGroups/ChatInfo.swift
+++ BestFriends/HomeView/ChatGroups/ChatInfo.swift
@@ -49,6 +49,25 @@ struct ChatInfo: View {
                     .frame(height: 30)
             
                     VStack {
+                        NavigationLink(
+                            destination: CreateChatRooms(user: user),
+                            label: {
+                                Text("Creating NEW Chat rooms")
+                                    .fontWeight(.thin)
+                                    .frame(width: 310, height: 30)
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                    .font(.system(size: 25))
+                                    .background(ColorManager.purple3)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                
+                            })
+                        
+                       
+                        
+                        
+                        Spacer()
+                            .frame(height: 15)
                         
                         NavigationLink(
                             destination: SafeChat(user: user),
@@ -64,24 +83,7 @@ struct ChatInfo: View {
                                 
                             })
                         
-                        
-                        Spacer()
-                            .frame(height: 15)
-                        
-                        NavigationLink(
-                            destination: CreateChatRooms(user: user),
-                            label: {
-                                Text("Creating NEW Chat rooms")
-                                    .fontWeight(.thin)
-                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager.purple3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                
-                            })
-                        
+
                         
                         Spacer()
                             .frame(height: 15)
