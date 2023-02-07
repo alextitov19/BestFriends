0879456e84a67e4b8c974cec3026e016f0343ace
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-02-06 21:02:18 -0500

    stickers

--- BestFriends/HomeView/INFO Pages/INFOPreload3.swift
+++ BestFriends/HomeView/INFO Pages/INFOPreload3.swift
@@ -24,7 +24,7 @@ struct INFOPreload3: View {
     var body: some View {
         ZStack {
 
-            ColorManager .grey3
+            ColorManager .grey4
                 .ignoresSafeArea()
                 .onAppear()
 
@@ -37,12 +37,12 @@ struct INFOPreload3: View {
 
             Circle()
                 .frame(width: 300, height: 300)
-                .foregroundColor(ColorManager.purple1)
+                .foregroundColor(ColorManager.purple2)
                 .offset(x: 200, y: -450)
 
             Circle()
                 .frame(width: 300, height: 300)
-                .foregroundColor(ColorManager.purple1)
+                .foregroundColor(ColorManager.purple3)
                 .offset(x: -200, y: 400)
 
 
@@ -56,40 +56,43 @@ struct INFOPreload3: View {
 
 
                     Text("Free stickers for you & your friends")
-                        .font(.system(size: 20))
+                        .font(.system(size: 25))
                         .fontWeight(.thin)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
                     
                     Spacer()
                         .frame(height: 10)
-                    Text("Just send us you name & mailing address")
-                        .font(.system(size: 20))
+                 
+
+                    Spacer()
+                        .frame(height: 50)
+                    
+                    Text("Just send us your name \n& mailing address to:")
+                        .font(.system(size: 17))
                         .fontWeight(.thin)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
-
+                    
+                    
                     Spacer()
-                        .frame(height: 30)
+                        .frame(height: 10)
                     
-                    Text("Just send us you name & mailing address to:")
+                    Text("stickers@SocialTechLabs.com")
                         .font(.system(size: 20))
-                        .fontWeight(.thin)
-                        .foregroundColor(ColorManager .purple3)
-                        .multilineTextAlignment(.center)
-
-                    
-                    Text("Stickers@SocialTechLabs.com")
-                        .font(.system(size: 20))
-                        .fontWeight(.thin)
-                        .foregroundColor(ColorManager .purple3)
-                        .multilineTextAlignment(.center)
-
-                    Text("(we pay postage)")
-                        .font(.system(size: 20))
-                        .italic()
                         .fontWeight(.thin)
                         .foregroundColor(ColorManager .grey4)
+                        .multilineTextAlignment(.center)
+
+                    
+                    Spacer()
+                        .frame(height: 10)
+                    
+                    Text("(we pay shipping)")
+                        .font(.system(size: 15))
+                        .italic()
+                        .fontWeight(.thin)
+                        .foregroundColor(ColorManager .grey1)
                         .multilineTextAlignment(.center)
                     
                        Spacer()
