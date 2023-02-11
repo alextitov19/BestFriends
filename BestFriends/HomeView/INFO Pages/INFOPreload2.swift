9a9e33617c795274e631eeb7647c2cfde8321b78
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-02-10 22:28:09 -0500

    Harvard event

--- BestFriends/HomeView/INFO Pages/INFOPreload2.swift
+++ BestFriends/HomeView/INFO Pages/INFOPreload2.swift
@@ -80,90 +80,104 @@ struct INFOPreload2: View {
                     Spacer()
                         .frame(height: 10)
                     
-//                    Image("Harvard")
-//                        .resizable()
-//                        .frame(width: 50, height: 50)
-//                        .cornerRadius(10)
-//                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-//
+                    //                    Image("Harvard")
+                    //                        .resizable()
+                    //                        .frame(width: 50, height: 50)
+                    //                        .cornerRadius(10)
+                    //                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                    //
                     Spacer()
                         .frame(height: 50)
                     
-                    Text("For stickers")
+                    Text("For stickers or questions")
                         .font(.system(size: 17))
                         .fontWeight(.thin)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
-                
+                    
                     Text("find our co-founder, Lizza")
                         .font(.system(size: 17))
                         .fontWeight(.thin)
                         .foregroundColor(Color.white)
                         .multilineTextAlignment(.center)
-                    
-                    
-                    Text("in the _____ Dorm, Rm. _____")
-                        .font(.system(size: 17))
-                        .fontWeight(.thin)
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.center)
-                    
-                    Spacer()
-                        .frame(height: 50)
-                    
                     VStack {
-                        ZStack {
-                            
-                            Image(systemName: "heart.fill")
-                                .resizable()
-                                .foregroundColor(ColorManager .purple1)
-                                .frame(width: 180, height: 150)
-                                .shadow(color: ColorManager .purple3, radius: 65, x: 30, y: 50)
-                            
-//
-                            
-                            NavigationLink(
-                                destination: INFOMainHowWorks(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
-                                label: {
-                                    VStack {
-                                        Text("Welcome")
-                                            .fontWeight(.thin)
-                                            .frame(width: 135, height: 30)
-                                            .foregroundColor(ColorManager .purple3)
-                                            .font(.system(size: 35))
-                                            .background(ColorManager .purple1)
-                                        
-                                        Text("Tap here")
-                                            .fontWeight(.thin)
-                                            .frame(width: 90, height: 30)
-                                    
-                                            .foregroundColor(Color .green)
-                                            .font(.system(size: 20))
-                                            .background(ColorManager .purple1)
-                                    }
-                                }
-                            )}
                         
-                        Spacer()
-                            .frame(height: 200)
-                        
-                        Text("(We are not affiliated with Harvard)")
-                            .font(.system(size: 13))
-                            .italic()
+                        Text("in the _____ Dorm, Rm. _____")
+                            .font(.system(size: 17))
                             .fontWeight(.thin)
                             .foregroundColor(Color.white)
                             .multilineTextAlignment(.center)
                         
+                        Spacer()
+                            .frame(height: 20)
+                        
+                        Text("Email: Admin@SocialTechLabs.com")
+                            .fontWeight(.thin)
+                            .frame(width: 310, height: 25)
+                            .foregroundColor(Color .blue)
+                          
+                            .font(.system(size: 20))
+                            .background(ColorManager .grey2)
+                            .cornerRadius(4)
+                    
                         
                         Spacer()
                             .frame(height: 50)
                         
+                        VStack {
+                            ZStack {
+                                
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .foregroundColor(ColorManager .purple1)
+                                    .frame(width: 180, height: 150)
+                                    .shadow(color: ColorManager .purple3, radius: 65, x: 30, y: 50)
+                                
+                                //
+                                
+                                NavigationLink(
+                                    destination: INFOMainHowWorks(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
+                                    label: {
+                                        VStack {
+                                            Text("Welcome")
+                                                .fontWeight(.thin)
+                                                .frame(width: 135, height: 30)
+                                                .foregroundColor(ColorManager .purple3)
+                                                .font(.system(size: 35))
+                                                .background(ColorManager .purple1)
+                                            
+                                            Text("Tap here")
+                                                .fontWeight(.thin)
+                                                .frame(width: 90, height: 30)
+                                            
+                                                .foregroundColor(Color .green)
+                                                .font(.system(size: 20))
+                                                .background(ColorManager .purple1)
+                                        }
+                                    }
+                                )}
+                            
+                            Spacer()
+                                .frame(height: 200)
+                            
+                            Text("(We are not affiliated with Harvard)")
+                                .font(.system(size: 13))
+                                .italic()
+                                .fontWeight(.thin)
+                                .foregroundColor(Color.white)
+                                .multilineTextAlignment(.center)
+                            
+                            
+                            Spacer()
+                                .frame(height: 50)
+                            
+                            
+                        }
                         
                     }
-                    
                 }
+                
             }
-            
         }
     }
 }
