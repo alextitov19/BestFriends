acd8b0411104cfa3248188bd2a854a755f98f504
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-03-18 09:59:19 -0400

    home buttons

--- BestFriends/HomeView/My ROOM/SaySomethingNice5.swift
+++ BestFriends/HomeView/My ROOM/SaySomethingNice5.swift
@@ -228,10 +228,42 @@ struct SaySomethingNice5: View {
 
                         
                         VStack {
-//                            Image("KissesHeart")
-//                                .resizable()
-//                                .frame(width: 300, height: 200)
-//
+
+                            Image("home-alt2")
+                                .frame(width: 50, height: 25)
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                                .background(Color .black)
+                                .cornerRadius(15)
+                                .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+                                .opacity(0.70)
+                            
+                            Button(action: {
+                                sessionManager.showLogin()
+                            },
+                                label: {
+                                Image("home-alt2")
+                                    .frame(width: 50, height: 25)
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 20))
+                                    .background(Color .black)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+                                    .opacity(0.70)
+                                
+//                                    Text("Home / Chat")
+//                                        .fontWeight(.thin)
+//                                        .frame(width: 200, height: 40)
+//                                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                        .font(.system(size: 30))
+//                                        .background(ColorManager.purple3)
+//                                        .cornerRadius(15)
+//                                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                })
+                            
+
+                            
+                            
                         }
                         
                      
