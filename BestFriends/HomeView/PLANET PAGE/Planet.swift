ed52fb83e67805a949858550c3a29f6fbe3d8ced
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-03-18 13:46:13 -0400

    home button and tap planet page

--- BestFriends/HomeView/PLANET PAGE/Planet.swift
+++ BestFriends/HomeView/PLANET PAGE/Planet.swift
@@ -320,7 +320,7 @@ struct PlanetActionsView: View {
                         
                         Text("tap")
                             .foregroundColor(.green)
-                            .font(.system(size: 20, weight: .regular))
+                            .font(.system(size: 30, weight: .regular))
                             .shadow(color: .black, radius: 1, x: 0, y: 1)
                             .opacity(0.50)
                         
--- BestFriends/HomeView/WORLD/SendSong/PlayLists/SendSongChat2.swift
+++ BestFriends/HomeView/WORLD/SendSong/PlayLists/SendSongChat2.swift
@@ -134,7 +134,22 @@ struct SendSongChat2: View {
                         .cornerRadius(10)
                         .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                 })
-
+//
+//                Button(action: {
+//                    sessionManager.showLogin()
+//                },
+//                       label: {
+//                    Image("home-alt2")
+//                        .frame(width: 50, height: 25)
+//                        .foregroundColor(.white)
+//                        .font(.system(size: 20))
+//                        .background(Color .black)
+//                        .cornerRadius(15)
+//                        .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+//                        .opacity(0.70)
+//                    
+//                })
+//                
                 Spacer()
                     .frame(height: 30)
                 
