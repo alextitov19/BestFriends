5537bfdd75ea7b9c383ede24552d482dafef310d
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-03-25 15:15:01 -0400

    dreams

--- BestFriends/DREAMS/DreamVaultView.swift
+++ BestFriends/DREAMS/DreamVaultView.swift
@@ -42,19 +42,19 @@ struct DreamVaultView: View {
             
             
                 
-                //            NavigationLink(destination: BeSinger(user: user), label: { WhoFightingCircle (color: .orange, friendName: "'I' wanna \nbe a \nsinger")
-                //            })
-                //                .offset(x: showItems ? 150 : 0, y: showItems ? -140: 0)
+                            NavigationLink(destination: BeSinger(user: user), label: { WhoFightingCircle (color: .orange, friendName: "'I' wanna \nbe a \nsinger")
+                            })
+                                .offset(x: showItems ? 150 : 0, y: showItems ? -140: 0)
                 
-                //                NavigationLink(destination: DreamSteps(), label: { WhoFightingCircle (color: .orange, friendName: "dream \nsteps")
-                //                })
-                //                .offset(x: showItems ? 0 : 0, y: showItems ? 250: 0)
+                                NavigationLink(destination: DreamSteps(), label: { WhoFightingCircle (color: .orange, friendName: "dream \nsteps")
+                                })
+                                .offset(x: showItems ? 0 : 0, y: showItems ? 250: 0)
                 
                 
-                //            NavigationLink(destination: HorizonHomeView(user: user, friends: friends, atmosphere: atmosphere), label: {
-                //                WhoFightingCircle (color: Color .green, friendName: "Let's \nDo This")
-                //            })
-                //                .offset(x: showItems ? 0 : 0, y: showItems ? 290: 0)
+                            NavigationLink(destination: HorizonHomeView(user: user, friends: friends, atmosphere: atmosphere), label: {
+                                WhoFightingCircle (color: Color .green, friendName: "Let's \nDo This")
+                            })
+                                .offset(x: showItems ? 0 : 0, y: showItems ? 290: 0)
                 
                 ZStack {
                     
@@ -63,10 +63,10 @@ struct DreamVaultView: View {
                     .offset(x: showItems ? -25 : 0, y: showItems ? -345: 0)
                     
                     
-                    //            NavigationLink(destination: EverybodyOk(user: user), label: {
-                    //                WhoFightingCircle (color: ColorManager.pmbc_green, friendName: "neuro- \nscience")
-                    //            })
-                    //               .offset(x: showItems ? 0 : 0, y: showItems ? 215: 0)
+                                NavigationLink(destination: EverybodyOk(user: user), label: {
+                                    WhoFightingCircle (color: ColorManager.pmbc_green, friendName: "neuro- \nscience")
+                                })
+                                   .offset(x: showItems ? 0 : 0, y: showItems ? 215: 0)
                     
                     
                     NavigationLink(destination: AudioCode(), label: {
@@ -97,7 +97,7 @@ struct DreamVaultView: View {
                         Image(systemName: "rectangle.fill")
                             .resizable()
                             .foregroundColor(ColorManager .grey4)
-                            .frame(width: 100, height: 230)
+                            .frame(width: 150, height: 275)
                         //                    .blur(radius: 2)
                             .opacity(0.90)
                         
@@ -106,14 +106,37 @@ struct DreamVaultView: View {
                         
                         VStack {
                             
-                            Text("our \ndream's \nfloating")
+                            Text("Secrets of Neuroscience")
                                 .font(.system(size: 12))
                                 .foregroundColor(ColorManager.grey2)
                                 .fontWeight(.medium)
                                 .multilineTextAlignment(.center)
                                 .shadow(color: .black, radius: 1, x: 0, y: 1)
                             
-                            Text("somewhere \nout \nthere")
+                            Text("& Your Success")
+                                .font(.system(size: 12))
+                                .foregroundColor(ColorManager.grey2)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            
+                            
+                            Text("Success via Passion")
+                                .font(.system(size: 12))
+                                .foregroundColor(ColorManager.grey2)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            
+                            
+                            Text("-vs.-")
+                                .font(.system(size: 12))
+                                .foregroundColor(ColorManager.grey2)
+                                .fontWeight(.medium)
+                                .multilineTextAlignment(.center)
+                                .shadow(color: .black, radius: 1, x: 0, y: 1)
+                            
+                            Text("raw \nDiscipline")
                                 .font(.system(size: 12))
                                 .foregroundColor(ColorManager.grey2)
                                 .fontWeight(.medium)
