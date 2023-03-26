ddcf2c95765c5c259b9965dc05444ce5ed749d6f
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-03-26 17:34:14 -0400

    build (9) and MindPeers stuff

--- BestFriends/HomeView/My ROOM/MyRoomInfo.swift
+++ BestFriends/HomeView/My ROOM/MyRoomInfo.swift
@@ -134,15 +134,15 @@ struct MyRoomInfo: View {
 
             
                     
-//
-//                    NavigationLink(destination: DreamVaultView(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres:  friendAtmospheres, groups: groups), label: {
-//                        MyRoomCircle (color: Color .white, friendName: "my \nDreamVault")
-//                    })
-//                    .offset(x: showItems ? -120 : 0, y: showItems ? 170: 0)
-//                    .shadow(color: ColorManager .purple2, radius: 10, x: 10, y: 10)
-//                    .opacity(0.4)
-//
-//                    
+
+                    NavigationLink(destination: EmptyView(), label: {
+                        MyRoomCircle (color: Color .white, friendName: "Mind \nPeers \nStuff")
+                    })
+                    .offset(x: showItems ? -120 : 0, y: showItems ? 170: 0)
+                    .shadow(color: ColorManager .purple2, radius: 10, x: 10, y: 10)
+                    .opacity(0.4)
+
+                    
           
 //
 //
