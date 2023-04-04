e39985d54229513367ea49ab575a85d200113d7d
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-04-03 19:51:22 -0700

    Update VirtualHug.swift

--- BestFriends/HomeView/WORLD/Virtual HUG/VirtualHug.swift
+++ BestFriends/HomeView/WORLD/Virtual HUG/VirtualHug.swift
@@ -8,9 +8,11 @@
 
 //import CoreHaptics
 import SwiftUI
+import CoreHaptics
 
 struct VirtualHug: View {
-    
+    @State private var engine: CHHapticEngine?
+
     @EnvironmentObject var sessionManager: SessionManager
     
     var body: some View {
@@ -19,6 +21,7 @@ struct VirtualHug: View {
         ZStack {
             Color(#colorLiteral(red: 0.9301232696, green: 0.9072448611, blue: 0.9865264297, alpha: 1))
                 .ignoresSafeArea()
+                .onAppear(perform: prepareHaptics)
             
             Image("purpleBackground")
                 .resizable()
@@ -78,15 +81,8 @@ struct VirtualHug: View {
                         .foregroundColor(Color.white)
                         .padding()
                         .onTapGesture {
-                            let generator = UINotificationFeedbackGenerator()
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
-                            generator.notificationOccurred(.success)
+                            
+                            complexSuccess()
 
                         }
                 }
@@ -109,6 +105,38 @@ struct VirtualHug: View {
                 Spacer()
                     .frame(height: 300)
             }
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
 }
