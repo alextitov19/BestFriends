6f7416e3b31931e1a0b1427114844ea3a8f39347
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-04-17 16:03:19 -0700

    Update HugPushNotification.swift

--- BestFriends/HomeView/WORLD/Virtual HUG/HugPushNotification.swift
+++ BestFriends/HomeView/WORLD/Virtual HUG/HugPushNotification.swift
@@ -326,6 +326,12 @@ struct HugPushNotification: View {
             for f in friends {
                 if f.id == id {
                     RestApi.instance.sendPushNotification(title: "BestFriends", body: "\(user.firstName) sent you a HUG. To get your hug, tap friend's planet on Home pg, then tap [Friend sent HUG] from dropdown.", APNToken: f.APNToken)
+                    
+                    //MARK: The code below creates an in-app notification for your friend (f.id)
+                    //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
+                    RestApi.instance.createInAppNotification(ian: InAppNotification(user: f.id, sender: user.id, text: "You got a new Hug!", createdOn: Int64(Date().timeIntervalSince1970))).then({ response in
+                        print("Create a hug notification response code: ", response)
+                    })
                 }
             }
         }
