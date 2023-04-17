2488eb97a89e84452f40d2f29b5a4926138ef88c
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-04-17 16:04:50 -0700

    Update SendKisses.swift

--- BestFriends/HomeView/SendKisses/SendKisses.swift
+++ BestFriends/HomeView/SendKisses/SendKisses.swift
@@ -278,6 +278,12 @@ struct SendKisses: View {
             for f in friends {
                 if f.id == id {
                     RestApi.instance.sendPushNotification(title: "BestFriends", body: "\(user.firstName) is blowing you Kisses. Jump up high and catch them.", APNToken: f.APNToken)
+                    
+                    //MARK: The code below creates an in-app notification for your friend (f.id)
+                    //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
+                    RestApi.instance.createInAppNotification(ian: InAppNotification(user: f.id, sender: user.id, text: "You got new Kisses!", createdOn: Int64(Date().timeIntervalSince1970))).then({ response in
+                        print("Create a kiss notification response code: ", response)
+                    })
                 }
             }
         }
