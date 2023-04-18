49b8babc9da36c4b00ab19d28caaa4afd1d1013c
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-04-17 20:34:06 -0400

    home view cleanup

--- BestFriends/HomeView/HomeView.swift
+++ BestFriends/HomeView/HomeView.swift
@@ -323,18 +323,18 @@ struct HomeView: View {
                             .frame(height: 15)
                         
                         HStack {
-                            NavigationLink(destination: VirtualHug(),
-                                           label: {
-                                Text("awe, you've got a HUG")
-                                    .fontWeight(.light)
-                                    .frame(width: 255, height: 30)
-                                    .foregroundColor(.white)
-                                    .background(ColorManager .purple7)
-                                    .cornerRadius(10)
-                                    .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
-                                    .opacity(0.5)
-                            })
-                            
+//                            NavigationLink(destination: VirtualHug(),
+//                                           label: {
+//                                Text("awe, you've got a HUG")
+//                                    .fontWeight(.light)
+//                                    .frame(width: 255, height: 30)
+//                                    .foregroundColor(.white)
+//                                    .background(ColorManager .purple7)
+//                                    .cornerRadius(10)
+//                                    .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+//                                    .opacity(0.5)
+//                            })
+//
                         }
                         
                         
@@ -417,20 +417,20 @@ struct HomeView: View {
                             //                                    .cornerRadius(10)
                             //                                    .opacity(0.7)
                             //
-                            Text("hide dropdown - tap friend's planet")
-                                .font(.system(size: 15))
-                                .italic()
-                            //                            .foregroundColor(ColorManager .grey2)
-                                .fontWeight(.thin)
-                                .multilineTextAlignment(.center)
-                                .frame(width: 255, height: 30)
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                                .background(ColorManager .purple7)
-                                .cornerRadius(10)
-                                .shadow(color: Color(.white), radius: 1, x: 0, y: 2.5)
-                                .opacity(0.30)
-                            
+//                            Text("hide dropdown - tap friend's planet")
+//                                .font(.system(size: 15))
+//                                .italic()
+//                            //                            .foregroundColor(ColorManager .grey2)
+//                                .fontWeight(.thin)
+//                                .multilineTextAlignment(.center)
+//                                .frame(width: 255, height: 30)
+//                                .foregroundColor(.white)
+//                                .font(.system(size: 20))
+//                                .background(ColorManager .purple7)
+//                                .cornerRadius(10)
+//                                .shadow(color: Color(.white), radius: 1, x: 0, y: 2.5)
+//                                .opacity(0.30)
+//
                         }
                         
                         
