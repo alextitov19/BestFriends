98ba4ff371a1e471b5532ca39a94ab9bd539d9e0
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-05-05 13:05:24 -0400

    rob Vibe update UI/UX

--- BestFriends/HomeView/WORLD/Atmosphere/AtmosphereMain2.swift
+++ BestFriends/HomeView/WORLD/Atmosphere/AtmosphereMain2.swift
@@ -636,7 +636,7 @@ struct AtmosphereMain2: View {
                                 .background(shareColor)
                                 .cornerRadius(15)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                .alert("friends will get push notification - \n\n'Blue' atmospher around your planet tells them \nsomething happenedm but \nyou need some time first", isPresented: $showingAlert) {
+                                .alert("Your friend(s) were notified. \n\nThey will send you Care Hearts for Emotional Support. \n\n'Blue' atmosphere around your planet indicates you need some time first.", isPresented: $showingAlert) {
                                     Button("OK", role: .cancel) { }
                                 }
                             
--- BestFriends/MY ROOM/TestPage2.swift
+++ BestFriends/MY ROOM/TestPage2.swift
@@ -40,7 +40,7 @@ struct TestPage2: View {
                 .ignoresSafeArea()
             
             
-            AdPlayerView(name: "Line")
+            AdPlayerView(name: "sky2")
                 .ignoresSafeArea()
             
             
@@ -49,64 +49,82 @@ struct TestPage2: View {
             
             VStack {
                 
-                Text("something")
-                    .font(.system(size: 30))
+                Text("something bad")
+                    .font(.system(size: 40))
 //                    .italic()
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager .grey3)
+                
+             
+                
+                Text("just happend")
+                    .font(.system(size: 35))
+                    .italic()
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager .grey3)
                 
+                Spacer ()
+                    .frame(height: 80)
                 
-//                Text("Not Good!")
-//                    .font(.system(size: 40))
-//                    .fontWeight(.light)
-//                    .multilineTextAlignment(.center)
-//                    .foregroundColor(ColorManager .purple2)
+                Text("But I'm not ready to talk")
+                    .font(.system(size: 25))
+//                    .italic()
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager .grey3)
+                
+                Text("to my friends yet")
+                    .font(.system(size: 25))
+//                    .italic()
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager .grey3)
                 
                 Spacer ()
-                    .frame(height: 7)
+                    .frame(height: 10)
                 
                 
-                Text("just happend")
+                Text("I need some time to myself in")
+                    .font(.system(size: 25))
+//                    .italic()
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(ColorManager .grey3)
+                
+                
+                Text("My Room")
                     .font(.system(size: 35))
 //                    .italic()
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager .grey3)
                 
+                
+                
+                
                 Spacer ()
-                    .frame(height: 145)
+                    .frame(height: 20)
                 
                 ZStack {
-                    //
-                    //
-                    //                    Image(systemName: "heart.fill")
-                    //                        .resizable()
-                    //                        .foregroundColor(ColorManager .purple2)
-                    //                        .frame(width: 360, height: 330)
-                    //                        .shadow(color: ColorManager .purple5, radius: 65, x: 30, y: 50)
-                    //                        .opacity(0.8)
+                    
+                    
+                                        Image(systemName: "heart.fill")
+                                            .resizable()
+                                            .foregroundColor(ColorManager .purple2)
+                                            .frame(width: 360, height: 330)
+                                            .shadow(color: ColorManager .purple5, radius: 65, x: 30, y: 50)
+                                            .opacity(0.8)
                     
                     
                     
                     VStack {
                         
                         Spacer ()
-                            .frame(height: 20)
+                            .frame(height: 100)
                         
-                        Text("your friends care and want to give you")
-                            .font(.system(size: 20))
-                            .italic()
-                            .fontWeight(.light)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(ColorManager .grey3)
-                        
-                        Text("emotional support")
-                            .font(.system(size: 20))
-                            .italic()
-                            .fontWeight(.light)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(ColorManager .grey3)
+                    
                         
                         //                        MyRoomInfo(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups)
                         
@@ -116,45 +134,44 @@ struct TestPage2: View {
                         
 //                        TestPage(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups
                         
+                   
+                     
                         
-                        NavigationLink( destination: AtmosphereMain2(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
-                                        label: {
-                            Text("Let them know when \nyou'll be ready to talk")
-                                .fontWeight(.thin)
-                                .frame(width: 290, height: 80)
-                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                .font(.system(size: 23))
-                                .background(ColorManager.purple3)
-                                .cornerRadius(15)
-                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                        })
-                        //
-                        //                        Text("they can't help - if they don't know!")
-                        //                           .font(.system(size: 20))
-                        //                           .italic()
-                        //                           .fontWeight(.light)
-                        //                           .multilineTextAlignment(.center)
-                        //                           .foregroundColor(ColorManager .grey3)
+                        Text("your friends care and want to give you")
+                            .font(.system(size: 20))
+                            .italic()
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(ColorManager .grey3)
+                        
+                        Text("Emotional Support")
+                            .font(.system(size: 30))
+//                            .italic()
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(ColorManager .grey3)
                         
                         VStack {
                             
+                            
+                            NavigationLink( destination: AtmosphereMain2(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
+                                            label: {
+                                Text("request Care Hearts")
+                                    .fontWeight(.thin)
+                                    .frame(width: 250, height: 40)
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                    .font(.system(size: 23))
+                                    .background(ColorManager.purple3)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                            })
+                            
+                            
+                            
+                            
+                            
                             Spacer ()
-                                .frame(height: 20)
-                            
-                            Text("friends to the rescue")
-                                .font(.system(size: 20))
-                                .italic()
-                                .fontWeight(.light)
-                                .multilineTextAlignment(.center)
-                                .foregroundColor(ColorManager .grey3)
-                            
-                    
-                            
-                            
-                            
-                            
-                            Spacer ()
-                                .frame(height: 200)
+                                .frame(height: 100)
                             
                             
 //                            NavigationLink( destination: MyRoomInfo(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
