e4b09e69df0029cdacce94d89abdf6a8e18ea5db
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-05-07 03:26:25 -0400

    care hearts rob

--- BestFriends/HomeView/WORLD/Virtual HUG/HugPushNotification2.swift
+++ BestFriends/HomeView/WORLD/Virtual HUG/HugPushNotification2.swift
@@ -144,12 +144,7 @@ struct HugPushNotification2: View {
                             .multilineTextAlignment(.center)
                             .foregroundColor(ColorManager .grey3)
                         
-                        Text("")
-                            .font(.system(size: 20))
-//                            .italic()
-                            .fontWeight(.light)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(ColorManager .grey3)
+                    
                         
                         VStack {
                             
@@ -170,8 +165,16 @@ struct HugPushNotification2: View {
                                     .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                             })
                             
+                           
+                            Spacer ()
+                                .frame(height: 10)
                             
-                            
+                            Text("Thx for being a special friend")
+                                .font(.system(size: 20))
+    //                            .italic()
+                                .fontWeight(.light)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(ColorManager .grey3)
                             
                             
                             Spacer ()
