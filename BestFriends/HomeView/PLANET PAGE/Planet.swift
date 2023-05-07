3f674bc34a89ec998fb78fb6c436f3061dfbf68a
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-05-07 16:47:37 -0400

    bad day quiet alert

--- BestFriends/HomeView/PLANET PAGE/Planet.swift
+++ BestFriends/HomeView/PLANET PAGE/Planet.swift
@@ -212,7 +212,7 @@ struct PlanetActionsView: View {
                                 
                                 VStack {
                                 
-                                    Text("Quiet \nAlert")
+                                    Text("Bad Day \nQuiet Alert")
                                         .foregroundColor(ColorManager .purple3)
                                         .font(.system(size: 20, weight: .regular))
                                         .shadow(color: .black, radius: 1, x: 0, y: 1)
