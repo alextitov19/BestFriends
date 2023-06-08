3f4fa39bab685235380ea4498a6ffbab033d21f8
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-06-07 19:21:47 -0700

    refactor

--- BestFriends/InfoPages/ChatInfo.swift
+++ BestFriends/InfoPages/ChatInfo.swift
@@ -52,19 +52,19 @@ struct ChatInfo: View {
                     .frame(height: 30)
             
                     VStack {
-                        NavigationLink(
-                            destination: CreateChatRooms(user: user),
-                            label: {
-                                Text("Creating NEW Chat rooms")
-                                    .fontWeight(.thin)
-                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager .purple3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                    .opacity(0.5)
-                            })
+//                        NavigationLink(
+//                            destination: CreateChatRooms(user: user),
+//                            label: {
+//                                Text("Creating NEW Chat rooms")
+//                                    .fontWeight(.thin)
+//                                    .frame(width: 310, height: 30)
+//                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                    .font(.system(size: 25))
+//                                    .background(ColorManager .purple3)
+//                                    .cornerRadius(15)
+//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                                    .opacity(0.5)
+//                            })
                         
                        
                         
@@ -72,19 +72,19 @@ struct ChatInfo: View {
                         Spacer()
                             .frame(height: 15)
                         
-                        NavigationLink(
-                            destination: SafeChat(user: user),
-                            label: {
-                                Text("Super 'Safe / Private' Chat")
-
-                                    .fontWeight(.thin)                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager .grey3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                    .opacity(0.5)
-                            })
+//                        NavigationLink(
+//                            destination: SafeChat(user: user),
+//                            label: {
+//                                Text("Super 'Safe / Private' Chat")
+//
+//                                    .fontWeight(.thin)                                    .frame(width: 310, height: 30)
+//                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                    .font(.system(size: 25))
+//                                    .background(ColorManager .grey3)
+//                                    .cornerRadius(15)
+//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                                    .opacity(0.5)
+//                            })
                         
 
                         
@@ -92,19 +92,19 @@ struct ChatInfo: View {
                             .frame(height: 15)
                         
                         
-                        NavigationLink(
-                            destination: HowChatWorks(user: user),
-                            label: {
-                                Text("Inside Chat")
-                                    .fontWeight(.thin)
-                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager .grey3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                    .opacity(0.5)
-                            })
+//                        NavigationLink(
+//                            destination: HowChatWorks(user: user),
+//                            label: {
+//                                Text("Inside Chat")
+//                                    .fontWeight(.thin)
+//                                    .frame(width: 310, height: 30)
+//                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                    .font(.system(size: 25))
+//                                    .background(ColorManager .grey3)
+//                                    .cornerRadius(15)
+//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                                    .opacity(0.5)
+//                            })
                         
                         
                         Spacer()
@@ -129,19 +129,19 @@ struct ChatInfo: View {
                             .frame(height: 15)
                         
                         
-                        NavigationLink(
-                            destination: ComingChat(user: user),
-                            label: {
-                                Text("FaceTime (Q:3 2023)")
-                                    .fontWeight(.thin)
-                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager.purple3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                    .opacity(0.5)
-                            })
+//                        NavigationLink(
+//                            destination: ComingChat(user: user),
+//                            label: {
+//                                Text("FaceTime (Q:3 2023)")
+//                                    .fontWeight(.thin)
+//                                    .frame(width: 310, height: 30)
+//                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                    .font(.system(size: 25))
+//                                    .background(ColorManager.purple3)
+//                                    .cornerRadius(15)
+//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                                    .opacity(0.5)
+//                            })
                       
                         
                         Spacer()
