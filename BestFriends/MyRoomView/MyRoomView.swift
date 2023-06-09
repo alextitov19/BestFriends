b70f12361f58672d798202ac94f7c28be361a7a8
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-06-08 21:16:14 -0700

    Update MyRoomView.swift

--- BestFriends/MyRoomView/MyRoomView.swift
+++ BestFriends/MyRoomView/MyRoomView.swift
@@ -21,7 +21,48 @@ struct MyRoomView: View {
             
             VStack {
                 Text("My Room")
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundColor(ColorManager.purple5)
+                
+                MyRoomRowView(title: "Journal", imageName: "text.book.closed", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
+                
+                MyRoomRowView(title: "Breathe", imageName: "wind", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
+                
+                MyRoomRowView(title: "Ground", imageName: "list.number", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
+                
+                MyRoomRowView(title: "Affirm", imageName: "star.leadinghalf.filled", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
+                
+                Spacer()
             }
         }
+    }
+}
+
+struct MyRoomRowView: View {
+    
+    let title, imageName: String
+    let backgroundColor, foregroundColor: CGColor
+    
+    var body: some View {
+        ZStack {
+            Color(cgColor: backgroundColor)
+            
+            HStack{
+                Image(systemName: imageName)
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .scaledToFit()
+                    .foregroundColor(Color(cgColor: foregroundColor))
+                
+                Spacer().frame(width: 20)
+
+                Text(title)
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color(cgColor: foregroundColor))
+            }
+        }
+        .frame(width: 200, height: 60)
+        .cornerRadius(15)
+        .padding()
     }
 }
