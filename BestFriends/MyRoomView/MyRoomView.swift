04f90163d274baf01d477e12aae3c9339436cd24
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-10 18:08:33 -0400

    link for SmileVault

--- BestFriends/MyRoomView/MyRoomView.swift
+++ BestFriends/MyRoomView/MyRoomView.swift
@@ -35,7 +35,8 @@ struct MyRoomView: View {
                 
                 MyRoomRowView(title: "Affirm", imageName: "star.leadinghalf.filled", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
                 
-               
+//                Link to:  SmileVaultPreload
+                MyRoomRowView(title: "SmileVault", imageName: "star.leadinghalf.filled", backgroundColor: ColorManager.purple2.cgColor!, foregroundColor: ColorManager.purple4.cgColor!)
                 
                 
 
