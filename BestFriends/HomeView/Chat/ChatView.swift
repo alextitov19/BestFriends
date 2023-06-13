4ed763895c4c2edb59526f2f2feb8f13daee0cc0
alextitov19 <54718346+alextitov19@users.noreply.github.com>
2023-06-13 01:03:28 -0700

    Update ChatView.swift

--- BestFriends/HomeView/Chat/ChatView.swift
+++ BestFriends/HomeView/Chat/ChatView.swift
@@ -48,19 +48,19 @@ struct ChatView: View {
                 
                 Spacer()
                     .frame(height: 15)
-                
-                NavigationLink(
-                    destination: ChatInfo2(user: user),
-                   label: {
-                       Text("Safety Shut-off after 30 sec. inactive")
-                           .fontWeight(.thin)
-                           .frame(width: 320, height: 20)
-                           .foregroundColor(ColorManager.purple1)
-                           .font(.system(size: 20))
-                           .background(ColorManager.grey3)
-                           .cornerRadius(15)
-                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                   })
+//
+//                NavigationLink(
+//                    destination: ChatInfo2(user: user),
+//                   label: {
+//                       Text("Safety Shut-off after 30 sec. inactive")
+//                           .fontWeight(.thin)
+//                           .frame(width: 320, height: 20)
+//                           .foregroundColor(ColorManager.purple1)
+//                           .font(.system(size: 20))
+//                           .background(ColorManager.grey3)
+//                           .cornerRadius(15)
+//                           .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                   })
                 HStack {
                     Image(systemName: "info.circle")
                         .resizable()
