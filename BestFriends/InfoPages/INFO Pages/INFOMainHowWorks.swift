a9a3959c868f3babdc1b67c06e1774106d9b90cb
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-13 08:45:39 -0400

    walkthrough

--- BestFriends/InfoPages/INFO Pages/INFOMainHowWorks.swift
+++ BestFriends/InfoPages/INFO Pages/INFOMainHowWorks.swift
@@ -94,7 +94,7 @@ struct INFOMainHowWorks: View {
                     
                     VStack {
                         HStack {
-                            ZStack {
+
                                 Image(systemName: "heart.fill")
                                     .resizable()
                                     .foregroundColor(ColorManager .purple2)
@@ -103,24 +103,9 @@ struct INFOMainHowWorks: View {
                                 //                                .shadow(color: .purple, radius: 65, x: 30, y: 50)
                                     .opacity(0.95)
                                 
-                                //                Image("IconPhotoNew40")
-                            }
-                            
-                            //                    InfoAddFriends(user: user),
-                            //
-                            //                NavigationLink(
-                            //                    destination: InfoWhyLoveFB(user: user),
-                            //                    label: {
-                            //                        Text("Why I love BF")
-                            //                            .fontWeight(.thin)
-                            //                            .frame(width: 150, height: 30)
-                            //                            .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                            //                            .font(.system(size: 25))
-                            //                            .background(ColorManager .purple2)
-                            //                            .cornerRadius(10)
-                            //                            .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                            //
-                            //                    })
+                              
+                           
+                         
                             
                         }
                         
