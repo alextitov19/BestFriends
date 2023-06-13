a3589e4789bd97ac86527b5677be255378ff34bc
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-12 21:53:56 -0400

    built by teens

--- BestFriends/HomeView/HomeView.swift
+++ BestFriends/HomeView/HomeView.swift
@@ -108,14 +108,14 @@ struct HomeView: View {
                             NavigationLink(
                                 destination: BuiltByTeensView(user: homeData!.user, friends: homeData!.friends),
                                label: {
-                                   Text("?")
+                                   Text("built by teens")
                                        .fontWeight(.thin)
-                                       .frame(width: 35, height: 35)
+                                       .frame(width: 100, height: 35)
                                        .foregroundColor(Color.white)
                                        
                                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                       .font(.system(size: 30))
-                                       .background(Color .purple)
+                                       .font(.system(size: 17))
+//                                       .background(Color .purple)
                                        .cornerRadius(20)
                                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                                })
