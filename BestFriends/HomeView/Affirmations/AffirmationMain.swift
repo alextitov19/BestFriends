8b2c71f14038c9967c9739ca50b2e9f419049961
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-14 17:23:04 -0400

    rob affirmations

--- BestFriends/HomeView/Affirmations/AffirmationMain.swift
+++ BestFriends/HomeView/Affirmations/AffirmationMain.swift
@@ -10,17 +10,18 @@ import SwiftUI
 struct AffirmationMain: View {
     
     let affirmationDictionary: [String: String] = [
-        "Heart": "I am pretty",
-        "Circle": "I look cute today",
-        "Square": "I am smart in school",
-        "Star": "I am always around when my friends are having a difficult day",
-        "Hexagon": "It is important to me to be a supportive friend without judgement"
+        "Heart": "I'm pretty",
+        "Square": "Exam - I got this",
+        "Star": "I'm always there for BFFs",
+        "Hexagon": "I support w/out judgement"
+//        "Circle": "I look cute today",
+//        "Circle": "I look cute today",
         // This should be read from backEnd
     ]
-
+    
     @State private var currentShape: String = ""
     @State private var currentSentence: String = ""
-
+    
     private func randomizeAffirmation() {
         let randomIndex = Int.random(in: 0..<affirmationDictionary.count)
         let randomShape = Array(affirmationDictionary.keys)[randomIndex]
@@ -31,98 +32,108 @@ struct AffirmationMain: View {
     }
     
     private func startTimer() {
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+        Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { _ in
             randomizeAffirmation()
         }
     }
-
+    
     var body: some View {
         ZStack{
             ColorManager.purple7
                 .opacity(0.3)
                 .ignoresSafeArea()
             
-            AdPlayerView(name: "sky2")
-                .ignoresSafeArea()
-                .blendMode(.screen)
-                .onAppear {
-                    randomizeAffirmation()
-                    startTimer()
-                }
-            
-            VStack {
-                switch currentShape {
-                    case "Star":
-                        Image(systemName: "star.fill")
-                            .font(.system(size: 100))
-                            .foregroundColor(.yellow)
-                    case "Circle":
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(.blue)
-                    case "Square":
-                        Rectangle()
-                            .frame(width: 100, height: 100)
-                            .foregroundColor(.green)
-                    case "Heart":
-                        Image(systemName: "heart.fill")
-                            .font(.system(size: 100))
-                            .foregroundColor(.red)
-                    case "Hexagon":
-                        HexagonShape()
-                                    .frame(width: 100, height: 100)
-                                    .foregroundColor(.blue)
-                    default:
-                        Text(currentShape)
-                            .font(.largeTitle)
+//            AdPlayerView(name: "sky2")
+//                .ignoresSafeArea()
+//                .blendMode(.screen)
+//                .onAppear {
+//                    randomizeAffirmation()
+//                    startTimer()
+                    
+                    AdPlayerView(name: "dramaLights")
+                        .ignoresSafeArea()
+                        .blendMode(.screen)
+                        .onAppear {
+                            randomizeAffirmation()
+                            startTimer()
+
+
+                        }
+                    
+                    VStack {
+                        switch currentShape {
+                        case "Star":
+                            Image(systemName: "star.fill")
+                                .font(.system(size: 100))
+                                .foregroundColor(.yellow)
+                        case "Circle":
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundColor(.blue)
+                        case "Square":
+                            Rectangle()
+                                .frame(width: 90, height: 90)
+                                .foregroundColor(.green)
+                        case "Heart":
+                            Image(systemName: "heart.fill")
+                                .font(.system(size: 100))
+                                .foregroundColor(.red)
+                        case "Hexagon":
+                            HexagonShape()
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.blue)
+                        default:
+                            Text(currentShape)
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Text(currentSentence)
+                            .font(.title)
                             .foregroundColor(.white)
+                        
+                        NavigationLink(destination: AffirmationEdit(affirmationDictionary: affirmationDictionary)) {
+                            Text("Edit")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .padding(.bottom, 300)
+                        
+                    }
                 }
-                
-                Text(currentSentence)
-                    .font(.title)
-                    .foregroundColor(.white)
-                
-                NavigationLink(destination: AffirmationEdit(affirmationDictionary: affirmationDictionary)) {
-                                       Text("Edit")
-                                           .foregroundColor(.white)
-                                           .padding()
-                                           .background(Color.blue)
-                                           .cornerRadius(10)
-                                   }
-                                   .padding(.bottom, 20)
-                
+        }
+    }
+    
+    struct AffirmationMain_Previews: PreviewProvider {
+        static var previews: some View {
+            AffirmationMain()
+        }
+    }
+    
+    struct HexagonShape: Shape {
+        func path(in rect: CGRect) -> Path {
+            let width = rect.width
+            let height = rect.height
+            
+            let sideLength = min(width, height)
+            let centerX = rect.midX
+            let centerY = rect.midY
+            
+            var path = Path()
+            
+            path.move(to: CGPoint(x: centerX + sideLength * cos(0), y: centerY + sideLength * sin(0)))
+            
+            for angle in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 3) {
+                let x = centerX + sideLength * cos(angle)
+                let y = centerY + sideLength * sin(angle)
+                path.addLine(to: CGPoint(x: x, y: y))
             }
+            
+            path.closeSubpath()
+            
+            return path
         }
     }
-}
 
-struct AffirmationMain_Previews: PreviewProvider {
-    static var previews: some View {
-        AffirmationMain()
-    }
-}
-
-struct HexagonShape: Shape {
-    func path(in rect: CGRect) -> Path {
-        let width = rect.width
-        let height = rect.height
-        
-        let sideLength = min(width, height)
-        let centerX = rect.midX
-        let centerY = rect.midY
-        
-        var path = Path()
-        
-        path.move(to: CGPoint(x: centerX + sideLength * cos(0), y: centerY + sideLength * sin(0)))
-        
-        for angle in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 3) {
-            let x = centerX + sideLength * cos(angle)
-            let y = centerY + sideLength * sin(angle)
-            path.addLine(to: CGPoint(x: x, y: y))
-        }
-        
-        path.closeSubpath()
-        
-        return path
-    }
-}
--- BestFriends/HomeView/DramaCentral/Step3_BreathIn/Step3_BreathInviteView.swift
+++ BestFriends/HomeView/DramaCentral/Step3_BreathIn/Step3_BreathInviteView.swift
@@ -30,12 +30,19 @@ struct Step3_BreathInviteView: View {
 //            
             VStack{
                 
-                Text("Ok, something bad just happened")
-                    .font(.system(size: 30, weight: .bold))
-                    .fontWeight(.ultraLight)
+                Text("Something bad")
+                    .font(.system(size: 35, weight: .bold))
+                    .fontWeight(.light)
                     .foregroundColor(ColorManager .grey4)
                     .multilineTextAlignment(.center)
                     
+                
+                Text("just happen?")
+                    .font(.system(size: 35, weight: .bold))
+                    .fontWeight(.light)
+                    .foregroundColor(ColorManager .grey4)
+                    .multilineTextAlignment(.center)
+                
                 HStack {
                     
                   
@@ -65,16 +72,30 @@ struct Step3_BreathInviteView: View {
                         
                         
                         
-                        Spacer()
-                            .frame(height: 10)
+                     
 
                         Spacer ()
-                            .frame(height: 30)
+                            .frame(height: 150)
+                        
+                        Text("Did you just have a huge fight")
+                            .font(.system(size: 25, weight: .bold))
+                            .fontWeight(.light)
+                            .foregroundColor(ColorManager .purple5)
+                            .multilineTextAlignment(.center)
+                       
+                        Text("with your BFF or romantic partner?")
+                            .font(.system(size: 25, weight: .bold))
+                            .fontWeight(.light)
+                            .foregroundColor(ColorManager .purple5)
+                            .multilineTextAlignment(.center)
+                        
+                        Spacer()
+                            .frame(height: 10)
                         
                         NavigationLink(
                             destination: Step4_NuclearOption(user: user, friends: friends),
                             label: {
-                                Text("Fight with Friend?")
+                                Text("Dented Heart?")
                                     .fontWeight(.thin)
                                     .frame(width: 310, height: 40)
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
@@ -84,8 +105,16 @@ struct Step3_BreathInviteView: View {
                                     .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                             })
                         
-//                        Spacer()
-//                            .frame(height: 15)
+                        Spacer()
+                            .frame(height: 15)
+                        
+                        Text("maybe we can help")
+                            .font(.system(size: 20, weight: .bold))
+                            .italic()
+                            .fontWeight(.light)
+                            .foregroundColor(ColorManager .grey4)
+                            .multilineTextAlignment(.center)
+
 //
 //                        Button(action: {
 //                                                     sessionManager.showLogin()
