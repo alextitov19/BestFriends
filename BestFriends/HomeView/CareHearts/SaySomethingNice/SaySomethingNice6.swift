1bcccf13cf228d5e368736788f079edac96e2de9
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-22 01:51:46 -0400

    rob 70

--- BestFriends/HomeView/CareHearts/SaySomethingNice/SaySomethingNice6.swift
+++ BestFriends/HomeView/CareHearts/SaySomethingNice/SaySomethingNice6.swift
@@ -108,14 +108,14 @@ struct SaySomethingNice6: View {
                         .foregroundColor(ColorManager.purple2)
                     
                     Spacer()
-                        .frame(height: 100)
+                        .frame(height: 50)
                     
-                    Text("Setup BestFriends")
-                        .font(.system(size: 27))
-                        .italic()
-                        .foregroundColor(ColorManager.grey2)
-                        .fontWeight(.thin)
-                        .multilineTextAlignment(.center)
+//                    Text("Setup BestFriends")
+//                        .font(.system(size: 17))
+//                        .italic()
+//                        .foregroundColor(ColorManager.grey2)
+//                        .fontWeight(.thin)
+//                        .multilineTextAlignment(.center)
                     
                     
                  
@@ -124,19 +124,19 @@ struct SaySomethingNice6: View {
                         
                         destination: INFOMainHowWorks(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
                         label: {
-                            Text("Walk-through")
+                            Text("SetUp Walk-through")
                                 .fontWeight(.light)
                                 .foregroundColor(Color.white)
-                                .frame(width: 220, height: 60)
+                                .frame(width: 310, height: 60)
                                 .font(.system(size: 25))
                                 .background(Color.green)
-                                .cornerRadius(7)
+                                .cornerRadius(15)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                             
                         })
                     
                     Spacer()
-                        .frame(height: 50)
+                        .frame(height: 30)
                     
                     
                     
@@ -173,31 +173,59 @@ struct SaySomethingNice6: View {
                                 Text("Invite your friends")
                                     .fontWeight(.light)
                                     .foregroundColor(Color.white)
-                                    .frame(width: 220, height: 60)
+                                    .frame(width: 310, height: 60)
                                     .font(.system(size: 25))
                                     .background(Color.orange)
-                                    .cornerRadius(7)
+                                    .cornerRadius(15)
                                     .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                                 
                             })
                         
                         
                         Text("This app doesn't work")
-                            .font(.system(size: 27))
+                            .font(.system(size: 17))
                             .italic()
                             .foregroundColor(ColorManager.grey2)
                             .fontWeight(.thin)
                             .multilineTextAlignment(.center)
                         
                         Text("without your friends")
-                            .font(.system(size: 27))
+                            .font(.system(size: 17))
                             .italic()
                             .foregroundColor(ColorManager.grey2)
                             .fontWeight(.thin)
                             .multilineTextAlignment(.center)
                         
+                        
+                        
+                        
                         Spacer()
-                            .frame(height: 50)
+                            .frame(height: 30)
+                        
+                        
+                        
+                        Link(destination: URL(string: "https://socialtechlabs.com/social-media-influencers/")!) {
+                            
+                            Text("Social Media Influencers")
+                                .fontWeight(.thin)
+                                .frame(width: 310, height: 60)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .font(.system(size: 25))
+                                .background(Color.purple)
+                                .cornerRadius(15)
+                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                        }
+                        
+                                            Text("Our fantastic supporters")
+                                                .font(.system(size: 22))
+                                                .italic()
+                                                .foregroundColor(ColorManager.grey2)
+                                                .fontWeight(.thin)
+                                                .multilineTextAlignment(.center)
+                        
+//
+//                        Spacer()
+//                            .frame(height: 50)
                    
                      
                         
