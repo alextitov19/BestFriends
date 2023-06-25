b10fd6694e164f1d6eb8a183cd2cabd73ddd5b00
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-06-24 20:08:57 -0400

    rob 106

--- BestFriends/InfoPages/INFO Pages/InfoInsideChat.swift
+++ BestFriends/InfoPages/INFO Pages/InfoInsideChat.swift
@@ -12,42 +12,42 @@ import SwiftUI
 import AVKit
 
 struct InfoInsideChat: View {
-
+    
     let user: User
-
+    
     var body: some View {
-
-//        ZStack {
-//
-//            ColorManager .purple1
-//                .ignoresSafeArea()
-//                .onAppear()
-//
-//
-//
-//
-//            VStack {
-//
-//
-//
-//                Text("Create New Chat Rooms")
-//                    .font(.system(size: 35))
-//
-//                    .fontWeight(.light)
-//                    .multilineTextAlignment(.center)
-//                    .foregroundColor(ColorManager.grey2)
-//
-//
-//                ZStack {
-//
-//
-//                    Image(systemName: "heart.fill")
-//                        .resizable()
-//                        .foregroundColor(ColorManager .grey2)
-//                        .frame(width: 330, height: 330)
-//                        .shadow(color: ColorManager .red, radius: 65, x: 30, y: 50)
-//                        .opacity(0.8)
-
+        
+        //        ZStack {
+        //
+        //            ColorManager .purple1
+        //                .ignoresSafeArea()
+        //                .onAppear()
+        //
+        //
+        //
+        //
+        //            VStack {
+        //
+        //
+        //
+        //                Text("Create New Chat Rooms")
+        //                    .font(.system(size: 35))
+        //
+        //                    .fontWeight(.light)
+        //                    .multilineTextAlignment(.center)
+        //                    .foregroundColor(ColorManager.grey2)
+        //
+        //
+        //                ZStack {
+        //
+        //
+        //                    Image(systemName: "heart.fill")
+        //                        .resizable()
+        //                        .foregroundColor(ColorManager .grey2)
+        //                        .frame(width: 330, height: 330)
+        //                        .shadow(color: ColorManager .red, radius: 65, x: 30, y: 50)
+        //                        .opacity(0.8)
+        
         
         ZStack {
             
@@ -63,18 +63,19 @@ struct InfoInsideChat: View {
                 
                 Text("Creating")
                     .font(.system(size: 35))
-            
+                
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager.grey3)
                 
                 Text("NEW Chat Rooms")
                     .font(.system(size: 35))
-            
+                
                     .fontWeight(.light)
                     .multilineTextAlignment(.center)
                     .foregroundColor(ColorManager.grey2)
                 ZStack {
+                    
                     
                     
                     Image(systemName: "circle.fill")
@@ -84,74 +85,91 @@ struct InfoInsideChat: View {
                         .shadow(color: ColorManager .purple2, radius: 65, x: 30, y: 50)
                         .opacity(0.95)
                     
-
+                    
                     VStack {
-
-
+                        
+                        
                         Spacer ()
                             .frame(height: 60)
-
-                        Text("On homepage, tap friend's planets")
-//                            .italic()
+                        
+                        Text("On homepage, tap friend's planet(s)")
+                        //                            .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(ColorManager .grey2)
-
+                        
                         Text("you want in the chat room")
-//                            .italic()
+                        //                            .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(ColorManager .grey2)
-
-
-
+                        
+                        
+                        
                         Spacer ()
                             .frame(height: 20)
-
+                        
                         Text("Then, tap the popup")
-//                            .italic()
+                        //                            .italic()
                             .font(.system(size: 20))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
                             .foregroundColor(ColorManager .grey2)
-
+                        
                         Text("[Create New Chat Room]")
-//                            .italic()
+                        //                            .italic()
                             .font(.system(size: 23))
                             .fontWeight(.light)
                             .multilineTextAlignment(.center)
-                            .foregroundColor(ColorManager .purple4)
-
-
+                            .foregroundColor(ColorManager .grey4)
+                        
                         Spacer ()
-                             .frame(height: 15)
+                            .frame(height: 20)
                         
-                        NavigationLink(
-                            destination: ChangeChatBackgroundView(user: user),
-                            label: {
-                                Text("Customize Chat Background")
-                                    .fontWeight(.thin)
-                                    .frame(width: 310, height: 30)
-                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                                    .font(.system(size: 25))
-                                    .background(ColorManager .grey3)
-                                    .cornerRadius(15)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                    .opacity(0.5)
-                            })
+                        Text("Then, name the Chat room in 2nd popup")
+                        //                            .italic()
+                            .font(.system(size: 20))
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(ColorManager .grey2)
                         
-                        
-                        
-                           Spacer ()
-                                .frame(height: 100)
-
+                        VStack {
+                            
+                            
+                            Text("(We're working on a new system to create chat rooms that is less confusing - coming soon)")
+                                .italic()
+                                .font(.system(size: 15))
+                                .fontWeight(.light)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.red)
+                                .padding()
+                            
+                            
+                            Spacer ()
+                                .frame(height: 40)
+                            
+                            NavigationLink(
+                                destination: ChangeChatBackgroundView(user: user),
+                                label: {
+                                    Text("Customize \nChat Backgrounds")
+                                        .fontWeight(.thin)
+                                        .frame(width: 280, height: 80)
+                                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                        .font(.system(size: 25))
+                                        .background(ColorManager .purple3)
+                                        .cornerRadius(15)
+                                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                        .opacity(0.5)
+                                })
+                            
+                        }
                     }
                 }
             }
         }
+        
     }
-
+    
 }
-
