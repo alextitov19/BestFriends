49f71f70b5eef1e5d2fc80a4591488cf81debcc8
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-06-27 18:57:27 +0800

    basic calendar created, still working on it.

--- BestFriends/HomeView/CareHearts/Atmosphere/FriendValutTrackMoods.swift
+++ BestFriends/HomeView/CareHearts/Atmosphere/FriendValutTrackMoods.swift
@@ -1,124 +1,52 @@
-//
-//  FriendValutTrackMoods.swift
-//  BestFriends
-//
-//  Created by Social Tech on 5/18/22.
-//
+// Created by Zhengxu(Andy) Wang on Jun 27th
 
 
 import Foundation
 import SwiftUI
 
-
 struct FriendVaultTrackMoods: View {
-    
     let user: User
     let atmosphere: Atmosphere
     let friends: [User]
     
-    @State private var selectedTimeRange: TimeRange = .lastWeek // Initial selection
-
-    
+    @State private var calendar = Calendar.current
+    @State private var currentMonth = DateComponents(year: Calendar.current.component(.year, from: Date()), month: Calendar.current.component(.month, from: Date()))
     @State private var moodLogs: [MoodLog] = []
     
     var body: some View {
         ZStack {
-
+            ColorManager.purple7
+                .ignoresSafeArea()
+                .onAppear { loadData() }
+                .scaledToFill()
             
-            ColorManager .purple7
-                 .ignoresSafeArea()
-                 .onAppear{ loadData() }
-                 .scaledToFill()
-
-                     
             AdPlayerView(name: "sky2")
-                          .ignoresSafeArea()
-                           .blendMode(.screen)
+                .ignoresSafeArea()
+                .blendMode(.screen)
             
             VStack {
                 Text("my daily 'Vibes'")
                     .font(.system(size: 30, weight: .light))
-                    .foregroundColor(ColorManager .purple4)
+                    .foregroundColor(ColorManager.purple4)
                 
-                Spacer()
-                    .frame(height: 30)
+                Spacer().frame(height: 30)
                 
-                HStack {
-                    timeRangeButton("Last Week", timeRange: .lastWeek)
-                    timeRangeButton("Last Month", timeRange: .lastMonth)
-                    timeRangeButton("Last 3 Months", timeRange: .lastThreeMonths)
-                }
-                .padding(.horizontal)
-                
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    ForEach(filteredMoodLogs, id: \.id) { moodLog in
-                        ZStack {
-                            if moodLog.mood < 4 {
-                                Color(.systemCyan)
-//                                ColorManager .
-                                    .cornerRadius(25)
-                                    .opacity(0.50)
-                            } else if moodLog.mood == 4 {
-//                                Color(.systemGreen)
-                                ColorManager .pmbc_green
-                                    .cornerRadius(25)
-                            } else if moodLog.mood == 5 {
-//                                Color(.orange)
-                                ColorManager .orange5
-                                    .cornerRadius(25)
-                            } else {
-                               Color(.systemYellow)
-//                                ColorManager .orange3
-                                    .cornerRadius(25)
-                                    .opacity(0.50)
-                            }
-                            
-                            VStack{
-                                //MARK: Date and time
-                                HStack {
-                                    Text(getDateString(date: Date(timeIntervalSince1970: TimeInterval(moodLog.createdOn))))
-                                    
-                                    Spacer()
-                                }
-                               .padding()
-                                
-                                //MARK: Mood Log sumary
-                                HStack {
-                                    Text(moodLog.summary)
-                                    
-                                    Spacer()
-                                }
-                                .padding(.horizontal)
-                                
-                                CustomDivider(color: ColorManager.grey3)
-                                    .padding(.horizontal)
-                                
-                                //MARK: Friends you shared with
-                                HStack {
-                                    ForEach(friends, id: \.id) { friend in
-                                        if moodLog.sharedWith.contains(friend.id) {
-                                            ZStack {
-                                                ColorManager.purple3
-                                                    .frame(width: 50, height: 50)
-                                                    .cornerRadius(25)
-                                                
-                                                Text("\(String(friend.firstName.first!)) \(String(friend.lastName.first!))")
-                                                    .foregroundColor(.white)
-                                                    .fontWeight(.bold)
-                                            }
-                                        }
-                                    }
-                                    
-                                    Spacer()
-                                }
-                                .padding()
-                            }
+                DatePicker(
+                    "",
+                    selection: Binding(
+                        get: {
+                            calendar.date(from: currentMonth)!
+                        },
+                        set: { date in
+                            let components = calendar.dateComponents([.year, .month], from: date)
+                            currentMonth.year = components.year
+                            currentMonth.month = components.month
                         }
-                        .frame(width: 320, height: 80)
-                        .padding(.vertical)
-                    }
-                }
+                    ),
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding(.horizontal)
             }
         }
     }
@@ -133,60 +61,42 @@ struct FriendVaultTrackMoods: View {
         }
     }
     
-    private func getDateString(date: Date) -> String {
-        let formatter3 = DateFormatter()
-        formatter3.dateFormat = "HH:mm E, d MMM y"
-        return formatter3.string(from: date)
+    private func getMoodEmoji(for mood: Int) -> String {
+        switch mood {
+        case 1:
+            return "😢"
+        case 2:
+            return "😞"
+        case 3:
+            return "😐"
+        case 4:
+            return "😊"
+        case 5:
+            return "😄"
+        default:
+            return ""
+        }
+    }
+    
+    private func daysInMonth() -> [Int] {
+        guard let startDate = calendar.date(from: currentMonth),
+              let endDate = calendar.date(byAdding: DateComponents(month: 1, day: -1), to: startDate) else {
+            return []
+        }
+
+        let range = calendar.range(of: .day, in: .month, for: startDate)!
+        let days = range.lowerBound..<range.upperBound
+
+        return days.map { calendar.component(.day, from: calendar.date(byAdding: DateComponents(day: $0 - 1), to: startDate)!) }
     }
     
     private var filteredMoodLogs: [MoodLog] {
-        let currentDate = Date()
-        let calendar = Calendar.current
+        let startDate = calendar.date(from: $currentMonth.wrappedValue)!
+        let endDate = calendar.date(byAdding: DateComponents(month: 1, day: -1), to: startDate)!
         
-        switch selectedTimeRange {
-        case .lastWeek:
-            let startDate = calendar.date(byAdding: .weekOfYear, value: -1, to: currentDate)!
-            let startDateTimestamp = Int64(startDate.timeIntervalSince1970)
-            return moodLogs.filter { $0.createdOn >= startDateTimestamp }
-        case .lastMonth:
-            let startDate = calendar.date(byAdding: .month, value: -1, to: currentDate)!
-            let startDateTimestamp = Int64(startDate.timeIntervalSince1970)
-            return moodLogs.filter { $0.createdOn >= startDateTimestamp }
-        case .lastThreeMonths:
-            let startDate = calendar.date(byAdding: .month, value: -3, to: currentDate)!
-            let startDateTimestamp = Int64(startDate.timeIntervalSince1970)
-            return moodLogs.filter { $0.createdOn >= startDateTimestamp }
+        return moodLogs.filter { moodLog in
+            let moodLogDate = Date(timeIntervalSince1970: TimeInterval(moodLog.createdOn))
+            return calendar.isDate(moodLogDate, inSameDayAs: startDate)
         }
-    }
-
-
-    
-    private func timeRangeButton(_ title: String, timeRange: TimeRange) -> some View {
-        Button(action: {
-            selectedTimeRange = timeRange
-        }) {
-            Text(title)
-                .foregroundColor(selectedTimeRange == timeRange ? .white : .black)
-                .padding()
-                .background(selectedTimeRange == timeRange ? Color.blue : Color.white)
-                .cornerRadius(10)
-        }
-    }
-
-    
-    enum TimeRange {
-        case lastWeek
-        case lastMonth
-        case lastThreeMonths
     }
 }
-
-
-
-//struct FriendVaultTrackMoods_Previews : PreviewProvider {
-//    static var previews: some View {
-//        FriendVaultTrackMoods()
-//    }
-//}
-
-
