7fe6261c8ada85fe3cdfb83ad57e100407f3db17
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-07-14 07:17:02 -0400

    rob comming soon journal friends

--- BestFriends/MyRoomView/MyRoomView.swift
+++ BestFriends/MyRoomView/MyRoomView.swift
@@ -271,7 +271,7 @@ struct MyRoomView: View {
                             .cornerRadius(15)
                             .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                         
-                        NavigationLink( destination: EmptyView(),
+                        NavigationLink( destination: CH1(),
                                         label: {
                             Text("Friends")
                                 .fontWeight(.thin)
