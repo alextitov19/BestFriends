bdb9b58924340f0bdb18144752a8b84f3191d140
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-07-15 23:14:28 -0400

    rob ui

--- BestFriends/MyRoomView/MyRoomView.swift
+++ BestFriends/MyRoomView/MyRoomView.swift
@@ -170,7 +170,7 @@ struct MyRoomView: View {
                             .frame(width: 155, height: 40)
                             .foregroundColor(Color.white)
                             .font(.system(size: 22))
-                            .background(Color.orange)
+                            .background(Color.pink)
                             .opacity(0.99)
                             .cornerRadius(15)
                             .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
