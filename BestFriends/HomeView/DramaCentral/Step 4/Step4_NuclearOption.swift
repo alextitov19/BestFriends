b1f011a29d2c8462f68ac718b53cb26b11e0dda4
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-07-29 01:58:43 -0400

    rob drama central

--- BestFriends/HomeView/DramaCentral/Step 4/Step4_NuclearOption.swift
+++ BestFriends/HomeView/DramaCentral/Step 4/Step4_NuclearOption.swift
@@ -26,33 +26,34 @@ struct Step4_NuclearOption: View {
 
         ZStack {
             
+            ColorManager .purple4
+                .opacity(0.6)
+                .ignoresSafeArea()
             
-//                        ColorManager.purple7
-//                            .ignoresSafeArea()
-            //
-            //            ColorManager.purple7
-            //                .opacity(0.6)
-            //                .ignoresSafeArea()
             
             Image("FHBackground")
-                .ignoresSafeArea()
-                .scaledToFit()
-
-                
-            AdPlayerView(name: "Galaxy2")
-                .ignoresSafeArea()
-                .blendMode(.screen)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+//            Image("FHBackground")
+//                .ignoresSafeArea()
+//                .scaledToFit()
+//
+//
+//            AdPlayerView(name: "Galaxy2")
+//                .ignoresSafeArea()
+//                .blendMode(.screen)
 
 
             ZStack{
 
-//                Spacer ()
-//                    .frame(height: 500)
+
 
                     NavigationLink(destination: Step2_FightLandingView(user: user, friends: friends), label: {
                         HugCircle (color: Color.purple, friendName: "When \nyou're ready to \nreach out to try to fix this \n\nTAP Here...")
                     })
-                    .offset(x: showItems ? 20 : 0, y: showItems ? 50: 0)
+                    .offset(x: showItems ? 0 : 0, y: showItems ? 80: 0)
                     .shadow(color: ColorManager .purple1, radius: 30, x: 10, y: 10)
 //                    .opacity(0.6)
                     .opacity(0.8)
@@ -78,9 +79,9 @@ struct Step4_NuclearOption: View {
                 NavigationLink(destination: EmptyView(), label: {
                             HugCircle (color: ColorManager .purple5, friendName: "")
                         })
-                        .offset(x: showItems ? 300 : 0, y: showItems ? -400: 0)
+                        .offset(x: showItems ? 400 : 0, y: showItems ? -400: 0)
                         .shadow(color: ColorManager .grey1, radius: 20, x: 10, y: 10)
-                        .opacity(0.9)
+                        .opacity(0.99)
            
                 
 //                NavigationLink(destination: EmptyView(), label: {
@@ -108,22 +109,22 @@ struct Step4_NuclearOption: View {
                         
                    
                     
-                    
-                    NavigationLink(destination: EmptyView(), label: {
-                        HugCircle (color: ColorManager .grey3, friendName: "Sometimes your heart \nneeds more time to accept what \nyou mind already knows")
-                    })
-                    .offset(x: showItems ? -400 : 0, y: showItems ? 400: 0)
-                    //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
-                    .opacity(0.9)
-                    
-                
-                    
-                NavigationLink(destination: EmptyView(), label: {
-                    HugCircle (color: ColorManager .grey3, friendName: "The hardest part of letting go \nis realizing the other \nperson already did")
-                })
-                .offset(x: showItems ? -400 : 0, y: showItems ? -600: 0)
-                //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
-                .opacity(0.6)
+//
+//                    NavigationLink(destination: EmptyView(), label: {
+//                        HugCircle (color: ColorManager .grey3, friendName: "Sometimes your heart \nneeds more time to accept what \nyou mind already knows")
+//                    })
+//                    .offset(x: showItems ? -400 : 0, y: showItems ? 400: 0)
+//                    //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
+//                    .opacity(0.9)
+//
+//
+//
+//                NavigationLink(destination: EmptyView(), label: {
+//                    HugCircle (color: ColorManager .grey3, friendName: "The hardest part of letting go \nis realizing the other \nperson already did")
+//                })
+//                .offset(x: showItems ? -400 : 0, y: showItems ? -600: 0)
+//                //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
+//                .opacity(0.6)
                 
                 
 //                NavigationLink(destination: EmptyView(), label: {
@@ -145,16 +146,16 @@ struct Step4_NuclearOption: View {
                     NavigationLink(destination: EmptyView(), label: {
                         HugCircle (color: ColorManager .grey3, friendName: "sharp words \nerase all the nice words \nyou've ever said")
                     })
-                    .offset(x: showItems ? 120 : 0, y: showItems ? 100: 0)
+                    .offset(x: showItems ? -350 : 0, y: showItems ? 350: 0)
                     //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
                     .opacity(0.6)
                     
-                    NavigationLink(destination: EmptyView(), label: {
-                        HugCircle (color: ColorManager .grey3, friendName: "Never allow someone \nto be your prioity while \nyou are their option")
-                    })
-                    .offset(x: showItems ? -130 : 0, y: showItems ? 310: 0)
-                    //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
-                    .opacity(0.7)
+//                    NavigationLink(destination: EmptyView(), label: {
+//                        HugCircle (color: ColorManager .grey3, friendName: "Never allow someone \nto be your prioity while \nyou are their option")
+//                    })
+//                    .offset(x: showItems ? -130 : 0, y: showItems ? 310: 0)
+//                    //                    .shadow(color: ColorManager .grey2, radius: 10, x: 10, y: 10)
+//                    .opacity(0.7)
                     
 
                     Spacer ()
@@ -166,7 +167,7 @@ struct Step4_NuclearOption: View {
                         
                         Image(systemName: "circle.fill")
                             .resizable()
-                            .foregroundColor(Color.purple)
+                            .foregroundColor(Color.cyan)
                             .frame(width: 90, height: 90)
                         //                    .blur(radius: 2)
                             .shadow(color: ColorManager .purple3, radius: 65, x: 30, y: 50) .opacity(0.99)
@@ -192,7 +193,8 @@ struct Step4_NuclearOption: View {
                     }
                         
                         VStack {
-
+                            Spacer ()
+                                .frame(height: 235)
                             
                             Link(destination: URL(string: "https://socialtechlabs.com/understanding-the-why/")!) {
                                 
@@ -201,16 +203,15 @@ struct Step4_NuclearOption: View {
                                     .frame(width: 300, height: 80)
                                     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
                                     .font(.system(size: 26))
-                                    .background(ColorManager.grey3)
+                                    .background(Color.orange)
                                     .cornerRadius(15)
                                     .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                                 
                             }
-                            Spacer()
-                                .frame(height: 40)
+                         
                             
                             Spacer ()
-                                .frame(height: 200)
+                                .frame(height: 100)
 
 
                         }
