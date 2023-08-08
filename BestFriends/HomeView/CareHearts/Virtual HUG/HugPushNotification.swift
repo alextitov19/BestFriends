e824b1945948810606d304cfd8fa4dfccfd981c6
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-08-08 17:57:23 -0400

    rob

--- BestFriends/HomeView/CareHearts/Virtual HUG/HugPushNotification.swift
+++ BestFriends/HomeView/CareHearts/Virtual HUG/HugPushNotification.swift
@@ -288,7 +288,7 @@ struct HugPushNotification: View {
                                 .background(ColorManager .grey2)
                                 .cornerRadius(25)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                .alert("Sometimes there's more power in a simple hug than a thousand words.", isPresented: $showingAlert) {
+                                .alert("Your hug has been sent. \n\nSometimes there's more power in a simple hug than a thousand words.", isPresented: $showingAlert) {
                                     Button("OK", role: .cancel) { }
                                 }
                         })
