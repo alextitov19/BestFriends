682a198d34f1a51978560c6dd70a1f478ca54334
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-08-11 03:44:36 -0400

    rob panic attack

--- BestFriends/MyRoomView/My VIBE/Anxiety Attack/MyFears.swift
+++ BestFriends/MyRoomView/My VIBE/Anxiety Attack/MyFears.swift
@@ -134,19 +134,31 @@ struct MyFears: View {
                         
                             .opacity(0.95)
                         
+                        Link(destination: URL(string: "https://cssnv.org/")!) {
+                            
+                            Text("Let's find you \nsome help")
+                                .fontWeight(.light)
+                                .frame(width: 150, height: 60)
+                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                .font(.system(size: 17))
+//                                .background(ColorManager.pmbc_green)
+                                .cornerRadius(15)
+                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                        }
                         
-                        NavigationLink(
-                            destination: ProfessionalHelp(user: user),
-                            label: {
-                                Text("Let's find you some help?")
-                                    .fontWeight(.light)
-                                    .frame(width: 100, height: 70)
-                                    .foregroundColor(ColorManager .grey4)
-                                    .font(.system(size: 17))
-//                                    .background(ColorManager .purple1)
-                                    .cornerRadius(10)
-                                
-                            })
+                        
+//                        NavigationLink(
+//                            destination: ProfessionalHelp(user: user),
+//                            label: {
+//                                Text("Let's find you some help?")
+//                                    .fontWeight(.light)
+//                                    .frame(width: 100, height: 70)
+//                                    .foregroundColor(ColorManager .grey4)
+//                                    .font(.system(size: 17))
+////                                    .background(ColorManager .purple1)
+//                                    .cornerRadius(10)
+//
+//                            })
                     }
                     
                     
