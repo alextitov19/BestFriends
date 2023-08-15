32c421f0f9043f222465d48f2e5a7b2ec06f61f9
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-08-15 14:11:20 -0400

    rob vibe

--- BestFriends/MyRoomView/My VIBE/AtmosphereMain2.swift
+++ BestFriends/MyRoomView/My VIBE/AtmosphereMain2.swift
@@ -348,18 +348,18 @@ struct AtmosphereMain2: View {
                         Spacer()
                             .frame(height: 12)
                         
-                        Button(action: {
-                            mood = 7
-                        }) {
-
-                            Text("I really need to TALK!")
-                                .font(.system(size: 10))
-                                .foregroundColor(Color.black)
-                        }
-                        .frame(width: 140, height: 30, alignment: .center)
-                        .background(mood == 7 ? Color.green : Color.red)
-                        .cornerRadius(7)
-                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//                        Button(action: {
+//                            mood = 7
+//                        }) {
+//
+//                            Text("I really need to TALK!")
+//                                .font(.system(size: 10))
+//                                .foregroundColor(Color.black)
+//                        }
+//                        .frame(width: 140, height: 30, alignment: .center)
+//                        .background(mood == 7 ? Color.green : Color.red)
+//                        .cornerRadius(7)
+//                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
 
 
                         Spacer()
