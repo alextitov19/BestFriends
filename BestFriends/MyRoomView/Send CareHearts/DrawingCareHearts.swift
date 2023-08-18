52613c66d35e063c5484dc6b15c8f0cd62744297
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-08-18 14:33:43 -0400

    New page

---
+++ BestFriends/MyRoomView/Send CareHearts/DrawingCareHearts.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingCareHearts.swift
+//  BestFriends
+//
+//  Created by Zhengxu Wang on 8/18/23.
+//
+
+import SwiftUI
+
+struct DrawingCareHearts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DrawingCareHearts_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawingCareHearts()
+    }
+}
