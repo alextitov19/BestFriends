97d39fa0e5717447ffc1078abcc9191a93cbcb54
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-08-20 15:37:06 -0400

    Draw with different colors

--- BestFriends/MyRoomView/Send CareHearts/DrawingCareHearts.swift
+++ BestFriends/MyRoomView/Send CareHearts/DrawingCareHearts.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 
-struct Line{
+struct Line {
     var points = [CGPoint]()
     var color: Color = .red
     var lineWidth: Double = 1.0
@@ -18,31 +18,53 @@ struct Line{
 struct DrawingCareHearts: View {
 
     @State private var currentLine = Line()
-    @State private var lines: [Line] = []
-    
-    var body: some View{
-        VStack {
-            Canvas{ context, size in
-                for line in lines{
-                    var path = Path()
-                    path.addLines(line.points)
-                    context.stroke(path, with: .color(line.color), lineWidth:
-                                    line.lineWidth)
-                }
-            }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                .onChanged({ value in
-                    let newPoint = value.location
-                    currentLine.points.append(newPoint)
-                    self.lines.append(currentLine)
-                })
-                .onEnded({ value in
-                            
-                        })
-                      
-                )
-
-        }
-    }
+   @State private var lines: [Line] = []
+   @State private var thickness: Double = 1.0
+   
+   var body: some View {
+       VStack {
+          
+           Canvas { context, size in
+               
+               for line in lines {
+                   var path = Path()
+                   path.addLines(line.points)
+                   context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+               }
+               
+               
+           }
+           .frame(minWidth: 400, minHeight: 400)
+           .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+           .onChanged({ value in
+               let newPoint = value.location
+               currentLine.points.append(newPoint)
+               self.lines.append(currentLine)
+             })
+           .onEnded({ value in
+               self.lines.append(currentLine)
+               self.currentLine = Line(points: [], color: currentLine.color, lineWidth: thickness)
+           })
+           )
+           
+           HStack {
+               
+               Slider(value: $thickness, in: 1...20) {
+                   Text("Thickness")
+               }.frame(maxWidth: 200)
+                   .onChange(of: thickness) { newThickness in
+                       currentLine.lineWidth = newThickness
+                   }
+               Divider()
+               ColorPicker(selectedColor: $currentLine.color)
+                   .onChange(of: currentLine.color) { newColor in
+                       print(newColor)
+                       currentLine.color = newColor
+               }
+           }
+           
+       }.padding()
+   }
 }
 
 
