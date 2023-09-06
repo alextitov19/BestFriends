8b296714c03bed884d8fa9ba2ba434bd68f48e34
robroe2 <github@socialtechlabs.com>
2023-09-06 03:47:35 -0400

    rob UI

--- BestFriends/HomeView/PlanetView/MyRoomView.swift
+++ BestFriends/HomeView/PlanetView/MyRoomView.swift
@@ -67,7 +67,7 @@ struct MyRoomView: View {
             //                .blendMode(.screen)
             //                .opacity(0.99)
             
-            AdPlayerView(name: "background_7")
+            AdPlayerView(name: "Fireworks2")
                 .ignoresSafeArea()
                 .blendMode(.screen)
                 .opacity(0.99)
