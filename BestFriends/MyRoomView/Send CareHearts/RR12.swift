ac285be023585c5d454af65c781018f410b74ec2
robroe2 <github@socialtechlabs.com>
2023-09-20 02:51:29 -0400

    rob making CH better

--- BestFriends/MyRoomView/Send CareHearts/RR12.swift
+++ BestFriends/MyRoomView/Send CareHearts/RR12.swift
@@ -403,7 +403,7 @@ struct RR12: View {
                                 .background(ColorManager .grey2)
                                 .cornerRadius(25)
                                 .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                .alert("Your Congratulations have been sent.", isPresented: $showingAlert) {
+                                .alert("Nice. You just told your friend they \n\nCRUSHED IT!.", isPresented: $showingAlert) {
                                     Button("OK", role: .cancel) { }
                                 }
                             
