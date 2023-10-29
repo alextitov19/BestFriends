e61d2a36bda2962d0a0d21ab9d172881f9b4dfc7
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-10-28 20:21:02 -0400

    rob motion

--- BestFriends/MyRoomView/INFO icon/START HERE/RR4.swift
+++ BestFriends/MyRoomView/INFO icon/START HERE/RR4.swift
@@ -23,6 +23,30 @@ struct RR4: View {
     
     
     var body: some View {
+        
+        
+        
+        VStack {
+            Spacer()
+                .frame(height: 100)
+            
+            NavigationLink(destination:  RR36Info(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
+                                       label: {
+                            Text("Getting Started")
+                                .fontWeight(.thin)
+                                .foregroundColor(Color.white)
+                                .frame(width: 220, height: 40)
+                                .font(.system(size: 30))
+                                .background(ColorManager .grey2)
+                                .opacity(0.95)
+                                .cornerRadius(10)
+                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                        })
+                    }
+        
+        
+        
+//      ***********************************
         GeometryReader { geo in
             HStack(spacing: -1) {
                 Image("rocketguy")
@@ -41,27 +65,5 @@ struct RR4: View {
                 animate.toggle()
             }
         }
-//            *************************
-            
-            VStack {
-                
-             
-                NavigationLink(destination:  RR36Info(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
-                                           label: {
-                                Text("Getting Started")
-                                    .fontWeight(.thin)
-                                    .foregroundColor(Color.white)
-                                    .frame(width: 220, height: 40)
-                                    .font(.system(size: 30))
-                                    .background(ColorManager .grey2)
-                                    .opacity(0.95)
-                                    .cornerRadius(10)
-                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
-                                
-                            })
-                        }
-// **********
-            
-//        }
     }
 }
