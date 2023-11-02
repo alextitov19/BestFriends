adacd4f8ff3b02bfa5f0e08132c7ec26378c6f26
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-11-01 22:08:49 -0400

    rob System

--- BestFriends/MyRoomView/User System Response/PhotoPopInfo.swift
+++ BestFriends/MyRoomView/User System Response/PhotoPopInfo.swift
@@ -408,7 +408,7 @@ VStack {
             
             Image("FatGuy200")
                 .aspectRatio(contentMode: .fit)
-            //                   .blinking(duration: 4.0)
+                               .blinking(duration: 4.0)
             
             Spacer()
                 .frame(width: 150)
