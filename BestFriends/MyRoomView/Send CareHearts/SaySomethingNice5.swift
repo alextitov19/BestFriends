36ca6e00242e9627dfca7ffbcf4034a81f2cb0a2
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-11-30 03:03:59 -0500

    rob

--- BestFriends/MyRoomView/Send CareHearts/SaySomethingNice5.swift
+++ BestFriends/MyRoomView/Send CareHearts/SaySomethingNice5.swift
@@ -32,16 +32,16 @@ struct SaySomethingNice5: View {
     @State private var sharedWith: [String] = []
     @State private var colorChangeTap: String = ""
     @State private var shareTapped: Bool = false
-
-
+    
+    
     
     var body: some View {
         
         ZStack {
-      
+            
             if shareTapped {
                 
-              
+                
                 
                 AdPlayerView(name: "dramaLights")
                     .opacity(0.5)
@@ -49,75 +49,69 @@ struct SaySomethingNice5: View {
                     .blendMode(.screen)
                 
             } else {
-
                 
-              
-
+                
+                
+                
                 AdPlayerView(name: "easyHearts2")
                     .ignoresSafeArea()
                     .blendMode(.screen)
                     .opacity(0.8)
                 
                 Color.black
-                  .opacity(0.4)
-                  .ignoresSafeArea()
+                    .opacity(0.4)
+                    .ignoresSafeArea()
                 
             }
             
-            VStack{
+
+
+            VStack {
                 
-                HStack {
-                    VStack {
-                   
-                        
-                        
-                        Spacer()
-                            .frame(height: 10)
-                        
-                        ZStack {
-                          
-                            VStack {
-                       
-                                Text("I am sending a ")
-                                    .font(.system(size: 30, weight: .light))
-                                    .foregroundColor(Color.white)
-                               
-                               
-                                Text("Heart")
-                                    .font(.system(size: 55, weight: .light))
-                                    .foregroundColor(Color.white)
+                
+                VStack{
+                    
+                    HStack {
+                        VStack {
+                            
+                            
+                            ZStack {
+                                
+                                Image(systemName: "cube.fill")
+                                    .resizable()
+                                    .foregroundColor(Color.purple)
+                                    .frame(width: 250, height: 200)
+                                    .shadow(color: ColorManager .purple3, radius: 65, x: 30, y: 50)
+                                    .opacity(0.5)
+                                
+                                VStack {
+                                    
+                                    
+                                    Text("Thinking of you")
+                                        .font(.system(size: 30, weight: .light))
+                                        .foregroundColor(ColorManager .grey1)
+                                        .opacity(0.5)
+                                    
+//                                    Text("light as butterfly wings")
+//                                        .font(.system(size: 20, weight: .light))
+//                                        .foregroundColor(ColorManager .grey1)
+//                                        .opacity(0.5)
+                                }
                             }
                         }
-                      
-                        
                     }
-                    
-
                 }
-             
-         
-                VStack {
-
-                 
-                    Spacer()
-                        .frame(height: 7)
-
-                    Text("Because I am thinking of you")
-                        .font(.system(size: 23))
-                        .fontWeight(.light)
-                        .foregroundColor(Color.white)
-                        .multilineTextAlignment(.center)
-
-//        
+                    
+                
                     VStack {
                         Spacer()
-                            .frame(height: 10)
+                            .frame(height: 150)
                         
                         NavigationLink( destination:  PhotoPopView(user: user, friends: friends),
                                         label: {
                             Text("Include a PhotoPOP")
                                 .fontWeight(.bold)
-                                .frame(width: 300, height: 40)
+                                .frame(width: 200, height: 40)
                                 .foregroundColor(Color.black)
                                 .font(.system(size: 16))
                                 .background(Color.cyan)
@@ -132,15 +126,15 @@ struct SaySomethingNice5: View {
                             .frame(height: 20)
                         
                         HStack {
-                      
-//                            Text("select >")
-//                                .fontWeight(.thin)
-//                                .frame(width: 100, height: 30)
-//                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-//                                .font(.system(size: 20))
-//                                .background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
-//                                .cornerRadius(25)
-//                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                            
+                            //                            Text("select >")
+                            //                                .fontWeight(.thin)
+                            //                                .frame(width: 100, height: 30)
+                            //                                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                            //                                .font(.system(size: 20))
+                            //                                .background(Color(hue: 0.555, saturation: 1.0, brightness: 0.845))
+                            //                                .cornerRadius(25)
+                            //                                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
                             //                                })
                             
                             if friends.count > 0 {
@@ -221,9 +215,9 @@ struct SaySomethingNice5: View {
                                 
                             }
                         }
-                  
                         
-             
+                        
+                        
                         Spacer()
                             .frame(height: 20)
                         
@@ -235,7 +229,7 @@ struct SaySomethingNice5: View {
                         },
                                label: {
                             Text("SHARE")
-                        
+                            
                                 .fontWeight(.thin)
                                 .frame(width: 100, height: 30)
                                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
@@ -247,93 +241,93 @@ struct SaySomethingNice5: View {
                                     Button("OK", role: .cancel) { }
                                 }
                         })
-//                        .confettiCannon(counter: $counter)
-
+                        //                        .confettiCannon(counter: $counter)
+                        
                         
                         VStack {
                             
                             
-                                 Button(action: {
-                                     sessionManager.showLogin()
-                                 },
-                                     label: {
-                                     Image("home-alt2")
-                                         .frame(width: 50, height: 25)
-                                         .foregroundColor(.white)
-                                         .font(.system(size: 20))
-                                         .background(ColorManager .grey3)
-                                         .cornerRadius(15)
-                                         .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
-                                         .opacity(0.70)
-                                     
-                                 })
+                            Button(action: {
+                                sessionManager.showLogin()
+                            },
+                                   label: {
+                                Image("home-alt2")
+                                    .frame(width: 50, height: 25)
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 20))
+                                    .background(ColorManager .grey3)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+                                    .opacity(0.70)
+                                
+                            })
                             
                             Spacer()
                                 .frame(height: 30)
                             
                             
-//                            Text("By tapping [Share] your CareHeart is automatically \nsent to: in-app & push notifications + our Widget")
-//                                .fontWeight(.bold)
-//                                .frame(width: 300, height: 40)
-//                                .foregroundColor(Color.black)
-//                                .font(.system(size: 10))
-//                                .background(Color.cyan)
-//                                .glow(color: ColorManager.purple1, radius: 1)
-//                                .shadow(color: .white, radius: 3, x: -4, y: 4)
-//                                .opacity(0.9)
-//                                .cornerRadius(15)
-//                                .shadow(color: Color.white, radius: 2, x: 0, y: 2)
+                            //                            Text("By tapping [Share] your CareHeart is automatically \nsent to: in-app & push notifications + our Widget")
+                            //                                .fontWeight(.bold)
+                            //                                .frame(width: 300, height: 40)
+                            //                                .foregroundColor(Color.black)
+                            //                                .font(.system(size: 10))
+                            //                                .background(Color.cyan)
+                            //                                .glow(color: ColorManager.purple1, radius: 1)
+                            //                                .shadow(color: .white, radius: 3, x: -4, y: 4)
+                            //                                .opacity(0.9)
+                            //                                .cornerRadius(15)
+                            //                                .shadow(color: Color.white, radius: 2, x: 0, y: 2)
                         }
                         
-                    }        
+                    }
                 }
                 
             }
             
         }
         
-    }
-    
-    
-    func shareButtonTapped() {
-        if selectedFriends.count == 0 { return }
-        for id in selectedFriends {
-            for f in friends {
-                if f.id == id {
-                    RestApi.instance.sendPushNotification(title: "BestFriends", body: "\(user.firstName) is thinking about you and sent you a Heart!", APNToken: f.APNToken)
-                    
-                    //MARK: The code below creates an in-app notification for your friend (f.id)
-                    //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
-                    RestApi.instance.createInAppNotification(ian: InAppNotification(user: f.id, sender: user.id, text: "Is thinking about you and sent you a Heart!", createdOn: Int64(Date().timeIntervalSince1970))).then({ response in
-                        print("Create a heart notification response code: ", response)
-                    })
-                    RestApi.instance.createStreakLog(friendID: f.id)
+        
+        
+        
+        func shareButtonTapped() {
+            if selectedFriends.count == 0 { return }
+            for id in selectedFriends {
+                for f in friends {
+                    if f.id == id {
+                        RestApi.instance.sendPushNotification(title: "BestFriends", body: "\(user.firstName) is thinking about you and sent you a Heart!", APNToken: f.APNToken)
+                        
+                        //MARK: The code below creates an in-app notification for your friend (f.id)
+                        //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
+                        RestApi.instance.createInAppNotification(ian: InAppNotification(user: f.id, sender: user.id, text: "Is thinking about you and sent you a Heart!", createdOn: Int64(Date().timeIntervalSince1970))).then({ response in
+                            print("Create a heart notification response code: ", response)
+                        })
+                        RestApi.instance.createStreakLog(friendID: f.id)
+                    }
                 }
             }
+            shareColor = ColorManager.darkGrey
+            showingAlert = true
         }
-        shareColor = ColorManager.darkGrey
-        showingAlert = true
-    }
-    
-    struct RectView: View {
-        let user: User
-        let friend: User
-        let color: Color
         
-        var body: some View {
-            Text(friend.firstName + " " + String(friend.lastName.first!))
-                .fontWeight(.bold)
-                .frame(width: 80, height: 80)
-                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                .font(.system(size: 8))
-                .background(color)
-                .cornerRadius(75)
-                .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+        struct RectView: View {
+            let user: User
+            let friend: User
+            let color: Color
+            
+            var body: some View {
+                Text(friend.firstName + " " + String(friend.lastName.first!))
+                    .fontWeight(.bold)
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    .font(.system(size: 8))
+                    .background(color)
+                    .cornerRadius(75)
+                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+            }
         }
+        
+        
+        
     }
-    
-    
-    
-}
 
 
