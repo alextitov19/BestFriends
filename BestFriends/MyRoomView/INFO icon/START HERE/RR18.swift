1beecf7a86b338691b14e0311afee2d4d4937b91
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-12-01 17:46:18 -0500

    rob

---
+++ BestFriends/MyRoomView/INFO icon/START HERE/RR18.swift
@@ -0,0 +1,227 @@
+//
+//  RR18.swift
+//  BestFriends
+//
+//  Created by Zhengxu Wang on 8/18/23.
+//
+
+
+import Foundation
+import SwiftUI
+
+struct RR18: View {
+    @EnvironmentObject var sessionManager: SessionManager
+    
+    let user: User
+    let atmosphere: Atmosphere
+    let friends: [User]
+    let friendAtmospheres: [Atmosphere]
+    let groups: [Group]
+    
+    
+    
+    @State private var selectedFriends: [String] = []
+    @State private var colors: [Color] = [ColorManager.purple3, ColorManager.purple3, ColorManager.purple3, ColorManager.purple3, ColorManager.purple3]
+    @State private var shareColor = ColorManager.purple5
+    @State private var showingAlert = false
+    
+    @State private var counter = 0
+    
+    @State private var mood: Int = -1
+    @State private var summary = ""
+    @State private var sharedWith: [String] = []
+    @State private var colorChangeTap: String = ""
+    @State private var shareTapped: Bool = false
+    
+    var body: some View {
+        //        ScrollView {
+        
+        ZStack {
+            
+            Color.black
+                .opacity(0.9)
+                .ignoresSafeArea()
+            
+            AdPlayerView(name: "sky2")
+                .ignoresSafeArea()
+                .blendMode(.screen)
+            
+            
+            VStack {
+                
+                ZStack {
+                    Image("comicBubble")
+                        .resizable()
+                        .foregroundColor(Color.purple)
+                        .frame(width: 430, height: 200)
+                        .shadow(color: ColorManager .purple2, radius: 3, x: 3, y: 3)
+                        .rotationEffect(.degrees(-10))
+                        .opacity(0.95)
+                        .blinking(duration: 2.0).blinking(duration: 3.0)
+                    
+                    
+                    VStack {
+                        
+                        VStack {
+                            
+                            
+                            
+                            Text("It's about showing \nyour friends you care")
+                                .font(.system(size: 25))
+                                .fontWeight(.regular)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(Color.cyan)
+                                .rotationEffect(.degrees(-10))
+                                .blinking(duration: 2.0)
+                            
+                            
+                            //                            Text("No feelings of...")
+                            //                                .font(.system(size: 15))
+                            //                                .italic()
+                            //                                .fontWeight(.light)
+                            //                                .multilineTextAlignment(.leading)
+                            //                                .rotationEffect(.degrees(-10))
+                            //                                .foregroundColor(Color.white)
+                            //                                .blinking(duration: 2.0)
+                            
+                            
+//                            Text("We got you convered")
+//                                .font(.system(size: 20))
+//                                .fontWeight(.light)
+//                                .multilineTextAlignment(.leading)
+//                                .rotationEffect(.degrees(-10))
+//                                .foregroundColor(Color.white)
+//                                .blinking(duration: 2.0)
+//
+//
+                        }
+                    }
+                }
+                
+                ZStack {
+                    
+                    
+                    Image(systemName: "circle.fill")
+                        .resizable()
+                        .foregroundColor(Color.purple)
+                        .frame(width: 450, height: 450)
+                        .shadow(color: ColorManager .purple2, radius: 65, x: 30, y: 50)
+                        .opacity(0.3)
+                    
+                    VStack {
+                        
+                        Text("It's about letting the outside \nof you match the inside of you \nin a safe place")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color.white)
+                            .fontWeight(.light)
+                            .multilineTextAlignment(.center)
+                            .opacity(0.7)
+                        
+//                        Spacer()
+//                            .frame(height: 10)
+//
+//                        Text("comming Q1:2024")
+//                            .font(.system(size: 27))
+//                            .italic()
+//                            .multilineTextAlignment(.center)
+//                            .opacity(0.4)
+//
+//
+//
+//                        Spacer()
+//                            .frame(height: 20)
+//
+//                        VStack {
+//
+//                            Link(destination: URL(string: "https://socialtechlabs.com/our-story-2/")!) {
+//
+//                                Text("We're here to help")
+//                                    .fontWeight(.thin)
+//                                    .frame(width: 240, height: 40)
+//                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+//                                    .font(.system(size: 25))
+//                                    .background(Color.cyan)
+//                                    .cornerRadius(10)
+//                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+//
+//                            }
+//
+                            
+                            Spacer()
+                                .frame(height: 30)
+                            
+                            VStack {
+                                
+                                Button(action: {
+                                    sessionManager.showLogin()
+                                },
+                                       label: {
+                                    Image("home-alt2")
+                                        .frame(width: 60, height: 30)
+                                        .foregroundColor(.black)
+                                        .font(.system(size: 20))
+                                        .background(ColorManager .grey3)
+                                        .cornerRadius(15)
+                                        .shadow(color: Color(.gray), radius: 1, x: 0, y: 2.5)
+                                        .opacity(0.70)
+                                    
+                                })
+                            }
+                        }
+                    }
+              
+                
+                
+                ZStack {
+                    
+                    NavigationLink(
+                        destination:  RR58(user: user, atmosphere: atmosphere, friends: friends, friendAtmospheres: friendAtmospheres, groups: groups),
+                        label: {
+                            VStack {
+                                Text("Off Planet")
+                                    .fontWeight(.light)
+                                    .frame(width: 220, height: 40)
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                    .font(.system(size: 25))
+                                    .background(Color.cyan)
+                                    .cornerRadius(15)
+                                    .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                Text("when your friends arn't around")
+                                    .fontWeight(.thin)
+                                    .italic()
+                                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                
+                            }
+                        }
+                    )}
+                
+                  
+                    Spacer()
+                        .frame(height: 40)
+                    
+                }
+            }
+        }
+    }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
