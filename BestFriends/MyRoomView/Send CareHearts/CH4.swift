965712322d246ffc8884c4ee25be55d34f3503e2
robroe1 <76884097+robroe1@users.noreply.github.com>
2023-12-24 10:57:20 -0500

    rob

--- BestFriends/MyRoomView/Send CareHearts/CH4.swift
+++ BestFriends/MyRoomView/Send CareHearts/CH4.swift
@@ -449,7 +449,7 @@ struct CH4: View {
         for id in selectedFriends {
             for f in friends {
                 if f.id == id {
-                    RestApi.instance.sendPushNotification(title: "BestFriends", body: "  \(user.firstName) just sent you a Song in chat", APNToken: f.APNToken)
+                    RestApi.instance.sendPushNotification(title: "BestFriends", body: "  \(user.firstName) just sent you a Song. Check Chat and 'received' CareHearts", APNToken: f.APNToken)
                     
                     //MARK: The code below creates an in-app notification for your friend (f.id)
                     //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
--- BestFriends/MyRoomView/Send CareHearts/RR27.swift
+++ BestFriends/MyRoomView/Send CareHearts/RR27.swift
@@ -402,7 +402,7 @@ struct RR27: View {
         for id in selectedFriends {
             for f in friends {
                 if f.id == id {
-                    RestApi.instance.sendPushNotification(title: "BestFriends", body: "  \(user.firstName) You just got a big Thumps-up message in Chat", APNToken: f.APNToken)
+                    RestApi.instance.sendPushNotification(title: "BestFriends", body: "  \(user.firstName) just wished you luck. Check Chat for message & 'received' CareHearts.", APNToken: f.APNToken)
                     
                     //MARK: The code below creates an in-app notification for your friend (f.id)
                     //MARK: DO NOT CHANGE THE TEXT OF THE NOTIFICATION, otherwise the code to take the user to a diffrent page will not work. Once you set it, do not change it.
