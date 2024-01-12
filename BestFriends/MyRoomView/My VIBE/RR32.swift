ec29ad36562174bb1651f1711ef498e54bb84abd
robroe1 <76884097+robroe1@users.noreply.github.com>
2024-01-12 03:42:02 -0500

    rob Thursday night

--- BestFriends/MyRoomView/My VIBE/RR32.swift
+++ BestFriends/MyRoomView/My VIBE/RR32.swift
@@ -364,12 +364,11 @@ struct RR32: View {
                                     
                                     Text("")
                                         .fontWeight(.light)
-                                        .frame(width: 320, height: 120)
-                                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                                        .frame(width: 330, height: 95)
                                         .font(.system(size: 25))
                                         .background(Color.black)
                                         .cornerRadius(15)
-                                        .shadow(color: Color(#colorLiteral(red: 0.2067186236, green: 0.2054963708, blue: 0.2076624334, alpha: 1)), radius: 2, x: 0, y: 2)
+                                      
                                     
                                     
                                     
