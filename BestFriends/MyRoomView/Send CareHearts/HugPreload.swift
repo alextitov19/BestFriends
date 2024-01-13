2cdc06709880fdf2155180dbf2e0cab1c857482f
robroe1 <76884097+robroe1@users.noreply.github.com>
2024-01-13 04:04:48 -0500

    rob Why CHs

--- BestFriends/MyRoomView/Send CareHearts/HugPreload.swift
+++ BestFriends/MyRoomView/Send CareHearts/HugPreload.swift
@@ -81,7 +81,7 @@ struct HugPreload: View {
                             
                             VStack {
                                 NavigationLink(destination: CH4(user: user, friend: user, friends: friends, groups: groups), label: {
-                                    HugCircle (color: Color.cyan, friendName: "Send \nComfort \nSong")
+                                    HugCircle (color: Color.cyan, friendName: "Send \ncomfort \nsong")
                                 })
                                 .offset(x: showItems ? -80 : 0, y: showItems ? 220: 0)
                                 .shadow(color: Color.blue, radius: 5, x: -2, y: -2)
@@ -94,7 +94,7 @@ struct HugPreload: View {
                                 .frame(width: 30)
                             
                             NavigationLink(destination: HugPushNotification(user: user, friends: friends), label: {
-                                HugCircle (color: Color.cyan, friendName: "Sending \nHugs")
+                                HugCircle (color: Color.cyan, friendName: "Sending \nhugs")
                             })
                             .offset(x: showItems ? -100 : 0, y: showItems ? -140: 0)
                             .shadow(color: Color .blue, radius: 5, x: 2, y: -2)
@@ -161,70 +161,35 @@ struct HugPreload: View {
                             
                             Spacer ()
                                 .frame(width: 60)
-//                            NavigationLink(destination: RRWantToTalk(user: user, friend: user, friends: friends, groups: groups), label: {
-//                                HugCircle (color: Color.cyan, friendName: "Want to \ntalk \nor text?")
-//                            })
-//                            .offset(x: showItems ? -80 : 0, y: showItems ? 220: 0)
-//                            .shadow(color: Color.blue, radius: 5, x: -2, y: -2)
-//                            .opacity(0.3)
 
                     }
                 }
                    
-//                    Spacer()
-//                        .frame(height: 10)
-                    
+
 //        *************************** Center
                     HStack {
                        
-                        Spacer ()
-                            .frame(width: 6)
+                        Spacer()
+                            .frame(width: 60)
                         
                         NavigationLink(destination: RRWantToTalk(user: user, friend: user, friends: friends, groups: groups), label: {
-                            HugCircle (color: Color.cyan, friendName: "Want to \ntalk \nor text?")
+                            HugCircle (color: Color.cyan, friendName: "Want to \ntalk?")
                         })
                         .offset(x: showItems ? -80 : 0, y: showItems ? 220: 0)
                         .shadow(color: Color.blue, radius: 5, x: -2, y: -2)
                         .opacity(0.3)
                         
                         Spacer()
-                            .frame(width: 40)
+                            .frame(width: 100)
+
+                        NavigationLink(destination: RRWantToTalk(user: user, friend: user, friends: friends, groups: groups), label: {
+                            HugCircle (color: Color.cyan, friendName: "Say \nsomething \nnice")
+                        })
+                        .offset(x: showItems ? -80 : 0, y: showItems ? 220: 0)
+                        .shadow(color: Color.blue, radius: 5, x: -2, y: -2)
+                        .opacity(0.3)
                         
-                        ZStack {
-                            
-                            //                            Image("planet_6")
-                            //
-                            //                                .resizable()
-                            //                                .scaledToFit()
-                            //                                .frame(width: 120, height: 120)
-                            //                                .foregroundColor(Color.orange)
-                            //                                .opacity(0.95)
-                            
-                            ZStack {
-//                                NavigationLink(destination: EmptyView(), label: {
-//                                    HugCircle (color: Color.cyan, friendName: "")
-//                                })
-//                                .offset(x: showItems ? -100 : 0, y: showItems ? -140: 0)
-//                                .shadow(color: Color .blue, radius: 5, x: 2, y: -2)
-//                                .opacity(0.99)
-//
-//
-//                                NavigationLink(
-//                                    destination:  PhotoPopView(user: user, friends: friends),
-//                                    label: {
-//
-//                                        Image("IconPhotoNew")
-//                                            .resizable()
-//                                            .scaledToFit()
-//                                            .frame(width: 70, height: 70)
-//                                            .foregroundColor(Color.purple)
-//                                            .glow(color: Color.white, radius: 0.3)
-//                                            .shadow(color: .white, radius: 1, x: 3, y: -0.5)
-//                                            .opacity(0.95)
-//
-//                                    })
-                            }
-                        }
+                    
                     }
                         
 //      *************************
