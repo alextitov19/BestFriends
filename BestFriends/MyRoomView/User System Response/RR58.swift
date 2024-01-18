cf23d72365bee3373fe0bbb669e1f3f88cc43a6f
robroe1 <76884097+robroe1@users.noreply.github.com>
2024-01-18 05:43:44 -0500

    rob

--- BestFriends/MyRoomView/User System Response/RR58.swift
+++ BestFriends/MyRoomView/User System Response/RR58.swift
@@ -152,46 +152,47 @@ struct RR58: View {
                         
                         NavigationLink(destination: RRLoveTarts(user: user, friend: user, friends: friends, groups: groups), label: {
                             ZStack {
-//                                VStack {
-                                    Text("my friends")
+                                VStack {
+                                    Text("Friendships")
                                         .font(.system(size: 23))
-                                        .foregroundColor(ColorManager .grey4)
+                                        .foregroundColor(ColorManager .grey1)
                                         .fontWeight(.bold)
                                         .opacity(0.15)
-                                        .rotationEffect(.degrees(-10))
+                                        .rotationEffect(.degrees(10))
                                         .multilineTextAlignment(.center)
                                         .glow(color: ColorManager .purple3, radius: 1)
                                         .opacity(0.5)
                                     
                                     
-//
-//                                    Spacer ()
-//                                        .frame(height: 20)
                                     
-//                                }
+                                    Spacer ()
+                                        .frame(height: 20)
+                                    
+                                }
                                 
                                 Image(systemName: "circle")
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 160, height: 160)
-                                    .foregroundColor(ColorManager .purple3)
-                                    .opacity(0.5)
-//                                    .rotationEffect(.degrees(-10))
+                                    .foregroundColor(Color.cyan)
+                                    .opacity(0.05)
+                                    .rotationEffect(.degrees(-10))
                                     .glow(color: Color.purple, radius: 1)
                                     .shadow(color: Color.purple, radius: 2, x: 0, y: 3)
                                 
-                                Image("planet_6")
+                                Image(systemName: "circle.fill")
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 150, height: 150)
                                     .foregroundColor(ColorManager .purple4)
-                                    .opacity(0.7)
+                                    .opacity(0.075)
                                 //                                        .rotationEffect(.degrees(10))
                                     .glow(color: Color.white, radius: 1)
                                     .shadow(color: Color.white, radius: 2, x: 0, y: 3)
                                 
                             }
                         })
+                        
                     }
                 }
         
