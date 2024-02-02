67bdba971d3dddb13668abfecb7cfc0f39f8afe0
robroe1 <76884097+robroe1@users.noreply.github.com>
2024-02-02 18:47:48 -0500

    rob care heart received background

--- BestFriends/MyRoomView/ReceivedCareHearts/RR56.swift
+++ BestFriends/MyRoomView/ReceivedCareHearts/RR56.swift
@@ -32,7 +32,7 @@ struct RR56: View {
     var body: some View {
         
         ZStack {
-            Color.purple
+            ColorManager .grey3
                 .opacity(0.9)
                 .ignoresSafeArea()
             
@@ -44,7 +44,7 @@ struct RR56: View {
             AdPlayerView(name: "girlFriends1")
                 .ignoresSafeArea()
                 .blendMode(.screen)
-                .opacity(0.4)
+                .opacity(0.2)
             
             
             ZStack{
